f80e1184794339170b922a6ecaccf0fdbb7bdb13
stanta <st.takt@gmail.com>
2021-08-04 12:33:49 +0300

    added development network fpr ganache-cli

--- truffle-config.js
+++ truffle-config.js
@@ -3,5 +3,17 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "vapp/src/contracts"),
-
+  networks: {
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    //
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    }
+  }
 };
