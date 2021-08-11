fd8fa78cb9c3c6d11a4a08d610d98d6eba9c1714
stanta <st.takt@gmail.com>
2021-08-11 16:25:53 +0300

    deploy conf to Mumbai testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
-const path = require('path')
+const path = require('path');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const pk = require("../pk.json");
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -9,8 +12,24 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
-    }
-  },
+    },
+    mumbai: {
+      provider: () => new HDWalletProvider({
+          privateKeys: pk["80001"]["pk"], 
+          providerOrUrl:  pk["80001"]["infkey"]
+        }),
+          network_id: 80001
+      // https://ropsten.infura.io/v3/753a98a2eb6c4d64918829f47d069440", // Endpoint of an node to connect to. Can be on localhost or on the internet
+      },
+  
+  devpk: {
+    provider: () => new HDWalletProvider({
+        privateKeys: pk["999"]["pk"], 
+        providerOrUrl: `http://localhost:8545`}),
+        network_id: 999
+    // https://ropsten.infura.io/v3/753a98a2eb6c4d64918829f47d069440", // Endpoint of an node to connect to. Can be on localhost or on the internet
+    },
+},
   compilers: {
     solc: {
       version: "^0.8.0"
