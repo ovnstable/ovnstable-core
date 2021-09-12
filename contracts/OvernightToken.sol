463e0c92b9a9734683782aaec401f619cac21755
Mosin Nick <mosinnik@yandex.ru>
2021-09-12 05:35:04 +0300

    add reward function to Exchange in naive realization

--- contracts/OvernightToken.sol
+++ contracts/OvernightToken.sol
@@ -2,12 +2,15 @@
 pragma solidity >=0.5.0 <0.9.0;
 import "./interfaces/IERC20MintableBurnable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 contract OvernightToken is IERC20MintableBurnable, ERC20 {
-
+    using EnumerableSet for EnumerableSet.AddressSet;
 
     uint256 private _totalMint;
     uint256 private _totalBurn;
+
+    EnumerableSet.AddressSet private _owners;
 
     constructor() ERC20("TstOvernightToken", "OVN") {}
 
@@ -32,5 +35,41 @@ contract OvernightToken is IERC20MintableBurnable, ERC20 {
 
     function totalBurn() public view returns (uint256) {
         return _totalBurn;
+    }
+
+    function ownerLength() public view returns (uint256) {
+        return _owners.length();
+    }
+
+    function ownerAt(uint256 index) public view returns (address) {
+        return _owners.at(index);
+    }
+
+    function ownerBalanceAt(uint256 index) public view returns (uint256) {
+        return balanceOf(_owners.at(index));
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, amount);
+
+        if (from == address(0)) {
+            // mint
+            _owners.add(to);
+        } else if (to == address(0)) {
+            // burn
+            if (balanceOf(from) == 0) {
+               _owners.remove(from); 
+            }
+        } else {
+            // transfer
+            if (balanceOf(from) == 0) {
+               _owners.remove(from); 
+            }
+            _owners.add(to);
+        }
     }
 }
