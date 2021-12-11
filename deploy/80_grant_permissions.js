12fd2f51c7f22e56aeb1805ff031b3ef77d67ec4
y.pavlov <lenvonder12vorh@gmail.com>
2021-12-11 18:31:00 +0300

    Remove Migrations.sol, add permissions.js

--- deploy/80_grant_permissions.js
+++ deploy/80_grant_permissions.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+const {ethers} = require("hardhat");
 
 const fs = require("fs");
 
@@ -6,9 +6,45 @@ module.exports = async ({getNamedAccounts, deployments}) => {
     const {deploy} = deployments;
     const {deployer} = await getNamedAccounts();
 
+    const timelockController = await ethers.getContract("TimelockController");
+    let address = timelockController.address;
 
+    await grantRevokeRole(await ethers.getContract("OvernightToken"), deployer, address);
+    await grantRevokeRole(await ethers.getContract("Exchange"), deployer, address);
+    await grantRevokeRole(await ethers.getContract("PortfolioManager"), deployer, address);
+    await grantRevokeRole(await ethers.getContract("Balancer"), deployer, address);
+    await grantRevokeRole(await ethers.getContract("Portfolio"), deployer, address);
+    await grantRevokeRole(await ethers.getContract("RewardManager"), deployer, address);
+    await grantRevokeRole(await ethers.getContract("Vault"), deployer, address);
+
+    await transferOwnership(await ethers.getContract("Mark2Market"), address);
+
+    await transferOwnership(await ethers.getContract("ConnectorAAVE"), address);
+    await transferOwnership(await ethers.getContract("ConnectorCurve"), address);
+    await transferOwnership(await ethers.getContract("ConnectorIDLE"), address);
 
 };
 
 module.exports.tags = ['permissions'];
 
+
+async function transferOwnership(contract, newAdmin) {
+
+    let tx = await contract.transferOwnership(newAdmin);
+    await tx.wait();
+    console.log('Contract:' + contract + '=> Transfer ownership to ' + newAdmin + '=> done')
+
+}
+
+async function grantRevokeRole(contract, oldAdmin, newAdmin) {
+
+    let tx = await contract.grantRole(await contract.DEFAULT_ADMIN_ROLE(), newAdmin);
+    await tx.wait();
+    console.log('Contract:' + contract + '=> Grant role ADMIN to ' + newAdmin + '=> done')
+
+    tx = await contract.revokeRole(await contract.DEFAULT_ADMIN_ROLE(), oldAdmin);
+    await tx.wait();
+
+    console.log('Contract:' + contract + '=> Revoke role ADMIN to ' + oldAdmin + '=> done')
+
+}
