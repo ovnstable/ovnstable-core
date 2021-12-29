f0f1966e8342dc09c761f96a0c9b8195271aa0b1
y.pavlov <lenvonder12vorh@gmail.com>
2021-12-29 16:49:09 +0300

    Add .env files

--- vapp/deploy.sh
+++ vapp/deploy.sh
@@ -12,10 +12,14 @@ if [ "$stand" = "prod" ]
 then
   nameDapp="dapp"
   deployments="polygon"
+  build="build-production"
+  dockerComposePath="/root/ovn/docker-compose.yaml"
 elif [ "$stand" = "dev" ]
 then
   nameDapp="dapp-dev"
   deployments="polygon_dev"
+  build="build-dev"
+  dockerComposePath="/root/ovn-dev/docker-compose.yaml"
 else
   exit
 fi
@@ -29,6 +33,11 @@ cp  ../deployments/$deployments/GovToken.json src/contracts/
 cp  ../deployments/$deployments/Portfolio.json src/contracts/
 cp  ../deployments/$deployments/TimelockController.json src/contracts/
 cp  ../deployments/$deployments/UsdPlusToken.json src/contracts/
+
+
+npm run $build
+
+docker build . -t cr.yandex/crpg11k469bhc8lch9gm/overnight/$nameDapp:$tag
 
 docker login \
          --username oauth \
@@ -44,7 +53,7 @@ ssh $url docker login \
         cr.yandex
 
 ssh $url docker pull cr.yandex/crpg11k469bhc8lch9gm/overnight/$nameDapp:$tag
-ssh $url docker-compose -f /root/ovn/docker-compose.yaml up -d --no-deps $nameDapp
+ssh $url docker-compose -f $dockerComposePath up -d --no-deps $nameDapp
 
 
 ssh $url docker logs ovn-$nameDapp -f
