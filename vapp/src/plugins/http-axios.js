45f13186ff7d1217806928fa25a1f5ad474c05c8
y.pavlov <lenvonder12vorh@gmail.com>
2022-01-19 15:15:00 +0300

    Remove vapp folders

--- vapp/src/plugins/http-axios.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-import Axios from 'axios';
-let axios = Axios.create({
-    baseURL: (process.env.VUE_APP_HOST ? process.env.VUE_APP_HOST : '') + process.env.VUE_APP_API,
-});
-
-export {axios};
