d467761cc5ba1ea529278b9a413de105273a5fa9
y.pavlov <lenvonder12vorh@gmail.com>
2022-01-21 17:24:41 +0300

    Add AccessControl to ConnectorMStable.sol, Usdc2VimUsdTokenExchange

--- test/exchange.js
+++ test/exchange.js
@@ -4,7 +4,7 @@ const {deployments, ethers, getNamedAccounts} = require('hardhat');
 const {smock} = require("@defi-wonderland/smock");
 
 const fs = require("fs");
-const {toUSDC, fromE18} = require("../utils/decimals");
+const {toUSDC, fromE18, fromOvn} = require("../utils/decimals");
 const hre = require("hardhat");
 const BN = require('bignumber.js');
 
@@ -46,6 +46,7 @@ describe("Exchange", function () {
         let weights;
         let assetPrices;
         let totalUsdcPrice;
+        let balanceUser;
 
         before(async () => {
             const sum = toUSDC(100);
@@ -58,8 +59,12 @@ describe("Exchange", function () {
             weights = await portfolio.getAllAssetWeights();
             assetPrices = await totalAssetPrices.assetPrices;
             totalUsdcPrice = await totalAssetPrices.totalUsdcPrice;
+            balanceUser = fromOvn(await usdPlus.balanceOf(account));
 
+        });
 
+        it("Balance USD+ should 99.96", function () {
+            expect(balanceUser.toString()).to.eq("99.96")
         });
 
 
@@ -88,64 +93,65 @@ describe("Exchange", function () {
             }
         });
 
+        describe("Redeem 50 USD+", function () {
+
+            let weights;
+            let assetPrices;
+            let totalUsdcPrice;
+            let balanceAccount;
+
+            before(async () => {
+                await usdPlus.approve(exchange.address, toUSDC(50));
+                let result = await exchange.redeem(usdc.address, toUSDC(50));
+                await result.wait();
+
+                let totalAssetPrices = await m2m.assetPrices();
+                weights = await portfolio.getAllAssetWeights();
+                assetPrices = await totalAssetPrices.assetPrices;
+                totalUsdcPrice = await totalAssetPrices.totalUsdcPrice;
+
+                balanceAccount = fromOvn(await usdPlus.balanceOf(account));
+
+            });
+
+            it("Balance USD+ should 49.96", function () {
+                expect(balanceAccount.toString()).to.eq("49.96")
+            });
+
+            it("total vault balance (USDC) should equal 49.96 (USDC)", function () {
+                console.log("totalUsdcPrice " + totalUsdcPrice);
+
+                expect(new BN(fromE18(totalUsdcPrice)).toFixed(2)).to.eq("49.96")
+            });
+
+            it("asset amounts match asset weights", function () {
+
+                let totalValue = 50;
+                for (let i = 0; i < weights.length; i++) {
+
+                    let weight = weights[i];
+                    let asset = findAssetPrice(weight.asset, assetPrices);
+
+                    let target = weight.targetWeight / 1000;
+                    let balance = (asset.amountInVault / asset.usdcPriceDenominator) * (asset.usdcSellPrice / asset.usdcPriceDenominator);
+
+                    let targetValue = totalValue / 100 * target + "";
+                    let message = 'Balance ' + balance + " weight " + target + " asset " + weight.asset + " symbol " + asset.symbol + " target value " + targetValue;
+                    console.log(message);
+
+                    // expect(new BN(balance).toFixed(0)).to.eq(targetValue, message);
+                }
+            });
+
+
+        })
 
     });
 
-    describe("Mint 100 USD+ => Redeem 50 USD+", function () {
-
-        let weights;
-        let assetPrices;
-        let totalUsdcPrice;
-
-        before(async () => {
-            const sum = toUSDC(100);
-            await usdc.approve(exchange.address, sum);
-
-            let result = await exchange.buy(assets.usdc, sum);
-            await result.wait();
-
-            await usdPlus.approve(exchange.address, toUSDC(50));
-            result = await exchange.redeem(usdc.address, toUSDC(50));
-            await result.wait();
-
-            let totalAssetPrices = await m2m.assetPrices();
-            weights = await portfolio.getAllAssetWeights();
-            assetPrices = await totalAssetPrices.assetPrices;
-            totalUsdcPrice = await totalAssetPrices.totalUsdcPrice;
-
-
-        });
-
-
-        it("total vault balance (USDC) should equal 49.97 (USDC)", function () {
-            console.log("totalUsdcPrice " + totalUsdcPrice);
-
-            expect(new BN(fromE18(totalUsdcPrice)).toFixed(2)).to.eq("49.97")
-        });
-
-        it("asset amounts match asset weights", function () {
-
-            let totalValue = 50;
-            for (let i = 0; i < weights.length; i++) {
-
-                let weight = weights[i];
-                let asset = findAssetPrice(weight.asset, assetPrices);
-
-                let target = weight.targetWeight / 1000;
-                let balance = (asset.amountInVault / asset.usdcPriceDenominator) * (asset.usdcSellPrice / asset.usdcPriceDenominator);
-
-                let targetValue = totalValue / 100 * target + "";
-                let message = 'Balance ' + balance + " weight " + target + " asset " + weight.asset + " symbol " + asset.symbol + " target value " + targetValue;
-                console.log(message);
-
-                expect(new BN(balance).toFixed(0)).to.eq(targetValue, message);
-            }
-        });
-
-
-    });
 
 });
+
+
 
 
 function findAssetPrice(address, assetPrices) {
