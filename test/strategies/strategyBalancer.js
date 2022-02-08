ace93a9614c6ed8c7329a6a8ebe4ba91e26304d4
Mosin Nick <mosinnik@yandex.ru>
2022-02-08 21:10:53 +0300

    gte in tests

--- test/strategies/strategyBalancer.js
+++ test/strategies/strategyBalancer.js
@@ -81,8 +81,8 @@ describe("StrategyBalancer", function () {
                 balanceUSDC = fromUSDC(balanceUsdcAfter - balanceUsdcBefore);
             });
 
-            it("Balance USDC should be eq 50 USDC", async function () {
-                greatLess(balanceUSDC, 50, 1);
+            it("Balance USDC should be gte 50 USDC", async function () {
+                expect(balanceUSDC).to.greaterThanOrEqual(50);
             });
 
             it("Balance bpspTUsd should be eq 50", async function () {
--- test/strategies/strategyMStable.js
+++ test/strategies/strategyMStable.js
@@ -79,8 +79,8 @@ describe("StrategyMStable", function () {
                 balanceUSDC = fromUSDC(balanceUsdcAfter - balanceUsdcBefore);
             });
 
-            it("Balance USDC should be eq 50 USDC", async function () {
-                greatLess(balanceUSDC, 50, 1);
+            it("Balance USDC should be gte 50 USDC", async function () {
+                expect(balanceUSDC).to.greaterThanOrEqual(50);
             });
 
             it("Balance vimUsd should be eq 450", async function () {
