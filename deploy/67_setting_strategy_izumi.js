cd60476c6d77f975d7a188f6bd59ff940b325298
Yaroslav <lenvonder12vorh@gmail.com>
2022-03-01 12:44:22 +0300

    Fix izumi (#19)

--- deploy/67_setting_strategy_izumi.js
+++ deploy/67_setting_strategy_izumi.js
@@ -7,12 +7,12 @@ let assets = JSON.parse(fs.readFileSync('./assets.json'));
 
 let uniswapV3PositionManager = "0xc36442b4a4522e871399cd717abdd847ab11fe88";
 let uniswapV3Pool = "0x3F5228d0e7D75467366be7De2c31D0d098bA2C23";
-let uniswapV2Router = "0xa5e0829caced8ffdd4de3c43696c57f7d7a678ff";
 let uniswapV3Router = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
 let izumiBoost = "0x01cc44fc1246d17681b325926865cdb6242277a5";
 let uniswapNftToken = "0xC36442b4a4522E871399CD717aBDD847Ab11FE88";
 let balancerPoolId = "0x0d34e5dd4d8f043557145598e4e2dc286b35fd4f000000000000000000000068";
 let balancerVault = "0xBA12222222228d8Ba445958a75a0704d566BF2C8";
+let aaveCurve = "0x445fe580ef8d70ff569ab36e80c647af338db351";
 
 module.exports = async ({getNamedAccounts, deployments}) => {
     const {deploy} = deployments;
@@ -22,7 +22,7 @@ module.exports = async ({getNamedAccounts, deployments}) => {
     const pm = await ethers.getContract("PortfolioManager");
 
     await (await strategy.setTokens(assets.usdc, assets.usdt, assets.izi, assets.yin, uniswapNftToken, assets.weth)).wait();
-    await (await strategy.setParams(uniswapV3PositionManager, uniswapV3Pool, uniswapV2Router, izumiBoost, uniswapV3Router, balancerPoolId, balancerVault)).wait();
+    await (await strategy.setParams(uniswapV3PositionManager, uniswapV3Pool,  izumiBoost, uniswapV3Router, balancerPoolId, balancerVault, aaveCurve)).wait();
     await (await strategy.setPortfolioManager(pm.address)).wait();
 
     console.log('StrategyIzumi setting done');
--- scripts/exchange_mint.js
+++ scripts/exchange_mint.js
@@ -6,9 +6,9 @@ const ethers = hre.ethers;
 let ERC20 = JSON.parse(fs.readFileSync('./artifacts/@openzeppelin/contracts/token/ERC20/ERC20.sol/ERC20.json'));
 let ERC20Metadata = JSON.parse(fs.readFileSync('./artifacts/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol/IERC20Metadata.json'));
 
-let Exchange = JSON.parse(fs.readFileSync('./deployments/polygon_dev/Exchange.json'));
-let PM = JSON.parse(fs.readFileSync('./deployments/polygon_dev/PortfolioManager.json'));
-let M2M = JSON.parse(fs.readFileSync('./deployments/polygon_dev/Mark2Market.json'));
+let Exchange = JSON.parse(fs.readFileSync('./deployments/polygon/Exchange.json'));
+let PM = JSON.parse(fs.readFileSync('./deployments/polygon/PortfolioManager.json'));
+let M2M = JSON.parse(fs.readFileSync('./deployments/polygon/Mark2Market.json'));
 
 async function main() {
     // need to run inside IDEA via node script running
@@ -28,76 +28,6 @@ async function main() {
     let m2m = await ethers.getContractAt(M2M.abi, M2M.address, wallet);
 
 
-    let items = await m2m.strategyAssets();
-    for (let i = 0; i <items.length ; i++) {
-
-        let item = items[i];
-        console.log("strategy " + item.strategy);
-        console.log("net asset " + fromUSDC(item.netAssetValue));
-        console.log("liq value " + fromUSDC(item.liquidationValue));
-    }
-
-    let aave = {
-        strategy: (await ethers.getContract("StrategyAave")).address,
-        minWeight: 0,
-        targetWeight: 5000,
-        maxWeight: 100000,
-        enabled: true,
-        enabledReward: true,
-    }
-    let curve = {
-        strategy: (await ethers.getContract("StrategyCurve")).address,
-        minWeight: 0,
-        targetWeight: 0,
-        maxWeight: 100000,
-        enabled: true,
-        enabledReward: true,
-    }
-
-    let mstable= {
-        strategy: (await ethers.getContract("StrategyMStable")).address,
-        minWeight: 0,
-        targetWeight: 20000,
-        maxWeight: 100000,
-        enabled: true,
-        enabledReward: true,
-    }
-
-    let izumu = {
-        strategy: (await ethers.getContract("StrategyIzumi")).address,
-        minWeight: 0,
-        targetWeight:75000,
-        maxWeight: 100000,
-        enabled: true,
-        enabledReward: true,
-    }
-
-    let balancer = {
-        strategy: (await ethers.getContract("StrategyBalancer")).address,
-        minWeight: 0,
-        targetWeight: 0,
-        maxWeight: 100000,
-        enabled: true,
-        enabledReward: true,
-    }
-
-    let idle = {
-        strategy: (await ethers.getContract("StrategyIdle")).address,
-        minWeight: 0,
-        targetWeight: 0,
-        maxWeight: 100000,
-        enabled: false,
-        enabledReward: false,
-    }
-
-    let weights = [
-        aave,
-        mstable,
-        izumu,
-        balancer,
-        curve,
-        idle
-    ]
 
 
     // await(await pm.balance()).wait();
@@ -110,7 +40,9 @@ async function main() {
     //
     // console.log('Balance USDC: ' + fromUSDC(balanceUSDC) )
     // await exchange.buy(USDC.address, balanceUSDC);
-    await exchange.payout();
+    // await exchange.payout();
+
+    await pm.balance();
 
 }
 
