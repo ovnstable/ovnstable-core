7e295d78602262a27d5c6777b7438e583d54a979
y.pavlov <lenvonder12vorh@gmail.com>
2022-04-14 19:42:51 +0300

    Add mint,rebalance scripts

---
+++ pkg/core/scripts/rebalance.js
@@ -0,0 +1,37 @@
+const hre = require("hardhat");
+const fs = require("fs");
+const {fromE18, fromUSDC} = require("@overnight-contracts/common/utils/decimals");
+const ethers = hre.ethers;
+
+let ERC20 = JSON.parse(fs.readFileSync('./artifacts/@openzeppelin/contracts/token/ERC20/ERC20.sol/ERC20.json'));
+let ERC20Metadata = JSON.parse(fs.readFileSync('./artifacts/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol/IERC20Metadata.json'));
+
+let PM = JSON.parse(fs.readFileSync('./deployments/polygon_dev/PortfolioManager.json'));
+
+let price = { maxFeePerGas: "1400000000000", maxPriorityFeePerGas: "1400000000000" };
+
+async function main() {
+    // need to run inside IDEA via node script running
+    await hre.run("compile");
+
+    let provider = ethers.provider;
+
+    console.log('Provider: ' + provider.connection.url);
+    let wallet = await new ethers.Wallet(process.env.PK_POLYGON, provider);
+    console.log('Wallet: ' + wallet.address);
+    const balance = await provider.getBalance(wallet.address);
+    console.log('Balance wallet: ' + fromE18(balance))
+
+    let pm = await ethers.getContractAt(PM.abi, PM.address, wallet);
+    await (await pm.balance(price)).wait();
+
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
