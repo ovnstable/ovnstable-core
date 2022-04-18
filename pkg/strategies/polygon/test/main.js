81919fc72a9dcec5c30dac1b6fc655d1d29806b5
y.pavlov <lenvonder12vorh@gmail.com>
2022-04-18 23:30:49 +0300

    Update run

--- pkg/strategies/polygon/test/main.js
+++ pkg/strategies/polygon/test/main.js
@@ -1,41 +1,50 @@
 const {POLYGON} = require('@overnight-contracts/common/utils/assets');
 const {strategyTest} = require('@overnight-contracts/common/utils/strategy-test');
 
+let id = process.env.TEST_STRATEGY;
+
 let arrays = [
-    //
-    // {
-    //     name: 'StrategyAave',
-    // },
-    // {
-    //     name: 'StrategyBalancer',
-    // },
-    // {
-    //     name: 'StrategyCurve',
-    // },
+
+    {
+        name: 'StrategyAave',
+    },
+    {
+        name: 'StrategyBalancer',
+    },
+    {
+        name: 'StrategyCurve',
+    },
     {
         name: 'StrategyDodoUsdc',
     },
     {
         name: 'StrategyDodoUsdt',
     },
-    // {
-    //     name: 'StrategyIdle',
-    // },
-    // {
-    //     name: 'StrategyImpermaxQsUsdt',
-    // },
-    // {
-    //     name: 'StrategyIzumi',
-    // },
-    // {
-    //     name: 'StrategyMStable',
-    // },
-    // {
-    //     name: 'StrategyArrakis',
-    // }
+    {
+        name: 'StrategyIdle',
+    },
+    {
+        name: 'StrategyImpermaxQsUsdt',
+    },
+    {
+        name: 'StrategyIzumi',
+    },
+    {
+        name: 'StrategyMStable',
+    },
+    {
+        name: 'StrategyArrakis',
+    }
 
 ];
 
+
+if (id !== undefined && id !== ""){
+    console.log(`Strategy ID ${id}`);
+    arrays = arrays.filter(value => value.name === id);
+}
+
+console.log(`Run tests [${arrays.map(value => value.name)}]`);
 
 describe("Polygon", function () {
 
