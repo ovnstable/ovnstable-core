d96d0ac5a535f55b80a4d892fb6daa812f070cbb
y.pavlov <lenvonder12vorh@gmail.com>
2022-04-19 15:34:30 +0300

    Deploy Strategies to Fantom

--- pkg/common/utils/core.js
+++ pkg/common/utils/core.js
@@ -9,6 +9,13 @@ function fantomDev(){
     }
 }
 
+function fantom(){
+    return {
+        pm: "0x11732E21D9DaB3B6fF6e7DD9EdcB24770260c7B4"
+    }
+}
+
+
 function polygonDev(){
     return {
         pm: "0x7f1bD57edA6995d3a4136C07f85bf7013C5f8243"
@@ -17,12 +24,12 @@ function polygonDev(){
 
 function getCoreAddresses(){
 
-    let stand = "fantom_dev";
+    let stand = "fantom";
     switch (stand){
         case "fantom_dev":
             return fantomDev();
         case "fantom":
-
+            return fantom();
         case "polygon_dev":
             return polygonDev();
         case "polygon":
--- pkg/core/scripts/set-strategies-fantom-dev.js
+++ pkg/core/scripts/set-strategies-fantom-dev.js
@@ -6,12 +6,11 @@ const ethers = hre.ethers;
 let ERC20 = JSON.parse(fs.readFileSync('./artifacts/@openzeppelin/contracts/token/ERC20/ERC20.sol/ERC20.json'));
 let ERC20Metadata = JSON.parse(fs.readFileSync('./artifacts/@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol/IERC20Metadata.json'));
 
-let Exchange = JSON.parse(fs.readFileSync('./deployments/fantom/Exchange.json'));
-let PM = JSON.parse(fs.readFileSync('./deployments/fantom/PortfolioManager.json'));
-let M2M = JSON.parse(fs.readFileSync('./deployments/fantom/Mark2Market.json'));
-let UsdPlusToken = JSON.parse(fs.readFileSync('./deployments/fantom/UsdPlusToken.json'));
+let Exchange = JSON.parse(fs.readFileSync('./deployments/fantom_dev/Exchange.json'));
+let PM = JSON.parse(fs.readFileSync('./deployments/fantom_dev/PortfolioManager.json'));
+let M2M = JSON.parse(fs.readFileSync('./deployments/fantom_dev/Mark2Market.json'));
 
-let {FANTOM } = require('@overnight-contracts/common/utils/assets');
+let {FANTOM, DEFAULT} = require('@overnight-contracts/common/utils/assets');
 
 
 
@@ -31,30 +30,65 @@ async function main() {
     let pm = await ethers.getContractAt(PM.abi, PM.address, wallet);
     let USDC = await ethers.getContractAt(ERC20.abi, FANTOM.usdc, wallet);
     let m2m = await ethers.getContractAt(M2M.abi, M2M.address, wallet);
-    let usdPlus = await ethers.getContractAt(UsdPlusToken.abi, UsdPlusToken.address, wallet);
 
 
     // await (await pm.setUsdc(FANTOM.usdc)).wait();
     // console.log('pm.setUsdc done')
     //
-    // await (await pm.setCashStrategy("0xF91FAbcC4bFFe1Dd91E1cC856379fF6b0D14e572")).wait();
+    // await (await pm.setCashStrategy("0x448e87779345cc2a4b3772DfD0f63200837B2615")).wait();
     // console.log('pm.setCashStrategy done');
 
 
     let aave = {
-        strategy: "0xF91FAbcC4bFFe1Dd91E1cC856379fF6b0D14e572",
+        strategy: "0x448e87779345cc2a4b3772DfD0f63200837B2615",
         minWeight: 0,
-        targetWeight: 5000,
-        maxWeight: 7500,
+        targetWeight: 2000,
+        maxWeight: 5000,
+        enabled: true,
+        enabledReward: true,
+    }
+
+    let beethovenxDeiUsdc= {
+        strategy: "0x08d387BAb84706946dC92651Dc794D41bb7eb6b5",
+        minWeight: 0,
+        targetWeight: 38000,
+        maxWeight: 100000,
+        enabled: true,
+        enabledReward: true,
+    }
+
+    let cream = {
+        strategy: "0xd2381abf796Fc9c83ca977E9153812B64712754A",
+        minWeight: 0,
+        targetWeight: 10000,
+        maxWeight: 100000,
+        enabled: true,
+        enabledReward: true,
+    }
+
+    let curve2Pool= {
+        strategy: "0x1E1F509963A6D33e169D9497b11c7DbFe73B7F13",
+        minWeight: 0,
+        targetWeight: 10000,
+        maxWeight: 100000,
+        enabled: true,
+        enabledReward: true,
+    }
+
+    let curveGeist= {
+        strategy: "0x1862f0115cc08dE3F24bE5109AdCcDf5E11B6350",
+        minWeight: 0,
+        targetWeight: 10000,
+        maxWeight: 100000,
         enabled: true,
         enabledReward: true,
     }
 
 
     let tarotSpiritUsdcFtm = {
-        strategy: "0xBE24082cf05aAaabb1C479aded4D8a6C015ADd27",
+        strategy: "0xB290830AA01854Ae64D352Ac51aa2783aA4dDb2a",
         minWeight: 0,
-        targetWeight: 45000,
+        targetWeight: 10000,
         maxWeight: 100000,
         enabled: true,
         enabledReward: true,
@@ -62,9 +96,18 @@ async function main() {
 
 
     let tarotSpookyUsdcFtm= {
-        strategy: "0x3E93772B43bF4bEFa4b22bdC08c254e6354FB4ee",
+        strategy: "0x3bE4a04d21D9cE2b38557CB9e89A9254AEE7c132",
         minWeight: 0,
-        targetWeight: 50000,
+        targetWeight: 10000,
+        maxWeight: 100000,
+        enabled: true,
+        enabledReward: true,
+    }
+
+    let tarotSupplyVaultUsdc = {
+        strategy: "0xF7d693CE960e70721F0353F967360046Ba7d4eFA",
+        minWeight: 0,
+        targetWeight: 10000,
         maxWeight: 100000,
         enabled: true,
         enabledReward: true,
@@ -74,22 +117,19 @@ async function main() {
 
     let weights = [
         aave,
+        cream,
+        beethovenxDeiUsdc,
+        curve2Pool,
+        curveGeist,
         tarotSpiritUsdcFtm,
         tarotSpookyUsdcFtm,
+        tarotSupplyVaultUsdc
     ]
 
 
     await (await pm.setStrategyWeights(weights)).wait();
     console.log("portfolio.setWeights done");
 
-
-    // await (await USDC.approve(exchange.address, toUSDC(50))).wait();
-    // console.log('Approve USDC done');
-    //
-    // await (await exchange.buy(FANTOM.usdc, toUSDC(50) )).wait();
-    //
-    // console.log('Buy 100 USD+ done');
-    // console.log('Total Net Assets: ' + await m2m.totalNetAssets());
 
     // await (await pm.balance()).wait();
     // console.log("portfolio.balance done");
