2cde361ddf6be68f1a91ed8dff6656b0ac577bb6
y.pavlov <lenvonder12vorh@gmail.com>
2022-05-09 13:27:57 +0300

    Add verify-utils.js

---
+++ pkg/core/scripts/verify.js
@@ -0,0 +1,16 @@
+const {verify } = require("@overnight-contracts/common/utils/verify-utils");
+
+async function main() {
+
+    let items = ["Exchange", "PortfolioManager","UsdPlusToken", "Mark2Market", "PayoutListener"];
+    await verify(items);
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
