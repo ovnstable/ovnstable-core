f3dadb5b365f088f9e08d22c68bd9000be64ee1d
y.pavlov <lenvonder12vorh@gmail.com>
2022-05-13 20:48:57 +0300

    Add Destopia to Listener:

--- pkg/core/deploy/24_setting_polygonPayoutListener.js
+++ pkg/core/deploy/24_setting_polygonPayoutListener.js
@@ -13,9 +13,10 @@ module.exports = async ({getNamedAccounts, deployments}) => {
 
 
     let pools = [
-        "0x68b7cEd0dBA382a0eC705d6d97608B7bA3CD8C55",
-        "0x901Debb34469e89FeCA591f5E5336984151fEc39",
-        "0x91f670270b86c80ec92bb6b5914e6532ca967bfb"
+        "0x68b7cEd0dBA382a0eC705d6d97608B7bA3CD8C55", // Qs
+        "0x901Debb34469e89FeCA591f5E5336984151fEc39", // Qs
+        "0x91f670270b86c80ec92bb6b5914e6532ca967bfb", // Meshswap
+        "0x421a018cC5839c4C0300AfB21C725776dc389B1a"  // Dystopia
     ]
 
     await (await polygonPL.setQsSyncPools(pools)).wait();
