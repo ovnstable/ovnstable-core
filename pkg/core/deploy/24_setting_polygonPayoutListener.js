f7ef1661b0f88b68624f9a2cd3c5e31c05b10933
y.pavlov <lenvonder12vorh@gmail.com>
2022-05-16 15:31:03 +0300

    Update polygon payout listeners

--- pkg/core/deploy/24_setting_polygonPayoutListener.js
+++ pkg/core/deploy/24_setting_polygonPayoutListener.js
@@ -13,10 +13,18 @@ module.exports = async ({getNamedAccounts, deployments}) => {
 
 
     let pools = [
-        "0x68b7cEd0dBA382a0eC705d6d97608B7bA3CD8C55", // Qs
-        "0x901Debb34469e89FeCA591f5E5336984151fEc39", // Qs
-        "0x91f670270b86c80ec92bb6b5914e6532ca967bfb", // Meshswap
-        "0x421a018cC5839c4C0300AfB21C725776dc389B1a"  // Dystopia
+
+        // dystopia usdPlus pools
+        "0x5A272ad79cBd3C874879E3FEc5753C2127f77583", // USD+/TETU
+        "0xB2094C94E8DE8d614000eC6802635524A79C30DA", // USD+/XZAR
+        "0x421a018cC5839c4C0300AfB21C725776dc389B1a", // USD+/USDC
+
+        // mesh usdPlus pools
+        "0x68b7cEd0dBA382a0eC705d6d97608B7bA3CD8C55",  // USDC/USD+
+
+        // QuickSwap usdPlus pools
+        "0x901Debb34469e89FeCA591f5E5336984151fEc39",  // USD+/WETH
+        "0x91F670270B86C80Ec92bB6B5914E6532cA967bFB",  // WMATIC/USD+
     ]
 
     await (await polygonPL.setQsSyncPools(pools)).wait();
