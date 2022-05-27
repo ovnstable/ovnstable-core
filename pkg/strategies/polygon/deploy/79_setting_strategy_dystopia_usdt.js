842ced2a085f1987d4966b43e64f7558d67d43b7
Nikita Slezkin <venikpapa@gmail.com>
2022-05-27 16:43:51 +0300

    minor fix

--- pkg/strategies/polygon/deploy/79_setting_strategy_dystopia_usdt.js
+++ pkg/strategies/polygon/deploy/79_setting_strategy_dystopia_usdt.js
@@ -4,7 +4,6 @@ let {POLYGON} = require('@overnight-contracts/common/utils/assets');
 let {core} = require('@overnight-contracts/common/utils/core');
 
 let dystToken = '0x39aB6574c289c3Ae4d88500eEc792AB5B947A5Eb';
-let wmaticToken = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270';
 let dystPair = '0x4570da74232c1A784E77c2a260F85cdDA8e7d47B'; //sAMM-USDC/USDT
 let dystRouter = '0xbE75Dd16D029c6B32B7aD57A0FD9C1c20Dd2862e'; //DystRouter01
 let gauge = '0x7c9716266795a04ae1fbbd017dc2585fbf78076d'; //aka MasterChef
@@ -12,7 +11,7 @@ let gauge = '0x7c9716266795a04ae1fbbd017dc2585fbf78076d'; //aka MasterChef
 module.exports = async () => {
     const strategy = await ethers.getContract("StrategyDystopiaUsdt");
 
-    await (await strategy.setTokens(POLYGON.usdc, POLYGON.usdt, dystToken, wmaticToken)).wait();
+    await (await strategy.setTokens(POLYGON.usdc, POLYGON.usdt, dystToken, POLYGON.wMatic)).wait();
     await (await strategy.setParams(gauge, dystPair, dystRouter, POLYGON.balancerVault, POLYGON.balancerPoolIdUsdcTusdDaiUsdt)).wait();
     await (await strategy.setPortfolioManager(core.pm)).wait();
 
--- pkg/strategies/polygon/deploy/80_setting_strategy_dystopia_dai.js
+++ pkg/strategies/polygon/deploy/80_setting_strategy_dystopia_dai.js
@@ -4,16 +4,15 @@ let {POLYGON} = require('@overnight-contracts/common/utils/assets');
 let {core} = require('@overnight-contracts/common/utils/core');
 
 let dystToken = '0x39aB6574c289c3Ae4d88500eEc792AB5B947A5Eb';
-let wmaticToken = '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270';
-let dystPair = '0xFec23508fE4b5d10A3eb0D83b9947CAa56F39463'; //sAMM-USDC/DAI +
-let dystRouter = '0xbE75Dd16D029c6B32B7aD57A0FD9C1c20Dd2862e'; //DystRouter01 +
-let gauge = '0x9c3Afbc9D0540168C6D4f3dA0F48FeBA6a3A7d2a'; //aka MasterChef +
-let dai = '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063';
+let dystPair = '0xFec23508fE4b5d10A3eb0D83b9947CAa56F39463'; //sAMM-USDC/DAI
+let dystRouter = '0xbE75Dd16D029c6B32B7aD57A0FD9C1c20Dd2862e'; //DystRouter01
+let gauge = '0x9c3Afbc9D0540168C6D4f3dA0F48FeBA6a3A7d2a'; //aka MasterChef
+let daiToken = '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063';
 
 module.exports = async () => {
     const strategy = await ethers.getContract("StrategyDystopiaDai");
 
-    await (await strategy.setTokens(POLYGON.usdc, dai, dystToken, wmaticToken)).wait();
+    await (await strategy.setTokens(POLYGON.usdc, daiToken, dystToken, POLYGON.wMatic)).wait();
     await (await strategy.setParams(gauge, dystPair, dystRouter, POLYGON.balancerVault, POLYGON.balancerPoolIdUsdcTusdDaiUsdt)).wait();
     await (await strategy.setPortfolioManager(core.pm)).wait();
 
