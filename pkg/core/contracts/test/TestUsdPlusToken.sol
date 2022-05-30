827e56d2d254d33eed5b70668e077a7f28c68ade
Mosin Nick <mosinnik@yandex.ru>
2022-05-30 23:33:43 +0300

    burn/transfer/transferFrom full fix

---
+++ pkg/core/contracts/test/TestUsdPlusToken.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../UsdPlusToken.sol";
+
+contract TestUsdPlusToken is UsdPlusToken {
+
+    function mintTest(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    function burnTest(address from, uint256 amount) external {
+        _burn(from, amount);
+    }
+}
