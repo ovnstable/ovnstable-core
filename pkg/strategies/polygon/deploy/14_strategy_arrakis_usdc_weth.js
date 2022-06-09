bfa76efeeb2f9b545d2744f20f4de15a8ac2ec1f
Nikita Slezkin <venikpapa@gmail.com>
2022-06-09 12:52:56 +0300

    add weth and wmatic

--- pkg/strategies/polygon/deploy/14_strategy_arrakis_usdc_weth.js
+++ pkg/strategies/polygon/deploy/14_strategy_arrakis_usdc_weth.js
@@ -1,9 +1,27 @@
-const {deployProxy} = require("../../../common/utils/deployProxy");
+const {deployProxy} = require("@overnight-contracts/common/utils/deployProxy");
 
 module.exports = async ({getNamedAccounts, deployments}) => {
+    const {deploy} = deployments;
+    const {deployer} = await getNamedAccounts();
     const {save} = deployments;
 
-    await deployProxy('StrategyArrakisWeth', deployments, save);
+    const strategyArrakisWethLibrary = await deploy("StrategyArrakisWethLibrary", {
+        from: deployer
+    });
+
+    const balancerLibrary = await deploy("BalancerLibrary", {
+        from: deployer
+    });
+
+    await deployProxy('StrategyArrakisWeth', deployments, save,
+        {
+            libraries: {
+                "StrategyArrakisWethLibrary": strategyArrakisWethLibrary.address,
+                "BalancerLibrary": balancerLibrary.address,
+            }
+        },
+        ["external-library-linking"]
+    );
 };
 
 module.exports.tags = ['base', 'StrategyArrakisWeth'];
