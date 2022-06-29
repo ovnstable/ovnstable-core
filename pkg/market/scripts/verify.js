d9162beba75bddc9442fe29524732aec47d7e51c
y.pavlov <lenvonder12vorh@gmail.com>
2022-06-29 13:58:53 +0300

    [Deploy] Market, Wrap USD+ to Polygon:prod

--- pkg/market/scripts/verify.js
+++ pkg/market/scripts/verify.js
@@ -1,7 +1,7 @@
 const { verify } = require("@overnight-contracts/common/utils/verify-utils");
 
 async function main() {
-    let items = ["Market"];
+    let items = ["Market", "WrappedUsdPlusToken"];
     await verify(items);
 }
 
