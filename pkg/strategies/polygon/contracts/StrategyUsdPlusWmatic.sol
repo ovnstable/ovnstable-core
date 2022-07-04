d4784e0318a258423fdaaec9f140161f9dd8202e
y.pavlov <lenvonder12vorh@gmail.com>
2022-07-04 15:53:29 +0300

    Stake/Unstake

--- pkg/strategies/polygon/contracts/StrategyUsdPlusWmatic.sol
+++ pkg/strategies/polygon/contracts/StrategyUsdPlusWmatic.sol
@@ -3,9 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
 import "./connectors/dystopia/interfaces/IDystopiaRouter.sol";
 import "./connectors/dystopia/interfaces/IDystopiaLP.sol";
 import "./connectors/aave/interfaces/IPriceFeed.sol";
@@ -15,6 +13,7 @@ import "./connectors/penrose/interface/IUserProxy.sol";
 import "./connectors/penrose/interface/IPenLens.sol";
 import "./libraries/WadRayMath.sol";
 import "./interfaces/IExchange.sol";
+import "./core/HedgeStrategy.sol";
 
 import {AaveBorrowLibrary} from "./libraries/AaveBorrowLibrary.sol";
 import {OvnMath} from "./libraries/OvnMath.sol";
@@ -23,11 +22,8 @@ import {DystopiaLibrary} from "./libraries/DystopiaLibrary.sol";
 
 import "hardhat/console.sol";
 
-contract StrategyUsdPlusWmatic is Initializable, AccessControlUpgradeable, UUPSUpgradeable {
+contract StrategyUsdPlusWmatic is HedgeStrategy {
     using WadRayMath for uint256;
-
-    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
-    bytes32 public constant EXCHANGER = keccak256("EXCHANGER");
 
     uint8 public constant E_MODE_CATEGORY_ID = 0;
     uint256 public constant INTEREST_RATE_MODE = 2;
@@ -35,9 +31,6 @@ contract StrategyUsdPlusWmatic is Initializable, AccessControlUpgradeable, UUPSU
     uint256 constant BASIS_POINTS_FOR_STORAGE = 100; // 1%
     uint256 constant BASIS_POINTS_FOR_SLIPPAGE = 4; // 0.04%
     uint256 constant MAX_UINT_VALUE = type(uint256).max;
-
-
-    address public exchanger;
 
     IExchange public exchange;
 
@@ -72,55 +65,14 @@ contract StrategyUsdPlusWmatic is Initializable, AccessControlUpgradeable, UUPSU
     uint256 public balancingDelta;
     uint256 public realHealthFactor;
 
-
-    event Reward(uint256 amount);
-    event Payout();
-
-    // ---  modifiers
-
-    modifier onlyAdmin() {
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Restricted to admins");
-        _;
-    }
-
-
-    // ---  constructor
-
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {}
 
     function initialize() initializer public {
-        __AccessControl_init();
-        __UUPSUpgradeable_init();
-
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(UPGRADER_ROLE, msg.sender);
-
-    }
-
-    function _authorizeUpgrade(address newImplementation)
-    internal
-    onlyRole(UPGRADER_ROLE)
-    override
-    {}
-
-
-    modifier onlyExchanger() {
-        require(hasRole(EXCHANGER, msg.sender), "Restricted to EXCHANGER");
-        _;
+        __Strategy_init();
     }
 
 
-    // --- Setters
-
-    function setExchanger(address _value) public onlyAdmin {
-        require(_value != address(0), "Zero address not allowed");
-
-        revokeRole(EXCHANGER, exchanger);
-        grantRole(EXCHANGER, _value);
-
-        exchanger = _value;
-    }
 
     function setTokens(
         address _usdc,
@@ -137,6 +89,7 @@ contract StrategyUsdPlusWmatic is Initializable, AccessControlUpgradeable, UUPSU
         wmaticDm = 10 ** IERC20Metadata(_wmatic).decimals();
 
         usdPlus = IERC20(_usdPlus);
+        setAsset(_usdPlus);
 
         penToken = IERC20(_penToken);
         dyst = IERC20(_dyst);
@@ -182,82 +135,69 @@ contract StrategyUsdPlusWmatic is Initializable, AccessControlUpgradeable, UUPSU
         balancingDelta = _balancingDelta * 10 ** 15;
     }
 
-    function stake(uint256 _amount) external onlyExchanger returns (uint256)  {
+    function _stake(uint256 _amount) internal override  {
 
         _borrowWmatic();
         _stakeDystopiaToPenrose();
-
-        return _amount;
     }
 
 
-    function unstake(
+    function _unstake(
         uint256 _amount
-    ) external onlyExchanger returns (uint256) {
+    ) internal override returns (uint256) {
 
-        // 1. Recalculate target amount and decreese usdcStorage proportionately.
-        uint256 amount = OvnMath.subBasisPoints(_amount, BASIS_POINTS_FOR_STORAGE);
-        usdcStorage = usdcStorage - (_amount - amount);
-        amount += 10;
+        uint256 amount = _amount + ((_amount * exchange.redeemFee()) / exchange.redeemFeeDenominator()) ;
 
         (uint256 reserveWmatic, uint256 reserveUsdPlus,) = dystVault.getReserves();
 
-
         uint256 wmaticBorrow = AaveBorrowLibrary.getBorrowForWithdraw(
             amount,
-            reserveWmatic,
             reserveUsdPlus,
+            reserveWmatic,
             liquidationThreshold,
             healthFactor,
-            wmaticDm,
             usdcDm,
-            uint256(oracleWmatic.latestAnswer()),
-            uint256(oracleUsdc.latestAnswer())
+            wmaticDm,
+            uint256(oracleUsdc.latestAnswer()),
+            uint256(oracleWmatic.latestAnswer())
         );
 
-
-        console.log('Reserve WMATIC      %s', reserveWmatic / 1e18);
-        console.log('Reserve USD+        %s', reserveUsdPlus / 1e6);
-        console.log('Amount              %s', amount / 1e6);
-        console.log('UsdcStorage         %s', usdcStorage );
-        console.log('wmaticBorrow        %s', wmaticBorrow );
-
-        // 3. Removing liquidity for aave calculation
         IPool aave = _aavePool();
         (, uint256 borrow,,,,) = aave.getUserAccountData(address(this));
-        uint256 totalBorrowUsd1 = AaveBorrowLibrary.convertUsdToTokenAmount(borrow, wmaticDm, uint256(oracleWmatic.latestAnswer()));
+        uint256 wmaticBorrowNative = AaveBorrowLibrary.convertUsdToTokenAmount(borrow, wmaticDm, uint256(oracleWmatic.latestAnswer()));
 
-        console.log('Borrow:          %s', borrow);
-        console.log('totalBorrowUsd1: %s', totalBorrowUsd1);
-        console.log('wmaticBorrow:    %s', wmaticBorrow);
+        uint256 borrowAmount;
+        uint256 getUsdc;
 
-        if (wmaticBorrow > totalBorrowUsd1) {
-            uint256 amountLp = _getLiquidityForToken(totalBorrowUsd1);
-            penProxy.unstakeLpAndWithdraw(address(dystVault), amountLp);
-            dystVault.approve(address(dystRouter), amountLp);
-
-            this._removeLiquidityAndUnstakeWithSlippage(amountLp);
-
-            wmatic.approve(address(aave), token0.balanceOf(address(this)));
-            aave.repay(address(token0), MAX_UINT_VALUE, interestRateMode, address(this));
-            aave.withdraw(address(usdcToken), MAX_UINT_VALUE, address(this));
+        if (wmaticBorrow > wmaticBorrowNative) {
+            borrowAmount = wmaticBorrowNative;
+            getUsdc = MAX_UINT_VALUE;
         } else {
-            uint256 amountLp = this._getLiquidityForToken(token0Borrow);
-            userProxy.unstakeLpAndWithdraw(address(dystVault), amountLp);
-            dystVault.approve(address(dystRouter), amountLp);
-            this._removeLiquidityAndUnstakeWithSlippage(amountLp);
-            token0.approve(address(aave), token0.balanceOf(address(this)));
-            aave.repay(address(token0), token0.balanceOf(address(this)), interestRateMode, address(this));
-            uint256 getusdc = amount - (token0Borrow * amount0Current) / amount1Current;
-            aave.withdraw(address(usdcToken), getusdc, address(this));
+            borrowAmount = wmaticBorrow;
+            getUsdc = amount - (wmaticBorrow * reserveUsdPlus) / reserveWmatic;
         }
 
-        return 0;
+        uint256 amountLp = _getLiquidityForToken(borrowAmount);
+        penProxy.unstakeLpAndWithdraw(address(dystVault), amountLp);
+        dystVault.approve(address(dystRouter), amountLp);
+        _removeLiquidity(amountLp);
+
+
+        wmatic.approve(address(aave), wmatic.balanceOf(address(this)));
+        aave.repay(address(wmatic), wmatic.balanceOf(address(this)), INTEREST_RATE_MODE, address(this));
+        aave.withdraw(address(usdc), getUsdc, address(this));
+
+        usdc.approve(address(exchange), usdc.balanceOf(address(this)));
+        exchange.buy(address(usdc), usdc.balanceOf(address(this)));
+
+        _showBalances();
+        return usdPlus.balanceOf(address(this));
     }
 
 
-    function _removeLiquidity() internal {
+    function _removeLiquidity(uint256 amountLp) internal {
 
+        (uint256 amountLiq0, uint256 amountLiq1) = _getLiquidity( amountLp);
         (uint256 amount0, uint256 amount1) = dystRouter.removeLiquidity(
             address(wmatic),
             address(usdPlus),
@@ -265,7 +205,7 @@ contract StrategyUsdPlusWmatic is Initializable, AccessControlUpgradeable, UUPSU
             amountLp,
             (amountLiq0 == 0) ? 0 : OvnMath.subBasisPoints(amountLiq0, BASIS_POINTS_FOR_SLIPPAGE),
             (amountLiq1 == 0) ? 0 : OvnMath.subBasisPoints(amountLiq1, BASIS_POINTS_FOR_SLIPPAGE),
-            address(self),
+            address(this),
             block.timestamp + 600
         );
 
@@ -365,9 +305,9 @@ contract StrategyUsdPlusWmatic is Initializable, AccessControlUpgradeable, UUPSU
 
     function _showBalances() internal {
         console.log('---Balances---');
-        console.log('USDC     %s', usdc.balanceOf(address(this)) / 1e6);
-        console.log('USD+     %s', usdPlus.balanceOf(address(this)) / 1e6);
-        console.log('WMATIC   %s', wmatic.balanceOf(address(this)) / 1e18);
+        console.log('USDC     %s', usdc.balanceOf(address(this)));
+        console.log('USD+     %s', usdPlus.balanceOf(address(this)));
+        console.log('WMATIC   %s', wmatic.balanceOf(address(this)));
         console.log('Dystopia %s', dystVault.balanceOf(address(this)));
 
         address userProxyThis = penLens.userProxyByAccount(address(this));
@@ -378,7 +318,7 @@ contract StrategyUsdPlusWmatic is Initializable, AccessControlUpgradeable, UUPSU
 
     }
 
-    function netAssetValue() public view returns (uint256){
+    function netAssetValue() external view override returns (uint256){
 
         address userProxyThis = penLens.userProxyByAccount(address(this));
         address stakingAddress = penLens.stakingRewardsByDystPool(address(dystVault));
@@ -437,7 +377,7 @@ contract StrategyUsdPlusWmatic is Initializable, AccessControlUpgradeable, UUPSU
 
 
 
-    function claimRewards() external onlyExchanger {
+    function _claimRewards(address _to) internal override returns (uint256){
 
         // claim rewards
         penProxy.claimStakingRewards();
@@ -475,14 +415,25 @@ contract StrategyUsdPlusWmatic is Initializable, AccessControlUpgradeable, UUPSU
             totalUsdc += penUsdc;
         }
 
-        emit Reward(totalUsdc);
+        return totalUsdc;
     }
 
 
-    function balance() external onlyExchanger{
+    function _balance() internal override returns (uint256) {
 
-        // Balance HF
+        IPool aave = _aavePool();
+        (,,,,,uint256 healthFactorCurrent) = aave.getUserAccountData(address(this));
+
+        if (healthFactorCurrent > 10 ** 20 || OvnMath.abs(healthFactorCurrent, healthFactor) < balancingDelta) {
+            return healthFactorCurrent;
+        }
+
+        if (healthFactorCurrent > healthFactor) {
+
+        } else {
+        }
+
+        return healthFactorCurrent;
+
     }
-
-
 }
