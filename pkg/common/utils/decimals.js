7ab5b2f9d7840f65f434d90058ad142eb67fdb85
y.pavlov <lenvonder12vorh@gmail.com>
2022-07-27 11:27:26 +0300

    [Scripts] Update decimals.js

--- pkg/common/utils/decimals.js
+++ pkg/common/utils/decimals.js
@@ -1,33 +1,48 @@
 const BigNumber = require('bignumber.js');
 
+
+
+function toE18(value){
+    return new BigNumber(value).times(new BigNumber(10).pow(18)).toFixed(0)
+
+}
+
+function fromE18(value){
+    return new BigNumber(value).div(new BigNumber(10).pow(18)).toFixed(4)
+}
+
+function toE6(value){
+    return value * 10 ** 6;
+}
+
+function fromE6(value){
+    return  value / 10 ** 6;
+}
+
+function toAsset(value){
+    if (process.env.ETH_NETWORK === 'BSC'){
+        return toE18(value);
+    }else {
+        return toE6(value);
+    }
+}
+
+function fromAsset(value){
+    if (process.env.ETH_NETWORK === 'BSC'){
+        return this.fromE18(value);
+    }else {
+        return this.fromE18(value);
+    }
+}
+
 module.exports = {
 
-    toE18: (value) => {
-        return new BigNumber(value).times(new BigNumber(10).pow(18)).toFixed(0)
-    },
-    fromE18: (value) => {
-        return new BigNumber(value).div(new BigNumber(10).pow(18)).toFixed(4)
-    },
+    toE18: toE18,
+    fromE18: fromE18,
 
-    toE6: (value) => value * 10 ** 6,
-    fromE6: (value) => value / 10 ** 6,
+    toE6: toE6,
+    fromE6: fromE6,
 
-
-    toAsset: (value) =>{
-
-        if (process.env.ETH_NETWORK === 'BSC'){
-            return this.toE18(value);
-        }else {
-            return this.toE6(value);
-        }
-    },
-
-    fromAsset: (value) =>{
-
-        if (process.env.ETH_NETWORK === 'BSC'){
-            return this.fromE18(value);
-        }else {
-            return this.fromE18(value);
-        }
-    }
+    toAsset:toAsset,
+    fromAsset: fromAsset,
 }
