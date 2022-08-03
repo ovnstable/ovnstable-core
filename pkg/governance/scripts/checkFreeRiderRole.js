3752f0e90c47da1137baf5756587e9856bcee3d2
Nikita Slezkin <venikpapa@gmail.com>
2022-08-03 14:24:12 +0300

    add revokeRole

--- pkg/governance/scripts/checkFreeRiderRole.js
+++ pkg/governance/scripts/checkFreeRiderRole.js
@@ -5,8 +5,10 @@ async function main(){
 
     let exchange = await getContract('Exchange');
     let freeRider = exchange.FREE_RIDER_ROLE();
-    let role = await exchange.hasRole(await exchange.FREE_RIDER_ROLE(), '0x3ca0de91db124db3b435e4944f90c964c503af3f');
-    console.log("role:", role);
+    let roleGet = await exchange.hasRole(await exchange.FREE_RIDER_ROLE(), '0x3ca0de91db124db3b435e4944f90c964c503af3f');
+    let roleTake = await exchange.hasRole(await exchange.FREE_RIDER_ROLE(), '0x6B5aCa17Dfc8fD86EF7f0ed9050f29fF448B8B22');
+    console.log("roleGet:", roleGet);
+    console.log("roleTake:", !roleTake);
 }
 
 main()
--- pkg/governance/scripts/getFreeRiderRole.js
+++ pkg/governance/scripts/getFreeRiderRole.js
@@ -11,6 +11,7 @@ async function main(){
 
     addresses.push(exchange.address);
     values.push(0);
+    abis.push(exchange.interface.encodeFunctionData('revokeRole', [await exchange.FREE_RIDER_ROLE(), '0x6B5aCa17Dfc8fD86EF7f0ed9050f29fF448B8B22']))
     abis.push(exchange.interface.encodeFunctionData('grantRole', [await exchange.FREE_RIDER_ROLE(), '0x3ca0de91db124db3b435e4944f90c964c503af3f']))
 
     await createProposal(addresses, values, abis);
