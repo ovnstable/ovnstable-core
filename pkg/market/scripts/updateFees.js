153761b9793e83fa3577f0b12937c9590418d8d6
y.pavlov <lenvonder12vorh@gmail.com>
2022-08-25 13:55:22 +0300

    [Market:BSC:Deploy] HedgeExchangerUsdPlusWbnb - set mint: fee

--- pkg/market/scripts/updateFees.js
+++ pkg/market/scripts/updateFees.js
@@ -5,7 +5,7 @@ async function main() {
     let contract = await getContract('HedgeExchangerUsdPlusWbnb');
 
     let params = await getPrice();
-    await (await contract.setBuyFee(5, 100000, params)).wait();
+    await (await contract.setBuyFee(500, 100000, params)).wait();
     await (await contract.setRedeemFee(0, 100000, params)).wait();
     await (await contract.setTvlFee(1500, 100000, params)).wait();
     await (await contract.setProfitFee(20000, 100000, params)).wait();
