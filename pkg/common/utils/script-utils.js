c67fc68478137a7a323daee34a5bc6c30bc026e6
y.pavlov <lenvonder12vorh@gmail.com>
2022-08-31 12:04:24 +0300

    Update script-utils.js

--- pkg/common/utils/script-utils.js
+++ pkg/common/utils/script-utils.js
@@ -303,7 +303,7 @@ async function getPrice(){
     else if(process.env.ETH_NETWORK === 'AVALANCHE')
         params.gasLimit = 8000000;
     else if (process.env.ETH_NETWORK === 'BSC'){
-        params = {gasPrice: "5000000000", gasLimit:  8000000}; // BSC gasPrice always 5 GWEI
+        params = {gasPrice: "5000000000", gasLimit:  15000000}; // BSC gasPrice always 5 GWEI
     }else if (process.env.ETH_NETWORK === "OPTIMISM"){
         params = {gasPrice: "1000000", gasLimit: 8000000}; // gasPrice 0.001
     }
