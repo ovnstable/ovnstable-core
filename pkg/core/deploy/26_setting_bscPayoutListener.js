e364e0ccdb08d1028114b5c2da799f0dabeaf8cf
y.pavlov <lenvonder12vorh@gmail.com>
2022-09-21 14:56:16 +0300

    [BSC:Sync] Add TIGAR/USD+

--- pkg/core/deploy/26_setting_bscPayoutListener.js
+++ pkg/core/deploy/26_setting_bscPayoutListener.js
@@ -17,6 +17,7 @@ module.exports = async ({getNamedAccounts, deployments}) => {
         "0x30f96ad4856d7e699963b589591f03710976a6e8",  // MDB+/USD+
         "0x0f8c95890b7bdecfae7990bf32f22120111e0b44",  // TETU/USD+
         "0x0fe6cf7a2687c5bddf302c5ccea901ba0bf71816",  // USD+/BUSD
+        "0xdee33737634bb7612c15b10488819e88fd62f0f9",  // TIGAR/USD+
     ]
 
     await (await pl.setQsSyncPools(pools)).wait();
