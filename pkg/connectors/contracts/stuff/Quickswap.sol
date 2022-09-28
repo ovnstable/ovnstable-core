4c6fdf760c3a584515dce903ec883e9c73a30821
Naran <95479946+4yuka@users.noreply.github.com>
2022-09-28 10:44:20 +0300

    Fix claiming in StrategyQsWmaticUsdc (#107)

--- pkg/connectors/contracts/stuff/Quickswap.sol
+++ pkg/connectors/contracts/stuff/Quickswap.sol
@@ -2,6 +2,7 @@
 
 pragma solidity >=0.8.0 <0.9.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IStakingDualRewards {
     // Views
@@ -27,4 +28,25 @@ interface IStakingDualRewards {
     function getReward() external;
 
     function exit() external;
+}
+
+
+interface IDragonLair is IERC20 {
+
+    function quick() external;
+
+    // Enter the lair. Pay some QUICK. Earn some dragon QUICK.
+    function enter(uint256 _quickAmount) external;
+
+    // Leave the lair. Claim back your QUICK.
+    function leave(uint256 _dQuickAmount) external;
+
+    // returns the total amount of QUICK an address has in the contract including fees earned
+    function QUICKBalance(address _account) external view returns (uint256 quickAmount_);
+
+    //returns how much QUICK someone gets for depositing dQUICK
+    function dQUICKForQUICK(uint256 _dQuickAmount) external view returns (uint256 quickAmount_);
+
+    //returns how much dQUICK someone gets for depositing QUICK
+    function QUICKForDQUICK(uint256 _quickAmount) external view returns (uint256 dQuickAmount_);
 }
