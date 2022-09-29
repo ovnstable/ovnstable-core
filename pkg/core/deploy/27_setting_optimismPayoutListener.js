bb93d7d850f41a044d21e441f7494bad4c3ff472
y.pavlov <lenvonder12vorh@gmail.com>
2022-09-29 09:45:59 +0300

    [PayoutListener:Optimism] Add pair to skim: USD+/LUSD

--- pkg/core/deploy/27_setting_optimismPayoutListener.js
+++ pkg/core/deploy/27_setting_optimismPayoutListener.js
@@ -11,11 +11,13 @@ module.exports = async ({getNamedAccounts, deployments}) => {
     await (await pl.setTokens(usdPlus.address)).wait();
 
     let velodromePools = [
-        '0x67124355cce2ad7a8ea283e990612ebe12730175'
+        '0x67124355cce2ad7a8ea283e990612ebe12730175', // USD+/USDC
+        '0x8a9Cd3dce710e90177B4332C108E159a15736A0F', // USD+/LUSD
     ];
 
     let velodromeBribes= [
-        '0xfd42e5d79567997552986fc8d96434e8a2ad9fb6'
+        '0xfd42e5d79567997552986fc8d96434e8a2ad9fb6', // USD+/USDC
+        '0x41a7540ec8cb3afafe16a834abe0863f22016ec0', // USD+/LUSD
     ];
     await (await pl.setVelodromePools(velodromePools, velodromeBribes)).wait();
 
