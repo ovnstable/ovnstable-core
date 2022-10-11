73f547651aac3e3ed7ee2b889fbd5003809522df
Yaroslav <lenvonder12vorh@gmail.com>
2022-10-11 22:06:56 +0300

    Add KyberSwapUsdcUsdt (#114)

--- pkg/strategies/polygon/deploy/30_strategy_kyberswap_usdc_usdt.js
+++ pkg/strategies/polygon/deploy/30_strategy_kyberswap_usdc_usdt.js
@@ -2,17 +2,17 @@ const {deployProxy} = require("@overnight-contracts/common/utils/deployProxy");
 const {POLYGON} = require('@overnight-contracts/common/utils/assets');
 const {deploySection, settingSection} = require("@overnight-contracts/common/utils/script-utils");
 
-let kncToken = '0x22a31bD4cB694433B6de19e0aCC2899E553e9481';
-let ldoToken = '0x22a31bD4cB694433B6de19e0aCC2899E553e9481';
+let kncToken = '0x1C954E8fe737F99f68Fa1CCda3e51ebDB291948C';
 let basePositionManager = '0x2B1c7b41f6A8F2b2bc45C3233a5d5FB3cD6dC9A8';
-let kyberSwapElasticLM = '0x5C503D4b7DE0633f031229bbAA6A5e4A31cc35d8';
+let kyberSwapElasticLM = '0xbdec4a045446f583dc564c0a227ffd475b329bf0';
 let reinvestmentToken = '0xF9cC934753a127100585812181Ac04d07158A4C2';
-let pid = 6;
+let pid = 8;
 let synapseSwapRouter = '0x85fCD7Dd0a1e1A9FCD5FD886ED522dE8221C3EE5';
-let kyberSwapRouter = '0xC1e7dFE73E1598E3910EF4C7845B68A9Ab6F4c83';
+let poolUsdcKnc = '0xCBD258f33B7A2705e8418708a4F615C43fedf23c';
 let poolFeeUsdcUsdtInUnits = 8; // 0.008%
-let poolFeeKncUsdc = 400; // 0.04%
-let poolFeeLdoUsdc = 3000; // 0.3%
+
+let tickLower = -10;
+let tickUpper = 10;
 
 module.exports = async ({deployments}) => {
     const {save} = deployments;
@@ -27,19 +27,17 @@ module.exports = async ({deployments}) => {
                 usdcToken: POLYGON.usdc,
                 usdtToken: POLYGON.usdt,
                 kncToken: kncToken,
-                ldoToken: ldoToken,
                 basePositionManager: basePositionManager,
                 elasticLM: kyberSwapElasticLM,
                 reinvestmentToken: reinvestmentToken,
                 pid: pid,
                 synapseSwapRouter: synapseSwapRouter,
-                kyberSwapRouter: kyberSwapRouter,
-                uniswapV3Router: POLYGON.uniswapV3Router,
+                poolUsdcKnc: poolUsdcKnc,
                 poolFeeUsdcUsdtInUnits: poolFeeUsdcUsdtInUnits,
-                poolFeeKncUsdc: poolFeeKncUsdc,
-                poolFeeLdoUsdc: poolFeeLdoUsdc,
                 oracleUsdc: POLYGON.oracleChainlinkUsdc,
-                oracleUsdt: POLYGON.oracleChainlinkUsdt
+                oracleUsdt: POLYGON.oracleChainlinkUsdt,
+                tickLower: tickLower,
+                tickUpper: tickUpper
             }
         )).wait();
     });
