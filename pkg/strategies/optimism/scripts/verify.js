1d60be778e4a6286a5cde39d1b3f073f996e7bd8
Naran Mucherinov <n.mucherinov@gmail.com>
2022-10-11 15:42:34 +0600

    Deploy StrategyReaperSonneDai and StrategyReaperSonneUsdc

--- pkg/strategies/optimism/scripts/verify.js
+++ pkg/strategies/optimism/scripts/verify.js
@@ -1,7 +1,7 @@
 const {verify } = require("@overnight-contracts/common/utils/verify-utils");
 
 async function main() {
-    let items = ["StrategyAave","StrategySynapseUsdc","StrategyVelodromeUsdcDai"];
+    let items = ["StrategyReaperSonneUsdc","StrategyReaperSonneDai"];
     await verify(items);
 }
 
