ab93dfe55b2a466298e44ba5feb356271e0b09ae
y.pavlov <lenvonder12vorh@gmail.com>
2022-10-14 12:04:01 +0300

    [Deploy:Optimism] StrategyReaperSonneUsdt

--- pkg/core/scripts/set-strategies-optimism.js
+++ pkg/core/scripts/set-strategies-optimism.js
@@ -19,7 +19,7 @@ async function main() {
             "strategy": "0x9520aEF41161f09Dce78a8e79482b654d4FFe641",
             "name": "Pika USDC",
             "minWeight": 0,
-            "targetWeight": 38.5,
+            "targetWeight": 38,
             "maxWeight": 100,
             "enabled": false,
             "enabledReward": true
@@ -28,7 +28,7 @@ async function main() {
             "strategy": "0x6C93A2A9eBC61ce664eE3D44531B76365150BFd8",
             "name": "Rubicon USDC",
             "minWeight": 0,
-            "targetWeight": 10.5,
+            "targetWeight": 11,
             "maxWeight": 100,
             "enabled": false,
             "enabledReward": true
@@ -38,7 +38,7 @@ async function main() {
             "strategy": "0x2c80d9ee6f42a9AF2f681fE569AB409Df3aa46f7",
             "name": "Rubicon USDT",
             "minWeight": 0,
-            "targetWeight": 11,
+            "targetWeight": 10,
             "maxWeight": 100,
             "enabled": false,
             "enabledReward": true
@@ -49,7 +49,7 @@ async function main() {
             "strategy": "0xfa5328a029575f460d9fb499B1cDCE25b69B1038",
             "name": "Rubicon DAI",
             "minWeight": 0,
-            "targetWeight": 5,
+            "targetWeight": 9,
             "maxWeight": 100,
             "enabled": false,
             "enabledReward": true
@@ -58,7 +58,7 @@ async function main() {
             "strategy": "0x2B65fb73A3fB0E738BBE0726754801BB422fad6d",
             "name": "Beethovenx Usdc",
             "minWeight": 0,
-            "targetWeight": 22.5,
+            "targetWeight": 14,
             "maxWeight": 100,
             "enabled": false,
             "enabledReward": true
@@ -68,7 +68,7 @@ async function main() {
             "strategy": "0xd27a3640CDF245f97b739F20605e66a79160361B",
             "name": "Reaper Sonne DAI",
             "minWeight": 0,
-            "targetWeight": 5,
+            "targetWeight": 8,
             "maxWeight": 100,
             "enabled": true,
             "enabledReward": true
@@ -77,7 +77,16 @@ async function main() {
             "strategy": "0xF3a75b6dD1A60526755A370afaB48fF3501e0D15",
             "name": "Reaper Sonne USDC",
             "minWeight": 0,
-            "targetWeight": 5,
+            "targetWeight": 5.5,
+            "maxWeight": 100,
+            "enabled": true,
+            "enabledReward": true
+        },
+        {
+            "strategy": "0x8855e27ABE0C121c5B9e1D1572B40cab184daf21",
+            "name": "Reaper Sonne USDT",
+            "minWeight": 0,
+            "targetWeight": 2,
             "maxWeight": 100,
             "enabled": true,
             "enabledReward": true
@@ -86,19 +95,14 @@ async function main() {
 
 
 
-    let exchange = await getContract('Exchange' );
-    let pm = await getContract('PortfolioManager');
-    let strategy = await getContract('StrategyBeethovenxUsdc');
-
-
     await execTimelock(async (timelock)=>{
+        let pm = await getContract('PortfolioManager');
 
         await showM2M();
 
         weights = await convertWeights(weights);
 
-        await pm.connect(timelock).addStrategy('0xd27a3640CDF245f97b739F20605e66a79160361B');
-        await pm.connect(timelock).addStrategy('0xF3a75b6dD1A60526755A370afaB48fF3501e0D15');
+        await pm.connect(timelock).addStrategy('0x8855e27ABE0C121c5B9e1D1572B40cab184daf21');
         await pm.connect(timelock).grantRole(await pm.PORTFOLIO_AGENT_ROLE(), timelock.address);
         await pm.connect(timelock).setStrategyWeights(weights);
         await pm.connect(timelock).balance();
