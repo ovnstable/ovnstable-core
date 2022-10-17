4056f60d661ca1ac5f3812222db1e97386a3674e
y.pavlov <lenvonder12vorh@gmail.com>
2022-10-17 14:31:14 +0300

    [Ref] Remove unused files

--- ready-node.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-rm -rf deployments/localhost
-cp -r deployments/polygon_dev deployments/localhost
-cp -r artifacts-external/ artifacts/
-
-echo 31337 > deployments/localhost/.chainId
