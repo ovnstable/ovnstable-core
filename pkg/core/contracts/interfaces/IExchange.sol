bf568c931342f478a501d8fda5f338ed32407043
Naran Mucherinov <n.mucherinov@gmail.com>
2022-10-17 19:06:44 +0600

    Add StrategyUsdPlusDai

--- pkg/core/contracts/interfaces/IExchange.sol
+++ pkg/core/contracts/interfaces/IExchange.sol
@@ -3,6 +3,12 @@ pragma solidity >=0.8.0 <0.9.0;
 
 interface IExchange {
 
+    struct MintParams {
+        address asset;   // USDC | BUSD depends at chain
+        uint256 amount;  // amount asset
+        string referral; // code from Referral Program -> if not have -> set empty
+    }
+
     function buyFee() external view returns (uint256);
 
     function buyFeeDenominator() external view returns (uint256);
@@ -12,6 +18,10 @@ interface IExchange {
     function redeemFeeDenominator() external view returns (uint256);
 
     function balance() external view returns (uint256);
+
+    // Minting USD+ in exchange for an asset
+
+    function mint(MintParams calldata params) external returns (uint256);
 
     /**
      * @param _asset Asset to spend
---
+++ pkg/strategies/optimism/deploy/13_strategy_usd_plus_dai.js
@@ -0,0 +1,32 @@
+const {deployProxy} = require("@overnight-contracts/common/utils/deployProxy");
+const {deploySection, settingSection} = require("@overnight-contracts/common/utils/script-utils");
+const {OPTIMISM} = require("@overnight-contracts/common/utils/assets");
+
+let usdPlus = '0x73cb180bf0521828d8849bc8CF2B920918e23032';
+let exchange = '0xe80772Eaf6e2E18B651F160Bc9158b2A5caFCA65';
+let poolUsdcDaiFee = 100; // 0.01%
+
+module.exports = async ({deployments}) => {
+    const {save} = deployments;
+
+    await deploySection(async (name) => {
+        await deployProxy(name, deployments, save);
+    });
+
+    await settingSection(async (strategy) => {
+        await (await strategy.setParams(
+            {
+                daiToken: OPTIMISM.dai,
+                usdcToken: OPTIMISM.usdc,
+                usdPlus: usdPlus,
+                exchange: exchange,
+                oracleUsdc: OPTIMISM.oracleUsdc,
+                oracleDai: OPTIMISM.oracleDai,
+                uniswapV3Router: OPTIMISM.uniswapV3Router,
+                poolUsdcDaiFee: poolUsdcDaiFee,
+            }
+        )).wait();
+    });
+};
+
+module.exports.tags = ['StrategyUsdPlusDai'];
