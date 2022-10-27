a3a15c28173b566878fa88cfaefeab23d5bc876e
y.pavlov <lenvonder12vorh@gmail.com>
2022-10-27 13:38:50 +0300

    Update script: print_weights.js

--- pkg/core/scripts/print_weights.js
+++ pkg/core/scripts/print_weights.js
@@ -1,4 +1,4 @@
-const {getContract} = require("@overnight-contracts/common/utils/script-utils");
+const {getContract, getStrategyMapping} = require("@overnight-contracts/common/utils/script-utils");
 const {createProposal} = require("@overnight-contracts/common/utils/governance");
 
 async function main() {
@@ -8,14 +8,21 @@ async function main() {
 
     let weights = await pm.getAllStrategyWeights();
 
+    let strategiesMapping = await getStrategyMapping();
+
+
     let items = [];
 
     for (let i = 0; i < weights.length; i++) {
         let weight = weights[i];
 
+
+        let mapping = strategiesMapping.find(value => value.address === weight.strategy);
+
+
         let item = {
             "strategy": weight.strategy,
-            "name": "-",
+            "name": mapping ? mapping.name : 'Not found',
             "minWeight": weight.minWeight / 1000,
             "targetWeight": weight.targetWeight / 1000,
             "maxWeight": weight.maxWeight / 1000,
