c335767351cf505076970d45d243b0851a67a450
y.pavlov <lenvonder12vorh@gmail.com>
2022-11-02 21:34:30 +0300

    Deploy PolygonPayoutListener: SushiSkim

--- pkg/core/scripts/exec-proposal.js
+++ pkg/core/scripts/exec-proposal.js
@@ -1,23 +1,27 @@
 const hre = require("hardhat");
 const fs = require("fs");
 
-const {showM2M, getContract, getPrice} = require("@overnight-contracts/common/utils/script-utils");
+const {showM2M, getContract, getPrice, getCoreAsset} = require("@overnight-contracts/common/utils/script-utils");
 const {evmCheckpoint, evmRestore} = require("@overnight-contracts/common/utils/sharedBeforeEach");
+const {execProposal} = require("@overnight-contracts/common/utils/governance");
+const {toAsset} = require("@overnight-contracts/common/utils/decimals");
 
 async function main() {
 
-    let governor = await getContract('OvnGovernor');
 
-    await evmCheckpoint('Before');
-    await showM2M();
-    let opts = await getPrice();
-    opts.gasLimit = "15000000"
-    await (await governor.executeExec(
-        "103592307014576708617364387971453183776614942898173418078661203019478279010206",
-        opts
-    )).wait();
-    await showM2M();
-    await evmRestore('Before');
+    await execProposal('8183587490322514041677079120766614478739037332802062240372913363775024669755');
+
+
+    let exchange = await getContract('Exchange');
+    let asset = await getCoreAsset();
+
+    await (await exchange.pause()).wait();
+    await (await exchange.unpause()).wait();
+
+    await (await asset.approve(exchange.address, toAsset(1))).wait();
+    console.log('Asset approve done');
+    await (await exchange.buy(asset.address, toAsset(1))).wait();
+    console.log('Exchange.buy done');
 }
 
 
