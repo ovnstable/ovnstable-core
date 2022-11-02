52a16b5c273947c75b7ec0f857a8003585fbd686
y.pavlov <lenvonder12vorh@gmail.com>
2022-11-03 02:11:38 +0300

    Add pause.js

---
+++ pkg/core/scripts/pause.js
@@ -0,0 +1,29 @@
+const {toAsset, toE6} = require("@overnight-contracts/common/utils/decimals");
+
+const {
+    getContract,
+    getCoreAsset,
+} = require("@overnight-contracts/common/utils/script-utils");
+
+
+async function main() {
+
+    let exchange = await getContract('Exchange');
+    let asset = await getCoreAsset();
+
+    await (await exchange.pause()).wait();
+    console.log('Pause done()');
+
+    await (await exchange.buy(asset.address, toAsset(1))).wait();
+    await (await exchange.redeem(asset.address, toE6(5))).wait();
+
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
