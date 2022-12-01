67e231b0241727561785bf72d1c9491baee6fe2a
y.pavlov <lenvonder12vorh@gmail.com>
2022-12-01 13:02:27 +0300

    Update weights

--- pkg/core/scripts/set-strategies-polygon-ins.js
+++ pkg/core/scripts/set-strategies-polygon-ins.js
@@ -8,8 +8,8 @@ async function main() {
             "strategy": "0x82d8F924b71459bAC871A9F0163d73B6a3FBbb10",
             "name": "Aave",
             "minWeight": 0,
-            "targetWeight": 100,
-            "maxWeight": 100,
+            "targetWeight": 10,
+            "maxWeight": 15,
             "riskFactor": 0,
             "enabled": true,
             "enabledReward": true
@@ -18,20 +18,20 @@ async function main() {
             "strategy": "0xDA31BeD342D68a553Cf8d14231B8784b2A0aEc74",
             "name": "Gamma",
             "minWeight": 0,
-            "targetWeight": 0,
+            "targetWeight": 45,
             "maxWeight": 100,
             "riskFactor": 0,
-            "enabled": false,
+            "enabled": true,
             "enabledReward": true
         },
         {
             "strategy": "0xe9c0c12D1e6F47b9338fF5E6Be894Fbbd454c68B",
             "name": "Alfa",
             "minWeight": 0,
-            "targetWeight": 0,
+            "targetWeight": 45,
             "maxWeight": 100,
             "riskFactor": 0,
-            "enabled": false,
+            "enabled": true,
             "enabledReward": true
         },
     ]
