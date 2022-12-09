cfdb1eaa536ca917d5f0aa125eba30bbaeb1ed84
y.pavlov <lenvonder12vorh@gmail.com>
2022-12-09 19:11:21 +0300

    Update exec-proposal.js

--- pkg/core/scripts/exec-proposal.js
+++ pkg/core/scripts/exec-proposal.js
@@ -1,27 +1,11 @@
-const hre = require("hardhat");
-const fs = require("fs");
-
-const {showM2M, getContract, getPrice, getCoreAsset} = require("@overnight-contracts/common/utils/script-utils");
-const {evmCheckpoint, evmRestore} = require("@overnight-contracts/common/utils/sharedBeforeEach");
-const {execProposal} = require("@overnight-contracts/common/utils/governance");
-const {toAsset} = require("@overnight-contracts/common/utils/decimals");
+const {getContract, getPrice, initWallet} = require("@overnight-contracts/common/utils/script-utils");
 
 async function main() {
 
-
-    await execProposal('8183587490322514041677079120766614478739037332802062240372913363775024669755');
-
-
-    let exchange = await getContract('Exchange');
-    let asset = await getCoreAsset();
-
-    await (await exchange.pause()).wait();
-    await (await exchange.unpause()).wait();
-
-    await (await asset.approve(exchange.address, toAsset(1))).wait();
-    console.log('Asset approve done');
-    await (await exchange.buy(asset.address, toAsset(1))).wait();
-    console.log('Exchange.buy done');
+    let wallet = await initWallet();
+    let price = await getPrice();
+    let governor = await getContract('OvnGovernor');
+    await (await governor.connect(wallet).executeExec('14492553784002505925514334695647669692763329380932899759405808384903001089874', price)).wait();
 }
 
 
