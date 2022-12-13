a65550f16768eca12794d6174019dded3c009c17
y.pavlov <lenvonder12vorh@gmail.com>
2022-12-13 22:52:24 +0300

    Deploy Exchange.json

--- pkg/core/scripts/upgradeExchange.js
+++ pkg/core/scripts/upgradeExchange.js
@@ -1,5 +1,5 @@
 const {getContract} = require("@overnight-contracts/common/utils/script-utils");
-const {createProposal} = require("@overnight-contracts/common/utils/governance");
+const {createProposal, execProposal} = require("@overnight-contracts/common/utils/governance");
 
 async function main() {
 
@@ -9,12 +9,25 @@ async function main() {
     let values = [];
     let abis = [];
 
+    let PORTFOLIO_AGENT_ROLE = '0xd67ad422505496469a1adf6cdf9e5ee92ac5d33992843c9ecc4b2f6d6cde9137'
 
     addresses.push(exchange.address);
     values.push(0);
-    abis.push(exchange.interface.encodeFunctionData('upgradeTo', ['0x98Db3ff85F35353B9282613c105fF274DFBD2812']));
+    abis.push(exchange.interface.encodeFunctionData('upgradeTo', ['0xceD883Db0FAa589Ac68fc457822277b582a9DCDf']));
 
-    await createProposal(addresses, values, abis);
+    addresses.push(exchange.address);
+    values.push(0);
+    abis.push(exchange.interface.encodeFunctionData('changeAdminRoles', []));
+
+    addresses.push(exchange.address);
+    values.push(0);
+    abis.push(exchange.interface.encodeFunctionData('grantRole', [PORTFOLIO_AGENT_ROLE, '0x0bE3f37201699F00C21dCba18861ed4F60288E1D']));
+
+    addresses.push(exchange.address);
+    values.push(0);
+    abis.push(exchange.interface.encodeFunctionData('grantRole', [PORTFOLIO_AGENT_ROLE, '0xe497285e466227F4E8648209E34B465dAA1F90a0']));
+
+    await createProposal(addresses, values, abis)
 
 }
 
