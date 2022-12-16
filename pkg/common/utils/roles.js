8c06463fa731436462b0cfd484038fd83ec134b1
y.pavlov <lenvonder12vorh@gmail.com>
2022-12-16 13:31:17 +0300

    Add roles.js

---
+++ pkg/common/utils/roles.js
@@ -0,0 +1,12 @@
+class Roles {
+
+    static get PORTFOLIO_AGENT_ROLE() { return '0xd67ad422505496469a1adf6cdf9e5ee92ac5d33992843c9ecc4b2f6d6cde9137'; }
+    static get FREE_RIDER_ROLE() { return '0x262f8dca0440d5010bae937fe18daeae459dacb80622baddc3b4f10759ac952a'; }
+    static get WHITELIST_ROLE() { return '0xdc72ed553f2544c34465af23b847953efeb813428162d767f9ba5f4013be6760'; }
+    static get UNIT_ROLE() { return '0xede8101501d89b9894e78e4f219420b6ddb840e8e75dde35741a0745408476d7'; }
+    static get DEFAULT_ADMIN_ROLE() { return '0x0000000000000000000000000000000000000000000000000000000000000000'; }
+    static get UPGRADER_ROLE() { return '0x189ab7a9244df0848122154315af71fe140f3db0fe014031783b0946b8c9d2e3'; }
+}
+
+
+module.exports.Roles = Roles;
