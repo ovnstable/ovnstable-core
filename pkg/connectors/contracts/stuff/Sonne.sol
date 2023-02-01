aa0545dbc47960bf1f3f06fd332deb7a8129eb60
Naran Mucherinov <n.mucherinov@gmail.com>
2023-02-01 10:45:40 +0300

    New versions common 0.0.25 connectors 0.0.17

---
+++ pkg/connectors/contracts/stuff/Sonne.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+interface Unitroller {
+    function enterMarkets(address[] calldata cTokens) external returns (uint[] memory);
+    function exitMarket(address cToken) external returns (uint);
+    function claimComp(address holder, CToken[] memory cTokens) external;
+}
+
+interface CToken {
+    function balanceOf(address owner) external view returns (uint);
+    function exchangeRateStored() external view returns (uint);
+    function borrowBalanceStored(address account) external view returns (uint);
+    function mint(uint mintAmount) external returns (uint);
+    function redeem(uint redeemTokens) external returns (uint);
+    function redeemUnderlying(uint redeemAmount) external returns (uint);
+    function borrow(uint borrowAmount) external returns (uint);
+    function repayBorrow(uint repayAmount) external returns (uint);
+    function repayBorrowBehalf(address borrower, uint repayAmount) external returns (uint);
+}
