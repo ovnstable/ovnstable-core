92929df92c4cfe546b869c2e60e4783aaed32209
y.pavlov <lenvonder12vorh@gmail.com>
2023-02-13 13:02:54 +0300

    Deploy StrategyEtsZeta, StrategyEtsEpsilon

---
+++ pkg/strategies/optimism/deploy/25_strategy_ets_epsilon.js
@@ -0,0 +1,26 @@
+const {deployProxyMulti} = require("@overnight-contracts/common/utils/deployProxy");
+const {deploySection, settingSection} = require("@overnight-contracts/common/utils/script-utils");
+const {OPTIMISM} = require("@overnight-contracts/common/utils/assets");
+
+let rebaseToken = '0x6B2303BbB4F872fd26add9E42543Fd5903EBA332';
+let hedgeExchanger = '0x4de6Bc3934D3b86A183e857C3C8cdd3e5B3013c3';
+
+module.exports = async ({deployments}) => {
+    const {save} = deployments;
+
+    await deploySection(async (name) => {
+        await deployProxyMulti(name, 'StrategyEts', deployments, save, null);
+    });
+
+    await settingSection(async (strategy) => {
+        await (await strategy.setParams(
+            {
+                asset: OPTIMISM.usdc,
+                rebaseToken: rebaseToken,
+                hedgeExchanger: hedgeExchanger,
+            }
+        )).wait();
+    });
+};
+
+module.exports.tags = ['StrategyEtsEpsilon'];
