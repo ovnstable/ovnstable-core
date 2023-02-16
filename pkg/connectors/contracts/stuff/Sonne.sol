a88714f453eefb11077589e3e848456236ac864e
y.pavlov <lenvonder12vorh@gmail.com>
2023-02-16 11:32:04 +0300

    Add ReaperStrategySonne.sol

--- pkg/connectors/contracts/stuff/Sonne.sol
+++ pkg/connectors/contracts/stuff/Sonne.sol
@@ -5,12 +5,15 @@ interface Unitroller {
     function enterMarkets(address[] calldata cTokens) external returns (uint[] memory);
     function exitMarket(address cToken) external returns (uint);
     function claimComp(address holder, CToken[] memory cTokens) external;
+    function markets(address ctoken) external view returns ( bool, uint256, bool );
+    function getAccountLiquidity(address account) external view returns (uint, uint256, uint);
 }
 
 interface CToken {
     function balanceOf(address owner) external view returns (uint);
     function exchangeRateStored() external view returns (uint);
     function borrowBalanceStored(address account) external view returns (uint);
+    function balanceOfUnderlying(address account) external view returns (uint);
     function mint(uint mintAmount) external returns (uint);
     function redeem(uint redeemTokens) external returns (uint);
     function redeemUnderlying(uint redeemAmount) external returns (uint);
