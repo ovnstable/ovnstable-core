2160d2009893a06fae87d25f93b06709a0ca1694
y.pavlov <lenvonder12vorh@gmail.com>
2023-02-21 16:49:35 +0300

    Add setProfitRecipient to base-setting.js

--- pkg/core/scripts/base-setting.js
+++ pkg/core/scripts/base-setting.js
@@ -19,6 +19,8 @@ async function main() {
     await (await exchange.grantRole(Roles.UNIT_ROLE, '0xb8f55cdd8330b9bf9822137Bc8A6cCB89bc0f055')).wait(); // payout
     await (await exchange.grantRole(Roles.UNIT_ROLE, '0x5CB01385d3097b6a189d1ac8BA3364D900666445')).wait(); // dev
 
+    await (await exchange.setProfitRecipient( '0x9030D5C596d636eEFC8f0ad7b2788AE7E9ef3D46')).wait(); // ovn reward wallet
+
     await (await pm.grantRole(Roles.PORTFOLIO_AGENT_ROLE, '0x5CB01385d3097b6a189d1ac8BA3364D900666445')).wait();       // dev
     await (await pm.grantRole(Roles.PORTFOLIO_AGENT_ROLE, '0x0bE3f37201699F00C21dCba18861ed4F60288E1D')).wait();       // pm
     await (await pm.grantRole(Roles.PORTFOLIO_AGENT_ROLE, '0xe497285e466227F4E8648209E34B465dAA1F90a0')).wait();       // ovn
