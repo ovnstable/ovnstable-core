dd87e5b316acf8406a587ae0e8afe50bcb8bd002
y.pavlov <lenvonder12vorh@gmail.com>
2023-03-07 23:40:42 +0300

    [Optimism] ETS Pi, ETS Rho

---
+++ pkg/strategies/optimism/deploy/37_strategy_ets_pi.js
@@ -0,0 +1,26 @@
+const {deployProxyMulti} = require("@overnight-contracts/common/utils/deployProxy");
+const {deploySection, settingSection} = require("@overnight-contracts/common/utils/script-utils");
+const {OPTIMISM} = require("@overnight-contracts/common/utils/assets");
+
+let rebaseToken = '';
+let hedgeExchanger = '';
+
+module.exports = async ({deployments}) => {
+    const {save} = deployments;
+
+    await deploySection(async (name) => {
+        await deployProxyMulti(name, 'StrategyEts', deployments, save, null);
+    });
+
+    await settingSection(async (strategy) => {
+        await (await strategy.setParams(
+            {
+                asset: OPTIMISM.usdc,
+                rebaseToken: rebaseToken,
+                hedgeExchanger: hedgeExchanger,
+            }
+        )).wait();
+    });
+};
+
+module.exports.tags = ['StrategyEtsPi'];
