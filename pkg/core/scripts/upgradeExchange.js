8c9252eef3b2caf267ff93059f0a8aea5db78399
y.pavlov <lenvonder12vorh@gmail.com>
2023-03-17 19:40:30 +0300

    [Optimism] Deploy OptimismPayoutListener.sol

--- pkg/core/scripts/upgradeExchange.js
+++ pkg/core/scripts/upgradeExchange.js
@@ -1,31 +1,39 @@
-const {getContract} = require("@overnight-contracts/common/utils/script-utils");
-const {createProposal, execProposal} = require("@overnight-contracts/common/utils/governance");
+const {getContract, execTimelock} = require("@overnight-contracts/common/utils/script-utils");
+const {createProposal, execProposal, testProposal} = require("@overnight-contracts/common/utils/governance");
+const {Roles} = require("@overnight-contracts/common/utils/roles");
+const {fromAsset} = require("@overnight-contracts/common/utils/decimals");
+const {showPoolOperations} = require("@overnight-contracts/common/utils/payoutListener");
 
 async function main() {
 
-    let exchange = await getContract('Exchange');
 
     let addresses = [];
     let values = [];
     let abis = [];
 
-    let PORTFOLIO_AGENT_ROLE = '0xd67ad422505496469a1adf6cdf9e5ee92ac5d33992843c9ecc4b2f6d6cde9137'
+
+    let exchange = await getContract('Exchange', 'optimism');
 
     addresses.push(exchange.address);
     values.push(0);
-    abis.push(exchange.interface.encodeFunctionData('upgradeTo', ['0xceD883Db0FAa589Ac68fc457822277b582a9DCDf']));
+    abis.push(exchange.interface.encodeFunctionData('upgradeTo', ['0xfF66c6A4B3910b62bBCF4Cfde61562ef3E96a5eD']));
 
     addresses.push(exchange.address);
     values.push(0);
-    abis.push(exchange.interface.encodeFunctionData('changeAdminRoles', []));
+    abis.push(exchange.interface.encodeFunctionData('setPayoutListener', ['0xf59C9A5110852a82434E02d1A43169b5fD9897Be']));
 
-    addresses.push(exchange.address);
-    values.push(0);
-    abis.push(exchange.interface.encodeFunctionData('grantRole', [PORTFOLIO_AGENT_ROLE, '0x0bE3f37201699F00C21dCba18861ed4F60288E1D']));
 
-    addresses.push(exchange.address);
+    let exchangeDai = await getContract('Exchange', 'optimism_dai');
+
+    addresses.push(exchangeDai.address);
     values.push(0);
-    abis.push(exchange.interface.encodeFunctionData('grantRole', [PORTFOLIO_AGENT_ROLE, '0xe497285e466227F4E8648209E34B465dAA1F90a0']));
+    abis.push(exchangeDai.interface.encodeFunctionData('upgradeTo', ['0xfF66c6A4B3910b62bBCF4Cfde61562ef3E96a5eD']));
+
+    addresses.push(exchangeDai.address);
+    values.push(0);
+    abis.push(exchangeDai.interface.encodeFunctionData('setPayoutListener', ['0xf59C9A5110852a82434E02d1A43169b5fD9897Be']));
+
+    // await testProposal(addresses, values, abis);
 
     await createProposal(addresses, values, abis)
 
