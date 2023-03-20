05e0ffa7022bed2610b39c64bd8b3e780df22001
Naran Mucherinov <n.mucherinov@gmail.com>
2023-03-20 19:22:37 +0300

    Deploy StrategyEtsEpsilon on Arbitrum

---
+++ pkg/strategies/arbitrum/deploy/12_strategy_ets_epsilon.js
@@ -0,0 +1,26 @@
+const {deployProxyMulti} = require("@overnight-contracts/common/utils/deployProxy");
+const {deploySection, settingSection} = require("@overnight-contracts/common/utils/script-utils");
+const {ARBITRUM} = require("@overnight-contracts/common/utils/assets");
+
+let rebaseToken = '0x3d58E75c9206F9a299be9279AF30c17383e077A4';
+let hedgeExchanger = '0xdf861e5eb58C937ba09A5488112fB52cC0d421Dd';
+
+module.exports = async ({deployments}) => {
+    const {save} = deployments;
+
+    await deploySection(async (name) => {
+        await deployProxyMulti(name, 'StrategyEts', deployments, save, null);
+    });
+
+    await settingSection(async (strategy) => {
+        await (await strategy.setParams(
+            {
+                asset: ARBITRUM.usdc,
+                rebaseToken: rebaseToken,
+                hedgeExchanger: hedgeExchanger,
+            }
+        )).wait();
+    });
+};
+
+module.exports.tags = ['StrategyEtsEpsilon'];
