d6df38c4b6829d9aeb86457b44322cd865867b04
Naran Mucherinov <n.mucherinov@gmail.com>
2023-03-22 18:08:44 +0300

    Deploy StrategyEtsEta and StrategyEtsZeta on Arbitrum

---
+++ pkg/strategies/arbitrum/deploy/13_strategy_ets_zeta.js
@@ -0,0 +1,26 @@
+const {deployProxyMulti} = require("@overnight-contracts/common/utils/deployProxy");
+const {deploySection, settingSection} = require("@overnight-contracts/common/utils/script-utils");
+const {ARBITRUM} = require("@overnight-contracts/common/utils/assets");
+
+let rebaseToken = '0xCdAc414c104Dd22b2F7157331F6B725168842AA5';
+let hedgeExchanger = '0x412E1eD3aB206Dd4695DEafB8Fd159E10962683b';
+
+module.exports = async ({deployments}) => {
+    const {save} = deployments;
+
+    await deploySection(async (name) => {
+        await deployProxyMulti(name, 'StrategyEts', deployments, save, null);
+    });
+
+    await settingSection(async (strategy) => {
+        await (await strategy.setParams(
+            {
+                asset: ARBITRUM.usdc,
+                rebaseToken: rebaseToken,
+                hedgeExchanger: hedgeExchanger,
+            }
+        )).wait();
+    });
+};
+
+module.exports.tags = ['StrategyEtsZeta'];
---
+++ pkg/strategies/arbitrum/deploy/14_strategy_ets_eta.js
@@ -0,0 +1,26 @@
+const {deployProxyMulti} = require("@overnight-contracts/common/utils/deployProxy");
+const {deploySection, settingSection} = require("@overnight-contracts/common/utils/script-utils");
+const {ARBITRUM} = require("@overnight-contracts/common/utils/assets");
+
+let rebaseToken = '0x16Cc2b19aAe552A4F5Ee6581c18d6922Bea2BA72';
+let hedgeExchanger = '0xA0E8C7e324876e957C916B2efcf41C336Fa4e889';
+
+module.exports = async ({deployments}) => {
+    const {save} = deployments;
+
+    await deploySection(async (name) => {
+        await deployProxyMulti(name, 'StrategyEts', deployments, save, null);
+    });
+
+    await settingSection(async (strategy) => {
+        await (await strategy.setParams(
+            {
+                asset: ARBITRUM.usdc,
+                rebaseToken: rebaseToken,
+                hedgeExchanger: hedgeExchanger,
+            }
+        )).wait();
+    });
+};
+
+module.exports.tags = ['StrategyEtsEta'];
