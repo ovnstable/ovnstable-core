cf66bdf2e0ee9b8fc8f5c85b4200beddab941b5d
Naran Mucherinov <n.mucherinov@gmail.com>
2023-03-28 18:51:16 +0700

    Delete old deployments. Delete useless params. Add cycle deplosit for Gamma

--- pkg/strategies/optimism/deploy/14_strategy_arrakis_usdc_dai.js
+++ pkg/strategies/optimism/deploy/14_strategy_arrakis_usdc_dai.js
@@ -5,15 +5,7 @@ const {OPTIMISM} = require("@overnight-contracts/common/utils/assets");
 let arrakisRouter = "0x86D62A8AD19998E315e6242b63eB73F391D4674B";
 let arrakisRewards = "0x87c7c885365700D157cd0f39a7803320fe86f0f5";
 let arrakisVault = "0x632336474f5Bf11aEbECd63B84A0a2800B99a490";
-let beethovenxVault = "0xBA12222222228d8Ba445958a75a0704d566BF2C8";
-let beethovenxPoolIdUsdc = "0xedcfaf390906a8f91fb35b7bac23f3111dbaee1c00000000000000000000007c";
-let beethovenxPoolIdDaiUsdtUsdc = "0x428e1cc3099cf461b87d124957a0d48273f334b100000000000000000000007f";
-let beethovenxPoolIdDai = "0x62ec8b26c08ffe504f22390a65e6e3c1e45e987700000000000000000000007e";
-let bbRfAUsdc = "0xEdcfaF390906a8f91fb35B7bAC23f3111dBaEe1C";
-let bbRfADai = "0x62eC8b26C08Ffe504F22390A65e6E3c1e45E9877";
 let poolUsdcOpFee = 500; // 0.05%
-let kyberSwapRouter = "0xC1e7dFE73E1598E3910EF4C7845B68A9Ab6F4c83";
-let poolUsdcDaiFee = 8; // 0.008%
 
 module.exports = async ({deployments}) => {
     const {save} = deployments;
@@ -31,18 +23,10 @@ module.exports = async ({deployments}) => {
                 arrakisRouter: arrakisRouter,
                 arrakisRewards: arrakisRewards,
                 arrakisVault: arrakisVault,
-                beethovenxVault: beethovenxVault,
-                beethovenxPoolIdUsdc: beethovenxPoolIdUsdc,
-                beethovenxPoolIdDaiUsdtUsdc: beethovenxPoolIdDaiUsdtUsdc,
-                beethovenxPoolIdDai: beethovenxPoolIdDai,
-                bbRfAUsdc: bbRfAUsdc,
-                bbRfADai: bbRfADai,
                 uniswapV3Router: OPTIMISM.uniswapV3Router,
                 poolUsdcOpFee: poolUsdcOpFee,
                 oracleUsdc: OPTIMISM.oracleUsdc,
                 oracleDai: OPTIMISM.oracleDai,
-                kyberSwapRouter: kyberSwapRouter,
-                poolUsdcDaiFee: poolUsdcDaiFee,
                 curve3Pool: OPTIMISM.curve3Pool,
             }
         )).wait();
--- pkg/strategies/optimism/deploy/17_strategy_gamma_usdc_dai.js
+++ pkg/strategies/optimism/deploy/17_strategy_gamma_usdc_dai.js
@@ -3,18 +3,10 @@ const {deploySection, settingSection} = require("@overnight-contracts/common/uti
 const {OPTIMISM} = require("@overnight-contracts/common/utils/assets");
 
 let lpToken = '0xaED05fdd471a4EecEe48B34d38c59CC76681A6C8';
-let uniProxy = '0xf874D4957861E193AEC9937223062679C14f9Aca';
+let uniProxy = '0x40a3E5778f66835265602f92D507AeC708c2C0AD';
 let masterChef = '0xC7846d1bc4d8bcF7c45a7c998b77cE9B3c904365';
 let pid = 1;
-let beethovenxVault = "0xBA12222222228d8Ba445958a75a0704d566BF2C8";
-let beethovenxPoolIdUsdc = "0xedcfaf390906a8f91fb35b7bac23f3111dbaee1c00000000000000000000007c";
-let beethovenxPoolIdDaiUsdtUsdc = "0x428e1cc3099cf461b87d124957a0d48273f334b100000000000000000000007f";
-let beethovenxPoolIdDai = "0x62ec8b26c08ffe504f22390a65e6e3c1e45e987700000000000000000000007e";
-let bbRfAUsdc = "0xEdcfaF390906a8f91fb35B7bAC23f3111dBaEe1C";
-let bbRfADai = "0x62eC8b26C08Ffe504F22390A65e6E3c1e45E9877";
 let poolFeeOpUsdc = 500; // 0.05%
-let kyberSwapRouter = "0xC1e7dFE73E1598E3910EF4C7845B68A9Ab6F4c83";
-let poolUsdcDaiFee = 8; // 0.008%
 
 module.exports = async ({deployments}) => {
     const {save} = deployments;
@@ -33,18 +25,10 @@ module.exports = async ({deployments}) => {
                 uniProxy: uniProxy,
                 masterChef: masterChef,
                 pid: pid,
-                beethovenxVault: beethovenxVault,
-                beethovenxPoolIdUsdc: beethovenxPoolIdUsdc,
-                beethovenxPoolIdDaiUsdtUsdc: beethovenxPoolIdDaiUsdtUsdc,
-                beethovenxPoolIdDai: beethovenxPoolIdDai,
-                bbRfAUsdc: bbRfAUsdc,
-                bbRfADai: bbRfADai,
                 uniswapV3Router: OPTIMISM.uniswapV3Router,
                 poolFeeOpUsdc: poolFeeOpUsdc,
                 oracleUsdc: OPTIMISM.oracleUsdc,
                 oracleDai: OPTIMISM.oracleDai,
-                kyberSwapRouter: kyberSwapRouter,
-                poolUsdcDaiFee: poolUsdcDaiFee,
                 curve3Pool: OPTIMISM.curve3Pool,
             }
         )).wait();
