a9e03a49b423e80db75d4877316fe1a17c7160fe
Naran Mucherinov <n.mucherinov@gmail.com>
2023-03-28 21:04:59 +0700

    Deploy StrategyArrakisUsdcDai and StrategyGammaUsdcDai

--- pkg/strategies/optimism/scripts/verify.js
+++ pkg/strategies/optimism/scripts/verify.js
@@ -1,7 +1,7 @@
 const {verify } = require("@overnight-contracts/common/utils/verify-utils");
 
 async function main() {
-    let items = ["StrategyEtsOmicron"];
+    let items = ["StrategyArrakisUsdcDai", "StrategyGammaUsdcDai"];
     await verify(items);
 }
 
