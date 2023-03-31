3ad36d478544c66a849f6937b0284220ce22a2bf
y.pavlov <lenvonder12vorh@gmail.com>
2023-03-31 18:28:03 +0300

    Add zkSync wallet to ethers.getContract

--- pkg/core/deploy/core/22_setting_m2m.js
+++ pkg/core/deploy/core/22_setting_m2m.js
@@ -1,11 +1,14 @@
 const { ethers } = require("hardhat");
+const {initWallet} = require("@overnight-contracts/common/utils/script-utils");
 
 module.exports = async ({getNamedAccounts, deployments}) => {
     const {deploy} = deployments;
     const {deployer} = await getNamedAccounts();
 
-    const m2m = await ethers.getContract("Mark2Market");
-    const pm = await ethers.getContract("PortfolioManager");
+    let wallet = await initWallet();
+
+    const m2m = await ethers.getContract("Mark2Market", wallet);
+    const pm = await ethers.getContract("PortfolioManager", wallet);
     await (await m2m.setPortfolioManager(pm.address)).wait();
     console.log("m2m.setPortfolioManager done");
 };
