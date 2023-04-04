b7473c367735867d3e8fded98afdd9bdf7d94531
y.pavlov <lenvonder12vorh@gmail.com>
2023-04-04 12:57:55 +0300

    [BSC] Upgrade Exchange.sol to last version

--- pkg/common/utils/exchange.js
+++ pkg/common/utils/exchange.js
@@ -1,6 +1,7 @@
 const {execTimelock, getWalletAddress} = require("./script-utils");
 const {Roles} = require("./roles");
 const {ZERO_ADDRESS} = require("@openzeppelin/test-helpers/src/constants");
+const {showPoolOperations} = require("./payoutListener");
 
 
 async function disableOracleLoss(exchange){
@@ -23,6 +24,20 @@ async function disablePayoutListener(exchange){
 
 }
 
+async function executePayout(exchange){
+
+    await execTimelock(async (timelock)=> {
+        await (await exchange.connect(timelock).grantRole(Roles.PORTFOLIO_AGENT_ROLE, timelock.address)).wait();
+        await (await exchange.connect(timelock).grantRole(Roles.UNIT_ROLE, await getWalletAddress())).wait();
+
+        await (await exchange.connect(timelock).setPayoutTimes(1637193600, 24 * 60 * 60, 15 * 60)).wait();
+        let tx = await (await exchange.payout()).wait();
+
+        await showPoolOperations(tx);
+    });
+}
+
 
 module.exports.disableOracleLoss = disableOracleLoss;
 module.exports.disablePayoutListener = disablePayoutListener;
+module.exports.executePayout = executePayout;
