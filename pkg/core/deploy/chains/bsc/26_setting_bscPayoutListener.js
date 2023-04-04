a501e5519ca24db84370942cdeaebb00d21b53fd
y.pavlov <lenvonder12vorh@gmail.com>
2023-04-04 12:47:49 +0300

    [BSC] Upgrade BscPayoutListener.sol to last version

--- pkg/core/deploy/chains/bsc/26_setting_bscPayoutListener.js
+++ pkg/core/deploy/chains/bsc/26_setting_bscPayoutListener.js
@@ -1,55 +1,68 @@
 const {ethers} = require("hardhat");
 
 let {BSC, COMMON} = require('@overnight-contracts/common/utils/assets');
+const {getContract} = require("@overnight-contracts/common/utils/script-utils");
+const {createSkim, createBribe, createSkimTo} = require("@overnight-contracts/common/utils/payoutListener");
+const {Roles} = require("@overnight-contracts/common/utils/roles");
 
 module.exports = async ({getNamedAccounts, deployments}) => {
-    const {deploy} = deployments;
-    const {deployer} = await getNamedAccounts();
+
 
     const pl = await ethers.getContract("BscPayoutListener");
-    const exchange = await ethers.getContract("Exchange");
 
-    await (await pl.setExchanger(exchange.address)).wait();
-    await (await pl.setPancakeDepositWallet(COMMON.rewardWallet)).wait();
-    await (await pl.setUsdPlus(BSC.usdPlus)).wait();
-    await (await pl.setRewardWallet(COMMON.rewardWallet)).wait();
+    let usdPlus = await getContract('UsdPlusToken', 'bsc');
+    let usdtPlus = await getContract('UsdPlusToken', 'bsc_usdt');
 
-    // Cone
-    let qsSyncPools = [
-        "0xec30da6361905b8f3e4a93513d937db992301276",  // WBNB/USD+
-        "0x30f96ad4856d7e699963b589591f03710976a6e8",  // MDB+/USD+
-        "0x0f8c95890b7bdecfae7990bf32f22120111e0b44",  // TETU/USD+
-        "0x0fe6cf7a2687c5bddf302c5ccea901ba0bf71816",  // USD+/BUSD
-        "0xdee33737634bb7612c15b10488819e88fd62f0f9",  // TIGAR/USD+
-    ];
-    await (await pl.setQsSyncPools(qsSyncPools)).wait();
+    let items = [];
 
-    // Pancake
-    let pancakeSkimPools = [
-        "0x2BD37f67EF2894024D3ee52b20A6cB87d71B2933",  // USD+/BUSD
-    ];
-    await (await pl.setPancakeSkimPools(pancakeSkimPools)).wait();
+    items.push(...wombat());
+    items.push(...thena());
+    items.push(...pacnake());
 
-    // Thena
-    let thenaSkimPools = [
-        '0x92573046BD4abA37d875eb45a0A1182ac63d5580', // sAMM-ETS Alpha/USD+
-        '0x1F3cA66c98d682fA1BeC31264692daD4f17340BC', // sAMM-HAY/USD+
-        '0x150990B9630fFe2322999e86905536E2E7e8d93f', // sAMM-USD+/CUSD
-        '0xea9abc7AD420bDA7dD42FEa3C4ACd058902A5845', // sAMM-USDT/USD+
-        '0x7F74D8C2A0D0997695eA121A2155e2710a6D62dc', // vAMM-USD+/THE
-    ];
-    let thenaSkimBribes = [
-        '0x90ab86ddd5fdae0207367c2d09e10ef1fb3cb651', // sAMM-ETS Alpha/USD+
-        '0x8FbF01a6D3Fef2847f2c35d9a05476eb859D6B2e', // sAMM-HAY/USD+
-        '0x8828Bd1b7abc5296D3FcfEb15F58c23088410449', // sAMM-USD+/CUSD
-        '0xe52917158c3C8e29Ae3B5458200cbF516E56F660', // sAMM-USDT/USD+
-        '0x39d46d0f49fab60002d2f359d1497ffbfc889ea6', // vAMM-USD+/THE
-    ];
-    await (await pl.setThenaSkimPools(thenaSkimPools, thenaSkimBribes)).wait();
+    await (await pl.addItems(items)).wait();
 
-    await (await pl.setWombatSkimPools(["0xC1e0b4b217E4b15b09457d0cdD93179215BeD911"])).wait();
+    await (await pl.grantRole(Roles.EXCHANGER, (await getContract('Exchange', 'bsc')).address)).wait();
+    await (await pl.grantRole(Roles.EXCHANGER, (await getContract('Exchange', 'bsc_usdt')).address)).wait();
 
     console.log('BscPayoutListener setting done');
+
+    function pacnake(){
+
+        let dex = 'Pancake';
+
+        let items = [];
+        items.push(createSkim('0x2BD37f67EF2894024D3ee52b20A6cB87d71B2933', usdPlus.address, 'USD+/BUSD', dex));
+
+        return items;
+    }
+
+
+    function thena(){
+
+        let dex = 'Thena';
+
+        let items = [];
+        items.push(createBribe('0x92573046BD4abA37d875eb45a0A1182ac63d5580', usdPlus.address, 'sAMM-ETS Alpha/USD+', dex, '0x90ab86ddd5fdae0207367c2d09e10ef1fb3cb651'));
+        items.push(createBribe('0x1F3cA66c98d682fA1BeC31264692daD4f17340BC', usdPlus.address, 'sAMM-HAY/USD+', dex, '0x8FbF01a6D3Fef2847f2c35d9a05476eb859D6B2e'));
+        items.push(createBribe('0x150990B9630fFe2322999e86905536E2E7e8d93f', usdPlus.address, 'sAMM-USD+/CUSD', dex, '0x8828Bd1b7abc5296D3FcfEb15F58c23088410449'));
+        items.push(createBribe('0xea9abc7AD420bDA7dD42FEa3C4ACd058902A5845', usdPlus.address, 'sAMM-USDT/USD+', dex, '0xe52917158c3C8e29Ae3B5458200cbF516E56F660'));
+        items.push(createBribe('0x7F74D8C2A0D0997695eA121A2155e2710a6D62dc', usdPlus.address, 'vAMM-USD+/THE', dex, '0x39d46d0f49fab60002d2f359d1497ffbfc889ea6'));
+
+        return items;
+    }
+
+    function wombat(){
+
+        let dex = 'Wombat';
+
+        let items = [];
+        items.push(createSkimTo('0x88bEb144352BD3109c79076202Fac2bcEAb87117', usdPlus.address, 'LP-USD+', dex, '0xdAa33667A9aB2791Fb0F3c0261C74c4A3d0A7eFA'));
+        items.push(createSkimTo('0xbd459E33307A4ae92fFFCb45C6893084CFC273B1', usdtPlus.address, 'LP-USDT+', dex, '0x51AbD2A24d225C80108252fb73AEd819f1Ef52Bd'));
+
+        return items;
+
+    }
+
 };
 
 module.exports.tags = ['SettingBscPayoutListener'];
