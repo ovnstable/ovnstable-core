fe963f2125a1a3bb1c3289fd77e56b289ec9a5f9
Naran Mucherinov <n.mucherinov@gmail.com>
2023-04-14 18:43:23 +0700

    Redeploy ArbitrumPayoutListener

--- pkg/core/contracts/GlobalPayoutListener.sol
+++ pkg/core/contracts/GlobalPayoutListener.sol
@@ -154,16 +154,6 @@ abstract contract GlobalPayoutListener is IGlobalPayoutListener, Initializable, 
       */
 
     function _skim(Item memory item) internal {
-
-        // skim check
-        uint256 reserve0 = IPool(item.pool).reserve0();
-        uint256 reserve1 = IPool(item.pool).reserve1();
-        uint256 token0Balance = IERC20(IPool(item.pool).token0()).balanceOf(item.pool);
-        uint256 token1Balance = IERC20(IPool(item.pool).token1()).balanceOf(item.pool);
-        if (token0Balance < reserve0 || token1Balance < reserve1) {
-            return;
-        }
-
         IERC20 token = IERC20(item.token);
         uint256 tokenBalanceBeforeSkim = token.balanceOf(address(this));
         IPool(item.pool).skim(address(this));
@@ -190,16 +180,6 @@ abstract contract GlobalPayoutListener is IGlobalPayoutListener, Initializable, 
       */
 
     function _bribe(Item memory item) internal {
-
-        // skim check
-        uint256 reserve0 = IPool(item.pool).reserve0();
-        uint256 reserve1 = IPool(item.pool).reserve1();
-        uint256 token0Balance = IERC20(IPool(item.pool).token0()).balanceOf(item.pool);
-        uint256 token1Balance = IERC20(IPool(item.pool).token1()).balanceOf(item.pool);
-        if (token0Balance < reserve0 || token1Balance < reserve1) {
-            return;
-        }
-
         IERC20 token = IERC20(item.token);
         uint256 tokenBalanceBeforeSkim = token.balanceOf(address(this));
         IPool(item.pool).skim(address(this));
@@ -273,14 +253,6 @@ interface IPool {
     function skim(address to) external;
 
     function sync() external;
-
-    function reserve0() view external returns (uint256);
-
-    function reserve1() view external returns (uint256);
-
-    function token0() view external returns (address);
-
-    function token1() view external returns (address);
 
 }
 
