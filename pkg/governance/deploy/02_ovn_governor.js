0f2277756aff34db27e526b3100ced90f13ffa0e
y.pavlov <lenvonder12vorh@gmail.com>
2023-04-18 19:54:04 +0300

    [ZkSync] Deploy Governance

--- pkg/governance/deploy/02_ovn_governor.js
+++ pkg/governance/deploy/02_ovn_governor.js
@@ -1,22 +1,43 @@
 const { ethers, upgrades} = require("hardhat");
 const { deployProxy } = require('@overnight-contracts/common/utils/deployProxy');
+const {isZkSync} = require("@overnight-contracts/common/utils/network");
+const {Deployer} = require("@matterlabs/hardhat-zksync-deploy");
+const hre = require("hardhat");
+const {initWallet} = require("@overnight-contracts/common/utils/script-utils");
 
 module.exports = async ({getNamedAccounts, deployments}) => {
     const {deploy, save} = deployments;
     const {deployer} = await getNamedAccounts();
 
-    await deployProxy('OvnTimelockController', deployments, save);
-    console.log("Deploy OvnTimelockController done");
+    // await deployProxy('OvnTimelockController', deployments, save);
+    // console.log("Deploy OvnTimelockController done");
 
     const token = await ethers.getContract("OvnToken");
     const controller = await ethers.getContract("OvnTimelockController");
 
-    await deploy('OvnGovernor', {
-        from: deployer,
-        args: [token.address, controller.address ],
-        log: true,
-    });
-    console.log("Deploy OvnGovernor done");
+
+    if (isZkSync()){
+
+        const deployerZkSync = new Deployer(hre, await initWallet());
+
+        let ovnGovernorArtifact = await deployerZkSync.loadArtifact('OvnGovernor');
+        const governor = await deployerZkSync.deploy(ovnGovernorArtifact, [token.address, controller.address ]);
+
+        await save('OvnGovernor', {
+            address: governor.address,
+            ...ovnGovernorArtifact
+        });
+        console.log("Deploy OvnGovernor done");
+
+    }else {
+        await deploy('OvnGovernor', {
+            from: deployer,
+            args: [token.address, controller.address ],
+            log: true,
+        });
+        console.log("Deploy OvnGovernor done");
+    }
+
 
     let governor = await ethers.getContract('OvnGovernor');
 
--- pkg/governance/hardhat.config.js
+++ pkg/governance/hardhat.config.js
@@ -3,6 +3,10 @@ require('@openzeppelin/hardhat-upgrades');
 require("@nomiclabs/hardhat-etherscan");
 require("hardhat-gas-reporter")
 require('@overnight-contracts/common/utils/hardhat-ovn');
+require("@matterlabs/hardhat-zksync-deploy");
+require("@matterlabs/hardhat-zksync-solc");
+require("@matterlabs/hardhat-zksync-verify");
+
 const config = require("../common/utils/hardhat-config");
 
 module.exports = {
@@ -10,6 +14,7 @@ module.exports = {
     namedAccounts: config.namedAccounts,
     networks: config.getNetwork(process.env.ETH_NETWORK),
     solidity: config.solidity,
+    zksolc: config.zksolc,
     etherscan: config.etherscan(),
     mocha: config.mocha,
     gasReporter: config.gasReport
