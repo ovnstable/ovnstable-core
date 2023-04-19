258794634058f43e9ed21d371aaeea5171d42213
y.pavlov <lenvonder12vorh@gmail.com>
2023-04-19 17:44:05 +0300

    Add wallet and blockNumber

--- pkg/recovery/scripts/recovery.js
+++ pkg/recovery/scripts/recovery.js
@@ -10,7 +10,7 @@ const { fromE18 } = require("@overnight-contracts/common/utils/decimals");
 async function main() {
 
 
-    let wallet = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80' // has USD+/DAI+ in beets pools
+    let wallet = '0x98a513e3caf92d9a402caed7b470f58e3e061c84' // has USD+/DAI+ in beets pools
 
     // await getContract()
 
@@ -33,9 +33,9 @@ async function main() {
     // - USD+
     // - DAI+
 
-    // 100 usdc -> Хочу положить в овернайт пул -> 
+    // 100 usdc -> Хочу положить в овернайт пул ->
 
-    await getBalance(wallet, 1300000);
+    await getBalance(wallet, 92134340);
 
 }
 
@@ -51,19 +51,44 @@ async function getBalanceBeets(wallet, blockNumber) {
     const gaugeBalance = await getBalanceBeetsGauge(wallet, blockNumber);
     const bptBalance = await getBptBalance(wallet, blockNumber);
     const beetsBalance = gaugeBalance + bptBalance;
+
+    console.table([
+        {
+            name: 'Gauge',
+            amount: gaugeBalance
+        },
+        {
+            name: 'BPT',
+            amount: bptBalance
+        }
+    ])
+
     return beetsBalance;
+}
+
+async function convertBptToLinearPools(amountBpt){
+
+    let amountLinearPoolUsdPlus;
+    let amountLinearPoolDaiPlus;
+
+}
+
+async function convertLinearPoolToAssets(amountLp){
+
+    let amountUnderlying;
+    let amountWrapped;
 }
 
 async function getBalanceBeetsGauge(wallet, blockNumber) {
     const gaugePool = await ethers.getContractAt(GaugePool, "0xa066243Ba7DAd6C779caA1f9417910a4AE83cf4D", wallet);
-    const gaugeBalance = fromE18(await gaugePool.balanceOf(wallet.address, { blockTag: blockNumber }))
+    const gaugeBalance = fromE18(await gaugePool.balanceOf(wallet, { blockTag: blockNumber }))
     return gaugeBalance;
 }
 
 
 async function getBptBalance(wallet, blockNumber) {
     const bptPool = await ethers.getContractAt(Pool, "0xb1C9aC57594e9B1EC0f3787D9f6744EF4CB0A024", wallet);
-    const bptBalance = fromE18(await bptPool.balanceOf(wallet.address, { blockTag: blockNumber }))
+    const bptBalance = fromE18(await bptPool.balanceOf(wallet, { blockTag: blockNumber }))
     return bptBalance;
 }
 
