12559d9c96774b714ea40f862fc80cc4dd0b33ae
Naran Mucherinov <n.mucherinov@gmail.com>
2023-04-19 20:47:11 +0700

    New version common 0.0.44 connectors 0.0.37

--- pkg/connectors/contracts/stuff/Thena.sol
+++ pkg/connectors/contracts/stuff/Thena.sol
@@ -339,6 +339,34 @@ interface IGaugeV2 {
 
 }
 
+interface IAlgebraPool {
+    /**
+     * @notice The globalState structure in the pool stores many values but requires only one slot
+   * and is exposed as a single method to save gas when accessed externally.
+   * @return price The current price of the pool as a sqrt(token1/token0) Q64.96 value;
+   * Returns tick The current tick of the pool, i.e. according to the last tick transition that was run;
+   * Returns This value may not always be equal to SqrtTickMath.getTickAtSqrtRatio(price) if the price is on a tick
+   * boundary;
+   * Returns fee The last pool fee value in hundredths of a bip, i.e. 1e-6;
+   * Returns timepointIndex The index of the last written timepoint;
+   * Returns communityFeeToken0 The community fee percentage of the swap fee in thousandths (1e-3) for token0;
+   * Returns communityFeeToken1 The community fee percentage of the swap fee in thousandths (1e-3) for token1;
+   * Returns unlocked Whether the pool is currently locked to reentrancy;
+   */
+    function globalState()
+    external
+    view
+    returns (
+        uint160 price,
+        int24 tick,
+        uint16 fee,
+        uint16 timepointIndex,
+        uint16 communityFeeToken0,
+        uint16 communityFeeToken1,
+        bool unlocked
+    );
+
+}
 
 interface IBribe {
     function _deposit(uint amount, uint tokenId) external;
