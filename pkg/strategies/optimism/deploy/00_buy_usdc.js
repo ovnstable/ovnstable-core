3f551090e1ef2e75dfd1731d72623cf94e7cb81c
y.pavlov <lenvonder12vorh@gmail.com>
2023-05-02 13:30:50 +0300

    [Optimism] FIX: 00_buy_usdc.js - support DAI

--- pkg/strategies/optimism/deploy/00_buy_usdc.js
+++ pkg/strategies/optimism/deploy/00_buy_usdc.js
@@ -1,35 +1,17 @@
-const hre = require("hardhat");
-const {getERC20, getDevWallet, transferETH} = require("@overnight-contracts/common/utils/script-utils");
-const {fromE6} = require("@overnight-contracts/common/utils/decimals");
-const {ethers} = require("hardhat");
+const {transferETH, transferAsset} = require("@overnight-contracts/common/utils/script-utils");
+const {OPTIMISM} = require("@overnight-contracts/common/utils/assets");
 
 module.exports = async ({getNamedAccounts, deployments}) => {
     const {deployer} = await getNamedAccounts();
 
-    await transferETH(10, '0x5CB01385d3097b6a189d1ac8BA3364D900666445');
-    let holder = '0x489f866c0698c8d6879f5c0f527bc8281046042d';
+    await transferETH(1, deployer);
 
-    await hre.network.provider.request({
-        method: "hardhat_impersonateAccount",
-        params: [holder],
-    });
-
-    let wallet = await getDevWallet();
-
-    const tx = {
-        from: wallet.address,
-        to: holder,
-        value: ethers.utils.parseEther('1'),
-        nonce: await hre.ethers.provider.getTransactionCount(wallet.address, "latest"),
-        gasLimit: 229059,
-        gasPrice: await hre.ethers.provider.getGasPrice(),
+    console.log('[TEST] Buy assets ....');
+    if (process.env.STAND === "optimism") {
+        await transferAsset(OPTIMISM.usdc, deployer);
+    }else if (process.env.STAND === "optimism_dai"){
+        await transferAsset(OPTIMISM.dai, deployer);
     }
-    await wallet.sendTransaction(tx);
-
-    const signerWithAddress = await hre.ethers.getSigner(holder);
-    let usdc = await getERC20("usdc");
-
-    await usdc.connect(signerWithAddress).transfer(deployer, await usdc.balanceOf(signerWithAddress.address));
 };
 
 module.exports.tags = ['test'];
