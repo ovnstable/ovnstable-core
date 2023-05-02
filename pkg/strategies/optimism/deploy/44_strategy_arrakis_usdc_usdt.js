afb0420cf959082ca80a7b17570a7e566ba35cde
Valpaq <viktor.paramonov2000@gmail.com>
2023-05-02 10:24:50 +0300

    wip

---
+++ pkg/strategies/optimism/deploy/44_strategy_arrakis_usdc_usdt.js
@@ -0,0 +1,36 @@
+const { deployProxy } = require("@overnight-contracts/common/utils/deployProxy");
+const { deploySection, settingSection } = require("@overnight-contracts/common/utils/script-utils");
+const { OPTIMISM } = require("@overnight-contracts/common/utils/assets");
+
+let arrakisRouter = "0x9ce88a56d120300061593eF7AD074A1B710094d5";
+let arrakisRewards = "0x87c7c885365700D157cd0f39a7803320fe86f0f5";
+let arrakisVault = "0x632336474f5Bf11aEbECd63B84A0a2800B99a490";
+let poolUsdcOpFee = 100; // 0.01%
+
+module.exports = async ({ deployments }) => {
+    const { save } = deployments;
+
+    await deploySection(async (name) => {
+        await deployProxy(name, deployments, save);
+    });
+
+    await settingSection(async (strategy) => {
+        await (await strategy.setParams(
+            {
+                usdc: OPTIMISM.usdc,
+                usdt: OPTIMISM.usdt,
+                op: OPTIMISM.op,
+                arrakisRouter: arrakisRouter,
+                arrakisRewards: arrakisRewards,
+                arrakisVault: arrakisVault,
+                uniswapV3Router: OPTIMISM.uniswapV3Router,
+                poolUsdcOpFee: poolUsdcOpFee,
+                oracleUsdc: OPTIMISM.oracleUsdc,
+                oracleUsdt: OPTIMISM.oracleUsdt,
+                curve3Pool: OPTIMISM.curve3Pool,
+            }
+        )).wait();
+    });
+};
+
+module.exports.tags = ['StrategyArrakisUsdcUsdt'];
