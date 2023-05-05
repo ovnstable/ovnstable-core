cf855411acf74ed40f3dc3ebd66bbcb24653828d
Nikita Slezkin <venikpapa@gmail.com>
2023-05-06 00:36:39 +0300

    connectors update

--- pkg/connectors/contracts/stuff/Thena.sol
+++ pkg/connectors/contracts/stuff/Thena.sol
@@ -366,6 +366,28 @@ interface IAlgebraPool {
         bool unlocked
     );
 
+    /**
+     * @notice Returns the information about a position by the position's key
+     * @dev This is a public mapping of structures, so the `return` natspec tags are omitted.
+     * @param key The position's key is a hash of a preimage composed by the owner, bottomTick and topTick
+     * @return liquidityAmount The amount of liquidity in the position;
+     * Returns lastLiquidityAddTimestamp Timestamp of last adding of liquidity;
+     * Returns innerFeeGrowth0Token Fee growth of token0 inside the tick range as of the last mint/burn/poke;
+     * Returns innerFeeGrowth1Token Fee growth of token1 inside the tick range as of the last mint/burn/poke;
+     * Returns fees0 The computed amount of token0 owed to the position as of the last mint/burn/poke;
+     * Returns fees1 The computed amount of token1 owed to the position as of the last mint/burn/poke
+     */
+    function positions(bytes32 key)
+        external
+        view
+        returns (
+        uint128 liquidityAmount,
+        uint32 lastLiquidityAddTimestamp,
+        uint256 innerFeeGrowth0Token,
+        uint256 innerFeeGrowth1Token,
+        uint128 fees0,
+        uint128 fees1
+        );
 }
 
 interface IBribe {
