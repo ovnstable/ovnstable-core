d269ae6d3735b061a24420ffd266c5d158e76fd5
Naran Mucherinov <n.mucherinov@gmail.com>
2023-05-12 23:57:06 +0700

    Deploy StrategyEtsKappa Optimism

---
+++ pkg/proposals/proposal_2023_05_13.js
@@ -0,0 +1,29 @@
+const {getContract} = require("@overnight-contracts/common/utils/script-utils");
+const {createProposal, testProposal} = require("@overnight-contracts/common/utils/governance");
+
+async function main() {
+
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+
+    let PortfolioManager = await getContract('PortfolioManager', 'optimism');
+    let StrategyEtsKappa = await getContract('StrategyEtsKappa', 'optimism');
+
+    addresses.push(PortfolioManager.address);
+    values.push(0);
+    abis.push(PortfolioManager.interface.encodeFunctionData('addStrategy', [StrategyEtsKappa.address]));
+
+
+    await createProposal(addresses, values, abis);
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
---
+++ pkg/strategies/optimism/deploy/46_strategy_ets_kappa.js
@@ -0,0 +1,27 @@
+const {deployProxyMulti} = require("@overnight-contracts/common/utils/deployProxy");
+const {deploySection, settingSection} = require("@overnight-contracts/common/utils/script-utils");
+const {OPTIMISM} = require("@overnight-contracts/common/utils/assets");
+
+module.exports = async ({deployments}) => {
+    const {save} = deployments;
+
+    await deploySection(async (name) => {
+        await deployProxyMulti(name, 'StrategyEts', deployments, save, null);
+    });
+
+    await settingSection(async (strategy) => {
+        await (await strategy.setParams(await getParams())).wait();
+    });
+};
+
+async function getParams() {
+    return {
+        asset: OPTIMISM.usdc,
+        rebaseToken: '0x909CC74fa5FC832Cc1C5c7D2d9DDD1574127D231',
+        hedgeExchanger: '0x2533A096f9716C9403A59d7a1877581D1CFC8AC9',
+    };
+}
+
+module.exports.tags = ['StrategyEtsKappa'];
+module.exports.getParams = getParams;
+module.exports.strategyEtsThetaDai = getParams;
