dff138a29e3db16e313038f244d5f1d5ddff45c8
Naran Mucherinov <n.mucherinov@gmail.com>
2023-05-12 16:09:34 +0700

    Deploy StrategyEtsThetaDai

---
+++ pkg/proposals/proposal_2023_05_12.js
@@ -0,0 +1,29 @@
+const {getContract} = require("@overnight-contracts/common/utils/script-utils");
+const {createProposal, testProposal} = require("@overnight-contracts/common/utils/governance");
+
+async function main() {
+
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+
+    let PortfolioManager = await getContract('PortfolioManager', 'arbitrum_dai');
+    let StrategyEtsThetaDai = await getContract('StrategyEtsThetaDai', 'arbitrum_dai');
+
+    addresses.push(PortfolioManager.address);
+    values.push(0);
+    abis.push(PortfolioManager.interface.encodeFunctionData('addStrategy', [StrategyEtsThetaDai.address]));
+
+
+    await createProposal(addresses, values, abis);
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
