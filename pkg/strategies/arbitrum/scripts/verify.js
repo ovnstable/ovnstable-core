f1af8fa080f2f74261be9f02139df2a380921b32
Naran Mucherinov <n.mucherinov@gmail.com>
2023-06-30 22:20:15 +0900

    Deploy StrategyBalancerUsdc, StrategyBalancerDai, StrategyAuraDaiUsdcUsdt

--- pkg/strategies/arbitrum/scripts/verify.js
+++ pkg/strategies/arbitrum/scripts/verify.js
@@ -1,7 +1,7 @@
 const {verify} = require("@overnight-contracts/common/utils/verify-utils");
 
 async function main() {
-    let items = ["StrategyUsdPlusDai"];
+    let items = ["StrategyAuraDaiUsdcUsdt"];
     await verify(items);
 }
 
