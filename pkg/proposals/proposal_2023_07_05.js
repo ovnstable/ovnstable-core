44c33dc0e275d7a958f213fc9a145c264a24f086
Valpaq <viktor.paramonov2000@gmail.com>
2023-07-05 13:11:17 +0300

    proposal for etsEpsilon

---
+++ pkg/proposals/proposal_2023_07_05.js
@@ -0,0 +1,28 @@
+const { getContract, execTimelock } = require("@overnight-contracts/common/utils/script-utils");
+const { createProposal, testProposal } = require("@overnight-contracts/common/utils/governance");
+
+async function main() {
+
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+
+    let PortfolioManager = await getContract('PortfolioManager', 'bsc_usdt');
+    let StrategyEtsEpsilon = await getContract('StrategyEtsEpsilon', 'bsc_usdt');
+
+    addresses.push(PortfolioManager.address);
+    values.push(0);
+    abis.push(PortfolioManager.interface.encodeFunctionData('addStrategy', [StrategyEtsEpsilon.address]));
+
+
+    await createProposal(addresses, values, abis);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
