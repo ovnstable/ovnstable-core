7e7e582a61427038f507d7ded519c79860a37104
Naran Mucherinov <n.mucherinov@gmail.com>
2023-07-26 21:17:36 +0900

    SettingZksyncPayoutListener

--- pkg/core/deploy/chains/zksync/02_setting_zksyncPayoutListener.js
+++ pkg/core/deploy/chains/zksync/02_setting_zksyncPayoutListener.js
@@ -2,7 +2,7 @@ const {ethers} = require("hardhat");
 
 let {BSC, COMMON} = require('@overnight-contracts/common/utils/assets');
 const {getContract} = require("@overnight-contracts/common/utils/script-utils");
-const {createSkim, createBribe, createSkimTo, createCustom, createSkimToWithFee, createSync} = require("@overnight-contracts/common/utils/payoutListener");
+const {createSkim, createBribe, createSkimTo, createCustom, createSkimToWithFee} = require("@overnight-contracts/common/utils/payoutListener");
 const {Roles} = require("@overnight-contracts/common/utils/roles");
 const {ZERO_ADDRESS} = require("@openzeppelin/test-helpers/src/constants");
 
@@ -19,13 +19,12 @@ module.exports = async ({getNamedAccounts, deployments}) => {
 
     let items = [];
 
-    items.push(...velocore());
-    // items.push(...syncSwap());
-   items.push(...vesync());
-   items.push(...mute());
-    items.push(...ezkalibur());
+//    items.push(...velocore());
+//    items.push(...syncSwap());
+//    items.push(...vesync());
+//    items.push(...mute());
+//    items.push(...ezkalibur());
 
-    await (await pl.removeItems()).wait();
     await (await pl.addItems(items)).wait();
 
     console.log('ZksyncPayoutListener setting done');
@@ -39,7 +38,7 @@ module.exports = async ({getNamedAccounts, deployments}) => {
         let dex = 'VeSync';
 
         let items = [];
-        items.push(createSync('0x16D0fC836FED0f645d832Eacc65106dDB67108Ef', usdPlus.address, 'USDC/USD+', dex));
+        items.push(createSkim('0x16D0fC836FED0f645d832Eacc65106dDB67108Ef', usdPlus.address, 'USDC/USD+', dex));
 
         return items;
     }
@@ -49,7 +48,7 @@ module.exports = async ({getNamedAccounts, deployments}) => {
         let dex = 'Mute';
 
         let items = [];
-        items.push(createSync('0x3848dbd3EAc429497abd464A18fBEC78EF76f750', usdPlus.address, 'USDC/USD+', dex));
+        items.push(createSkim('0x3848dbd3EAc429497abd464A18fBEC78EF76f750', usdPlus.address, 'USDC/USD+', dex));
 
         return items;
     }
@@ -59,7 +58,7 @@ module.exports = async ({getNamedAccounts, deployments}) => {
         let dex = 'Velocore';
 
         let items = [];
-        items.push(createSync('0x4b9f00860d7f42870addeb687fa4e47062df71d9', usdPlus.address, 'USDC/USD+', dex, '0x691e8644Efc7FDad4e502c9370eF5F43af34C647'));
+        items.push(createBribe('0x4b9f00860d7f42870addeb687fa4e47062df71d9', usdPlus.address, 'USDC/USD+', dex, '0x691e8644Efc7FDad4e502c9370eF5F43af34C647'));
 
         return items;
     }
@@ -80,7 +79,7 @@ module.exports = async ({getNamedAccounts, deployments}) => {
         let to = '0x5f112507359b2EdfB02FE8A7e9aCD66008b8343a';
 
         let items = [];
-        items.push(createSync('0x0DfD96f6DbA1F3AC4ABb4D5CA36ce7Cb48767a13', usdPlus.address, 'USDC/USD+', dex, to, 20, COMMON.rewardWallet));
+        items.push(createSkimToWithFee('0x0DfD96f6DbA1F3AC4ABb4D5CA36ce7Cb48767a13', usdPlus.address, 'USDC/USD+', dex, to, 20, COMMON.rewardWallet));
 
         return items;
     }
