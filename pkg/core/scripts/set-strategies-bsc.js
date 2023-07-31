ce6e973cfd1c4a66be7c2f22c8e753b4413bd88e
Naran Mucherinov <n.mucherinov@gmail.com>
2023-07-31 22:13:48 +0700

    test set-strategies-bsc.js

--- pkg/core/scripts/set-strategies-bsc.js
+++ pkg/core/scripts/set-strategies-bsc.js
@@ -20,7 +20,7 @@ async function main() {
                 "strategy": "0x621409Ad21B486eA8688c5608abc904Cd8DB8e9b",
                 "name": "Wombex USDC",
                 "minWeight": 0,
-                "targetWeight": 43.5,
+                "targetWeight": 0,
                 "maxWeight": 100,
                 "riskFactor": 0,
                 "enabled": false,
@@ -30,7 +30,7 @@ async function main() {
                 "strategy": "0xFe7f3FEa8972313F859194EE00158798be3ED108",
                 "name": "Wombex BUSD",
                 "minWeight": 0,
-                "targetWeight": 9.3,
+                "targetWeight": 0,
                 "maxWeight": 100,
                 "riskFactor": 0,
                 "enabled": false,
@@ -48,27 +48,27 @@ async function main() {
             },
             {
                 "strategy": "0x970D50d09F3a656b43E11B0D45241a84e3a6e011",
-                "name": "Ellipsis Dot",
+                "name": "Ellipsis DotDot",
                 "minWeight": 0,
-                "targetWeight": 44.3,
+                "targetWeight": 18,
                 "maxWeight": 100,
                 "riskFactor": 0,
-                "enabled": false,
+                "enabled": true,
                 "enabledReward": true
             },
             {
                 "strategy": "0x27D9425bE1375E7CBB38b9FAaDbD446a0196E6eD",
                 "name": "Venus USDC",
                 "minWeight": 0,
-                "targetWeight": 2.9,
+                "targetWeight": 16.53,
                 "maxWeight": 100,
                 "riskFactor": 0,
                 "enabled": true,
-                "enabledReward": false
+                "enabledReward": true
             },
             {
                 "strategy": "0xC7998aCE498eb987D1Ff625Af7Fb7a0449C1C6EE",
-                "name": "BetaOp",
+                "name": "BetaBsc",
                 "minWeight": 0,
                 "targetWeight": 0,
                 "maxWeight": 100,
@@ -78,34 +78,67 @@ async function main() {
             },
             {
                 "strategy": "0x049915356d236a93ff235098042fb27301a9BDf4",
-                "name": "GammaOp",
+                "name": "GammaBsc",
                 "minWeight": 0,
                 "targetWeight": 0,
                 "maxWeight": 100,
                 "riskFactor": 0,
                 "enabled": false,
                 "enabledReward": false
+            },
+            {
+                "strategy": "0xb4b149F2190773076773A5E88F6eA582c613983e",
+                "name": "Magpie USDC",
+                "minWeight": 0,
+                "targetWeight": 0,
+                "maxWeight": 100,
+                "riskFactor": 0,
+                "enabled": false,
+                "enabledReward": true
+            },
+            {
+                "strategy": "0xE84BEB646A7bF50AcA89684542A1e7ed4B30Bfd1",
+                "name": "Magpie BUSD",
+                "minWeight": 0,
+                "targetWeight": 0,
+                "maxWeight": 100,
+                "riskFactor": 0,
+                "enabled": false,
+                "enabledReward": true
+            },
+            {
+            "strategy": "0x872Ac5EdAf732E140168f8c7645C47817a04D60D",
+                "name": "Thena USDC/USDT",
+                "minWeight": 0,
+                "targetWeight": 65.47,
+                "maxWeight": 100,
+                "riskFactor": 0,
+                "enabled": false,
+                "enabledReward": true
             }
         ];
 
         weights = await convertWeights(weights);
+        let price = await getPrice();
+
+        let pm = await getContract('PortfolioManager');
+        await pm.connect(timelock).grantRole(await pm.PORTFOLIO_AGENT_ROLE(), (await initWallet()).address, price);
+        console.log("PORTFOLIO_AGENT_ROLE granted")
 
         await showM2M();
-        let pm = await getContract('PortfolioManager');
-        let price = await getPrice();
-        await pm.connect(timelock).grantRole(await pm.PORTFOLIO_AGENT_ROLE(), (await initWallet()).address, price);
-        console.log("role granted")
+
         await pm.setStrategyWeights(weights, price);
         console.log("setStrategyWeights done")
-//        await pm.balance();
+
+        await showM2M();
+
+        await pm.balance(price);
+        console.log("balance done")
+
         await showM2M();
     });
 
-    // await changeWeightsAndBalance(weights);
-
-
 }
-
 
 
 main()
