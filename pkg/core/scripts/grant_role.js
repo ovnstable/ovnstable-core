66b989c425549b4feb4217e14d896760da32ccf3
Naran Mucherinov <n.mucherinov@gmail.com>
2023-08-01 15:45:47 +0700

    Deploy StrategyUsdcUsdtPlus

--- pkg/core/scripts/grant_role.js
+++ pkg/core/scripts/grant_role.js
@@ -1,12 +1,15 @@
-const {getContract} = require("@overnight-contracts/common/utils/script-utils");
+const {getContract, getPrice} = require("@overnight-contracts/common/utils/script-utils");
 const {createProposal} = require("@overnight-contracts/common/utils/governance");
 const {Roles} = require("@overnight-contracts/common/utils/roles");
 
 async function main(){
 
-    let exchange = await getContract('Exchange');
+    let exchange = await getContract('Exchange', 'bsc_usdt');
+    let StrategyUsdcUsdtPlus = await getContract('StrategyUsdcUsdtPlus', 'bsc');
 
-    await (await exchange.grantRole(Roles.FREE_RIDER_ROLE, "0xd05c15AA8D3E8AEb9833826AbC6C5C591C762D9d")).wait();
+    let price = await getPrice();
+    await (await exchange.grantRole(Roles.FREE_RIDER_ROLE, StrategyUsdcUsdtPlus.address, price)).wait();
+    console.log("grantRole done")
 
 }
 
