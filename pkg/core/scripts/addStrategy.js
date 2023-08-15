00b9989c2b1c7bf0ecfda6decd8a705543feeb98
y.pavlov <lenvonder12vorh@gmail.com>
2023-08-15 12:43:25 +0300

    Update addStrategy.js

--- pkg/core/scripts/addStrategy.js
+++ pkg/core/scripts/addStrategy.js
@@ -1,14 +1,26 @@
-const {getContract, getPrice} = require("@overnight-contracts/common/utils/script-utils");
-const {createProposal} = require("@overnight-contracts/common/utils/governance");
+const {getContract, getPrice, showM2M} = require("@overnight-contracts/common/utils/script-utils");
+const {createProposal, testProposal} = require("@overnight-contracts/common/utils/governance");
 
 async function main() {
 
     let pm = await getContract('PortfolioManager');
-    let strategy = await getContract('StrategyEtsGamma');
 
-    let price = await getPrice();
-    await (await pm.addStrategy(strategy.address, price)).wait();
-    console.log('Add strategy done');
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+    addresses.push(pm.address);
+    values.push(0);
+    abis.push(pm.interface.encodeFunctionData('addStrategy', ['0x48d49a208BA0239198083E274836Ba1B9Bef8722']));
+
+    addresses.push(pm.address);
+    values.push(0);
+    abis.push(pm.interface.encodeFunctionData('addStrategy', ['0x48d49a208BA0239198083E274836Ba1B9Bef8722']));
+
+    // await showM2M();
+    // await testProposal(addresses, values, abis);
+    // await showM2M();
+    await createProposal(addresses, values, abis);
 }
 
 
