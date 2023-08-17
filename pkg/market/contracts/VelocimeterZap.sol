b2bc015b949df454a18d6fa19ed18f3e90953483
valpaq <71923310+valpaq@users.noreply.github.com>
2023-08-17 18:09:08 +0300

    Dev 1799 velocimeter zap (#229)
    * DEV-1799
    Added Velocimeter Zap
    
    ---------
    
    Co-authored-by: dmitry <dmitry.timoschuk@gmail.com>
    Co-authored-by: Valpaq <viktor.paramonov2000@gmail.com>

---
+++ pkg/market/contracts/VelocimeterZap.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "./OdosZap.sol";
+
+import "@overnight-contracts/connectors/contracts/stuff/Velocimeter.sol";
+import "hardhat/console.sol";
+
+contract VelocimeterZap is OdosZap {
+    IRouter public velocimeterRouter;
+
+    struct ZapParams {
+        address velocimeterRouter;
+        address odosRouter;
+    }
+
+    struct VelocimeterZapInParams {
+        address gauge;
+        uint256[] amountsOut;
+    }
+
+    function setParams(ZapParams memory params) external onlyAdmin {
+        require(params.velocimeterRouter != address(0), "Zero address not allowed");
+        require(params.odosRouter != address(0), "Zero address not allowed");
+
+        velocimeterRouter = IRouter(params.velocimeterRouter);
+        odosRouter = params.odosRouter;
+    }
+
+    function zapIn(SwapData memory swapData, VelocimeterZapInParams memory velocimeterData) external {
+        _prepareSwap(swapData);
+        _swap(swapData);
+
+        IGauge gauge = IGauge(velocimeterData.gauge);
+        address _token = gauge.stake();
+        IPair pair = IPair(_token);
+        (address token0, address token1) = pair.tokens();
+
+        address[] memory tokensOut = new address[](2);
+        tokensOut[0] = token0;
+        tokensOut[1] = token1;
+        uint256[] memory amountsOut = new uint256[](2);
+
+        for (uint256 i = 0; i < tokensOut.length; i++) {
+            IERC20 asset = IERC20(tokensOut[i]);
+
+            if (velocimeterData.amountsOut[i] > 0) {
+                asset.transferFrom(msg.sender, address(this), velocimeterData.amountsOut[i]);
+            }
+            amountsOut[i] = asset.balanceOf(address(this));
+        }
+
+        _addLiquidity(pair, tokensOut, amountsOut);
+        _returnToUser(pair);
+    }
+
+    function getProportion(
+        address _gauge
+    ) public view returns (uint256 token0Amount, uint256 token1Amount, uint256 denominator) {
+        IGauge gauge = IGauge(_gauge);
+        address _token = gauge.stake();
+        IPair pair = IPair(_token);
+        (uint256 reserve0, uint256 reserve1, ) = pair.getReserves();
+        (address token0, address token1) = pair.tokens();
+        uint256 dec0 = IERC20Metadata(token0).decimals();
+        uint256 dec1 = IERC20Metadata(token1).decimals();
+        denominator = 10 ** (dec0 > dec1 ? dec0 : dec1);
+        token0Amount = reserve0 * (denominator / (10 ** dec0));
+        token1Amount = reserve1 * (denominator / (10 ** dec1));
+    }
+
+    function _addLiquidity(IPair pair, address[] memory tokensOut, uint256[] memory amountsOut) internal {
+        (uint256 reserve0, uint256 reserve1, ) = pair.getReserves();
+        (uint256 tokensAmount0, uint256 tokensAmount1) = getAmountToSwap(
+            amountsOut[0],
+            amountsOut[1],
+            reserve0,
+            reserve1,
+            10 ** IERC20Metadata(tokensOut[0]).decimals(),
+            10 ** IERC20Metadata(tokensOut[1]).decimals()
+        );
+
+        IERC20 asset0 = IERC20(tokensOut[0]);
+        IERC20 asset1 = IERC20(tokensOut[1]);
+        asset0.approve(address(velocimeterRouter), tokensAmount0);
+        asset1.approve(address(velocimeterRouter), tokensAmount1);
+
+        uint256 amountAsset0Before = asset0.balanceOf(address(this));
+        uint256 amountAsset1Before = asset1.balanceOf(address(this));
+
+        velocimeterRouter.addLiquidity(
+            tokensOut[0],
+            tokensOut[1],
+            pair.stable(),
+            tokensAmount0,
+            tokensAmount1,
+            OvnMath.subBasisPoints(tokensAmount0, stakeSlippageBP),
+            OvnMath.subBasisPoints(tokensAmount1, stakeSlippageBP),
+            address(this),
+            block.timestamp
+        );
+
+        uint256 amountAsset0After = asset0.balanceOf(address(this));
+        uint256 amountAsset1After = asset1.balanceOf(address(this));
+
+        if (amountAsset0After > 0) {
+            asset0.transfer(msg.sender, amountAsset0After);
+        }
+
+        if (amountAsset1After > 0) {
+            asset1.transfer(msg.sender, amountAsset1After);
+        }
+
+        uint256[] memory amountsPut = new uint256[](2);
+        amountsPut[0] = amountAsset0Before - amountAsset0After;
+        amountsPut[1] = amountAsset1Before - amountAsset1After;
+
+        uint256[] memory amountsReturned = new uint256[](2);
+        amountsReturned[0] = amountAsset0After;
+        amountsReturned[1] = amountAsset1After;
+        emit PutIntoPool(amountsPut, tokensOut);
+        emit ReturnedToUser(amountsReturned, tokensOut);
+    }
+
+    function getAmountToSwap(
+        uint256 amount0,
+        uint256 amount1,
+        uint256 reserve0,
+        uint256 reserve1,
+        uint256 denominator0,
+        uint256 denominator1
+    ) internal pure returns (uint256 newAmount0, uint256 newAmount1) {
+        if ((reserve0 * 100) / denominator0 > (reserve1 * 100) / denominator1) {
+            newAmount1 = (reserve1 * amount0) / reserve0;
+            // 18 + 6 - 6
+            newAmount1 = newAmount1 > amount1 ? amount1 : newAmount1;
+            newAmount0 = (newAmount1 * reserve0) / reserve1;
+            // 18 + 6 - 18
+        } else {
+            newAmount0 = (reserve0 * amount1) / reserve1;
+            newAmount0 = newAmount0 > amount0 ? amount0 : newAmount0;
+            newAmount1 = (newAmount0 * reserve1) / reserve0;
+        }
+    }
+
+    function _returnToUser(IPair pair) internal {
+        uint256 pairBalance = pair.balanceOf(address(this));
+        pair.transfer(msg.sender, pairBalance);
+    }
+}
---
+++ pkg/market/deploy/09_velocimeter_zap.js
@@ -0,0 +1,24 @@
+const { deployProxy } = require("@overnight-contracts/common/utils/deployProxy");
+const { BASE } = require("@overnight-contracts/common/utils/assets");
+const { ethers } = require("hardhat");
+const { Roles } = require("@overnight-contracts/common/utils/roles");
+
+module.exports = async ({ deployments }) => {
+    const { save } = deployments;
+
+    await deployProxy('VelocimeterZap', deployments, save);
+    console.log("VelocimeterZap deploy done()");
+
+    let params = {
+        odosRouter: BASE.odosRouter,
+        velocimeterRouter: BASE.velocimeterRouter
+    }
+
+    let zap = await ethers.getContract('VelocimeterZap');
+    await (await zap.grantRole(Roles.DEFAULT_ADMIN_ROLE, '0x5CB01385d3097b6a189d1ac8BA3364D900666445')).wait();
+
+    await (await zap.setParams(params)).wait();
+    console.log('VelocimeterZap setParams done()');
+};
+
+module.exports.tags = ['VelocimeterZap'];
