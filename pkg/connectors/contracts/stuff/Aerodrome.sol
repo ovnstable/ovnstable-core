469199a50dd402d358f593bdb621729cbf65253f
Naran Mucherinov <n.mucherinov@gmail.com>
2023-08-29 18:26:46 +0700

    connectors 0.0.82

--- pkg/connectors/contracts/stuff/Aerodrome.sol
+++ pkg/connectors/contracts/stuff/Aerodrome.sol
@@ -3,6 +3,12 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
+interface IWETH is IERC20 {
+    function deposit() external payable;
+
+    function withdraw(uint256) external;
+}
+
 interface IRouter {
     struct Route {
         address from;
