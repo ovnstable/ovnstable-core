b0ced24ba7ff8b4a127da3accb1807072c4606bb
Naran Mucherinov <n.mucherinov@gmail.com>
2023-08-31 16:47:09 +0700

    DEV-2018. Add StrategyCompoundUsdbc

---
+++ pkg/proposals/proposal_2023_08_31.js
@@ -0,0 +1,32 @@
+const { getContract, execTimelock, showM2M} = require("@overnight-contracts/common/utils/script-utils");
+const { createProposal, testProposal, testStrategy} = require("@overnight-contracts/common/utils/governance");
+
+async function main() {
+
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+
+    let PortfolioManager = await getContract('PortfolioManager', 'base');
+    let StrategyCompoundUsdbc = await getContract('StrategyCompoundUsdbc', 'base');
+
+    addresses.push(PortfolioManager.address);
+    values.push(0);
+    abis.push(PortfolioManager.interface.encodeFunctionData('addStrategy', [StrategyCompoundUsdbc.address]));
+
+    addresses.push(PortfolioManager.address);
+    values.push(0);
+    abis.push(PortfolioManager.interface.encodeFunctionData('setCashStrategy', [StrategyCompoundUsdbc.address]));
+
+
+    await createProposal(addresses, values, abis);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
