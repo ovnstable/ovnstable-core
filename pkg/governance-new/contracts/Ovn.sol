d4c4d01f4f76154aeeff03c37330ae8a9b6e019a
y.pavlov <lenvonder12vorh@gmail.com>
2023-09-06 14:48:36 +0300

    Remove Axelar

--- pkg/governance-new/contracts/Ovn.sol
+++ pkg/governance-new/contracts/Ovn.sol
@@ -9,21 +9,10 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpg
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-import {ITokenManager} from "@axelar-network/interchain-token-service/contracts/interfaces/ITokenManager.sol";
-import {ITokenManagerType} from "@axelar-network/interchain-token-service/contracts/interfaces/ITokenManagerType.sol";
-import {IInterchainToken} from "@axelar-network/interchain-token-service/contracts/interfaces/IInterchainToken.sol";
-import {IInterchainTokenService} from "@axelar-network/interchain-token-service/contracts/interfaces/IInterchainTokenService.sol";
-import {AddressBytesUtils} from "@axelar-network/interchain-token-service/contracts/libraries/AddressBytesUtils.sol";
-
-import "hardhat/console.sol";
-
 contract Ovn is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, AccessControlUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, UUPSUpgradeable {
-    using AddressBytesUtils for address;
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    ITokenManager public tokenManager;
-    IInterchainTokenService public interchainTokenService;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -52,68 +41,6 @@ contract Ovn is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, Acces
     }
 
 
-    function initAxelar(address tokenServiceAddress) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        interchainTokenService = IInterchainTokenService(tokenServiceAddress);
-        deployTokenManager("OVN");
-    }
-
-    function deployTokenManager(bytes32 salt) internal {
-
-        bytes memory params = interchainTokenService.getParamsMintBurn(
-            AddressBytesUtils.toBytes(msg.sender),
-            address(this)
-        );
-
-        bytes32 tokenId = interchainTokenService.deployCustomTokenManager(
-            salt,
-            ITokenManagerType.TokenManagerType.MINT_BURN,
-            params
-        );
-        tokenManager = ITokenManager(interchainTokenService.getTokenManagerAddress(tokenId));
-        _grantRole(MINTER_ROLE, address(tokenManager));
-    }
-
-
-    function interchainTransfer(
-        string calldata destinationChain,
-        bytes calldata recipient,
-        uint256 amount,
-        bytes calldata metadata
-    ) external payable {
-        address sender = msg.sender;
-
-        // Metadata semantics are defined by the token service and thus should be passed as-is.
-        tokenManager.transmitInterchainTransfer{value: msg.value}(
-            sender,
-            destinationChain,
-            recipient,
-            amount,
-            metadata
-        );
-    }
-
-
-    function interchainTransferFrom(
-        address sender,
-        string calldata destinationChain,
-        bytes calldata recipient,
-        uint256 amount,
-        bytes calldata metadata
-    ) external payable {
-        uint256 _allowance = allowance(sender, msg.sender);
-
-        if (_allowance != type(uint256).max) {
-            _approve(sender, msg.sender, _allowance - amount);
-        }
-
-        tokenManager.transmitInterchainTransfer{value: msg.value}(
-            sender,
-            destinationChain,
-            recipient,
-            amount,
-            metadata
-        );
-    }
 
     function _authorizeUpgrade(address newImplementation)
     internal
--- pkg/governance-new/deploy/01_ovn.js
+++ pkg/governance-new/deploy/01_ovn.js
@@ -4,17 +4,6 @@ const hre = require("hardhat");
 module.exports = async ({deployments}) => {
     const {save} = deployments;
     await deployProxy('Ovn', deployments, save);
-
-
-    let interchainTokenService;
-    switch (hre.network.name){
-        case "base":
-            interchainTokenService = "";
-            break;
-        default:
-            throw new Error('Please add support chain');
-
-    }
 };
 
 module.exports.tags = ['Ovn'];
