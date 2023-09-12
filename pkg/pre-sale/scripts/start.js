554797e195224e384b5eccafe92af7e6ed298672
y.pavlov <lenvonder12vorh@gmail.com>
2023-09-12 16:33:13 +0300

    Update start.js

--- pkg/pre-sale/scripts/start.js
+++ pkg/pre-sale/scripts/start.js
@@ -1,11 +1,19 @@
-const {toAsset} = require("@overnight-contracts/common/utils/decimals");
+const {toAsset, toE18} = require("@overnight-contracts/common/utils/decimals");
 
 const {getContract, showM2M, getCoreAsset, transferETH, initWallet, getWalletAddress} = require("@overnight-contracts/common/utils/script-utils");
 
 
 async function main() {
 
+
+    let salesToken = await getContract('SalesToken');
+
+    let wallet = await initWallet();
+    await (await salesToken.mint(wallet.address, toE18(10_000)))
+
     let overflowICO= await getContract('OverflowICO');
+
+    await (await salesToken.approve(overflowICO.address, toE18(10_000))).wait();
 
     console.log(`Start: ${await overflowICO.started()}`);
     await (await overflowICO.start()).wait();
