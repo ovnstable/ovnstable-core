aeb145211f4d7cc0bb2f797dd6657eea5087d8da
y.pavlov <lenvonder12vorh@gmail.com>
2023-09-14 16:30:30 +0300

    Add OvnGovernor.sol, empty MotherTimelock.sol

---
+++ pkg/governance-new/contracts/MotherTimelock.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+import "@openzeppelin/contracts-upgradeable/governance/TimelockControllerUpgradeable.sol";
+
+
+contract MotherTimelock {
+    constructor(){
+
+    }
+}
