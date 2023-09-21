6ae3e1c7373f69bc7dc35ad46bb41a042a718026
y.pavlov <lenvonder12vorh@gmail.com>
2023-09-21 21:59:58 +0300

    Add scripts

--- pkg/governance-new/deploy/06_agent_timelock.js
+++ pkg/governance-new/deploy/06_agent_timelock.js
@@ -7,7 +7,7 @@ module.exports = async ({deployments}) => {
     const {save} = deployments;
 
     const motherTimelock = '0xA4fc2F25CA4dFEc08F07eE92d3173BA21A01E9f8';
-    const chainId = "10";
+    const motherChainId = "optimism";
 
     let ovnAgent;
     let gateway;
@@ -17,13 +17,16 @@ module.exports = async ({deployments}) => {
             ovnAgent = '0x410FcB956B021b1ee51198F8Eb944A979C428d22';
             gateway = ZERO_ADDRESS;
             break;
-
+        case "arbitrum":
+            ovnAgent = "0x5cBb2167677c2259F421457542f6E5A805B1FF2F";
+            gateway = "0xe432150cce91c13a887f7D836923d5597adD8E31";
+            break
         default:
             throw new Error('Unknown chain');
     }
 
     let params = {
-        args: [gateway, motherTimelock, ovnAgent, chainId]
+        args: [gateway, motherTimelock, ovnAgent, motherChainId]
     }
 
     await deployProxy('AgentTimelock', deployments, save, params);
