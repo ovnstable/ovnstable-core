fd2638506867ca0cacfc1bb0a327f35ca4304851
y.pavlov <lenvonder12vorh@gmail.com>
2023-09-21 15:12:22 +0300

    Update scripts governance-new

---
+++ pkg/governance-new/scripts/agent_settings.js
@@ -0,0 +1,26 @@
+const {getContract, initWallet} = require("@overnight-contracts/common/utils/script-utils");
+const {createProposal} = require("@overnight-contracts/common/utils/governance");
+const {fromE18} = require("@overnight-contracts/common/utils/decimals");
+const {ethers} = require("hardhat");
+
+async function main() {
+
+    let timelock = await getContract('AgentTimelock');
+
+    console.log('Timelock: getMinDelay:    ' + await timelock.getMinDelay());
+    console.log('Timelock: gateway:        ' + await timelock.gateway());
+    console.log('Timelock: ovnAgent:       ' + await timelock.ovnAgent());
+    console.log('Timelock: motherTimelock: ' + await timelock.motherTimelock());
+
+}
+
+
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
