d863fad41d39f0ff3879523436b7e505d5fea824
Naran Mucherinov <n.mucherinov@gmail.com>
2023-09-22 19:30:28 +0700

    SettingBasePayoutListener

--- pkg/core/deploy/chains/base/12_setting_basePayoutListener.js
+++ pkg/core/deploy/chains/base/12_setting_basePayoutListener.js
@@ -100,9 +100,9 @@ module.exports = async () => {
         let dex = 'Aerodrome';
 
         let items = [];
-        items.push(createBribeWithFee('0x1b05e4e814b3431a48b8164c41eaC834d9cE2Da6', usdPlus.address, 'sAMM-DAI+/USD+', dex, '0x64952300B2544B3B48B7fa2effAc01d31F6C1370', 20, COMMON.rewardWallet));
-        items.push(createBribeWithFee('0x1b05e4e814b3431a48b8164c41eaC834d9cE2Da6', daiPlus.address, 'sAMM-DAI+/USD+', dex, '0x64952300B2544B3B48B7fa2effAc01d31F6C1370', 20, COMMON.rewardWallet));
-        items.push(createBribeWithFee('0x4a3636608d7Bc5776CB19Eb72cAa36EbB9Ea683B', usdPlus.address, 'sAMM-USD+/USDbC', dex, '0x1Cb79D8cdB7E621f1545545D50c8561dF4003D38', 20, COMMON.rewardWallet));
+        items.push(createSkim('0x1b05e4e814b3431a48b8164c41eaC834d9cE2Da6', usdPlus.address, 'sAMM-DAI+/USD+', dex));
+        items.push(createSkim('0x1b05e4e814b3431a48b8164c41eaC834d9cE2Da6', daiPlus.address, 'sAMM-DAI+/USD+', dex));
+        items.push(createSkim('0x4a3636608d7Bc5776CB19Eb72cAa36EbB9Ea683B', usdPlus.address, 'sAMM-USD+/USDbC', dex));
         items.push(createBribeWithFee('0xdC0F1F6eCd03ec1C9FFC2A17BaBAbd313477b20E', usdPlus.address, 'vAMM-USD+/USDbC', dex, '0x3CAE2895a4fc57336Fc8FBab40D9e534874B17CE', 20, COMMON.rewardWallet));
         items.push(createBribeWithFee('0x607363389331f4B2D1b955d009506A67c565D75E', usdPlus.address, 'vAMM-USD+/stERN', dex, '0x6CEd86715f74ff109ea7f908eAc78AF4ab2f41ea', 20, COMMON.rewardWallet));
         items.push(createBribeWithFee('0x8E9154AC849e839d60299E85156bcb589De2693A', usdPlus.address, 'sAMM-DOLA/USD+', dex, '0xfC996Abd85Bcf64C3fA7DA20f33278Cd46f25ab7', 20, COMMON.rewardWallet));
