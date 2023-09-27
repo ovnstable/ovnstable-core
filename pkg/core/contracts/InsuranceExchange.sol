f139377b11159a126fa0336b2e8e3ac0e420601a
Nikita Slezkin <venikpapa@gmail.com>
2023-09-27 17:37:45 +0300

    delete cross rate

--- pkg/core/contracts/InsuranceExchange.sol
+++ pkg/core/contracts/InsuranceExchange.sol
@@ -328,7 +328,7 @@ contract InsuranceExchange is IInsuranceExchange, Initializable, AccessControlUp
         uint256 usdDecimals = 10**IERC20Metadata(address(usdAsset)).decimals();
 
         uint256 price = getTwapPrice(); // 1 ovn = price usd+
-        price = price * getPlusTwapPrice(address(usdAsset)) / 1e6;
+        // price = price * getPlusTwapPrice(address(usdAsset)) / 1e6;
             
         if (address(inputAsset) == address(asset)) {
             // ovn --> usdc
@@ -372,7 +372,7 @@ contract InsuranceExchange is IInsuranceExchange, Initializable, AccessControlUp
         IVelodromeTwap velodromeTwap = IVelodromeTwap(poolAddress);
 
         uint256 lastIndex = velodromeTwap.observationLength() - 1;
-        uint256 firstIndex = 0;
+        uint256 firstIndex = lastIndex - 1;
 
         IVelodromeTwap.Observation memory lastObservation = velodromeTwap.observations(lastIndex);
         IVelodromeTwap.Observation memory firstObservation = velodromeTwap.observations(firstIndex);
@@ -384,102 +384,102 @@ contract InsuranceExchange is IInsuranceExchange, Initializable, AccessControlUp
         return ovnPrice;
     }
 
-    function getPlusTwapPrice(address usdAddress) public view returns (uint256) {
+    // function getPlusTwapPrice(address usdAddress) public view returns (uint256) {
 
-        address usdc = 0x7F5c764cBc14f9669B88837ca1490cCa17c31607;
-        address dai = 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1;
+    //     address usdc = 0x7F5c764cBc14f9669B88837ca1490cCa17c31607;
+    //     address dai = 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1;
 
-        address poolAddress;
-        if (usdAddress == usdc) {
-           poolAddress = 0xd95E98fc33670dC033424E7Aa0578D742D00f9C7;
-        } else if (usdAddress == dai) {
-           poolAddress = 0x667002F9DC61ebcBA8Ee1Cbeb2ad04060388f223;
-           revert("Not supported");
-        } else {
-            revert("Not supported");
-        } 
+    //     address poolAddress;
+    //     if (usdAddress == usdc) {
+    //        poolAddress = 0xd95E98fc33670dC033424E7Aa0578D742D00f9C7;
+    //     } else if (usdAddress == dai) {
+    //     //    poolAddress = 0x667002F9DC61ebcBA8Ee1Cbeb2ad04060388f223;
+    //        revert("Not supported");
+    //     } else {
+    //         revert("Not supported");
+    //     } 
          
-        IVelodromeTwap velodromeTwap = IVelodromeTwap(poolAddress);
+    //     IVelodromeTwap velodromeTwap = IVelodromeTwap(poolAddress);
 
-        uint256 lastIndex = velodromeTwap.observationLength() - 1;
-        uint256 firstIndex = 0;
+    //     uint256 lastIndex = velodromeTwap.observationLength() - 1;
+    //     uint256 firstIndex = lastIndex - 1;
 
-        IVelodromeTwap.Observation memory lastObservation = velodromeTwap.observations(lastIndex);
-        IVelodromeTwap.Observation memory firstObservation = velodromeTwap.observations(firstIndex);
+    //     IVelodromeTwap.Observation memory lastObservation = velodromeTwap.observations(lastIndex);
+    //     IVelodromeTwap.Observation memory firstObservation = velodromeTwap.observations(firstIndex);
 
-        uint256 timeElapsed = lastObservation.timestamp - firstObservation.timestamp;
-        uint256 reserve0 = (lastObservation.reserve0Cumulative - firstObservation.reserve0Cumulative) / timeElapsed;
-        uint256 reserve1 = (lastObservation.reserve1Cumulative - firstObservation.reserve1Cumulative) / timeElapsed;
+    //     uint256 timeElapsed = lastObservation.timestamp - firstObservation.timestamp;
+    //     uint256 reserve1 = (lastObservation.reserve0Cumulative - firstObservation.reserve0Cumulative) / timeElapsed;
+    //     uint256 reserve0 = (lastObservation.reserve1Cumulative - firstObservation.reserve1Cumulative) / timeElapsed;
 
-        uint xy = _k(reserve0, reserve1);
-        reserve0 = reserve0 * 1e18 / 1e6;
-        reserve1 = reserve1 * 1e18 / 1e6;
-        uint y = reserve0 - _get_y(1e18 + reserve1, xy, reserve0);
-        uint256 usdPrice = y * 1e6 / 1e18; // 1 usd+ = usdPrice usdc
-        return usdPrice;
-    }
+    //     uint xy = _k(reserve0, reserve1);
+    //     reserve0 = reserve0 * 1e18 / 1e6;
+    //     reserve1 = reserve1 * 1e18 / 1e6;
+    //     uint y = reserve0 - _get_y(1e18 + reserve1, xy, reserve0);
+    //     uint256 usdPrice = y * 1e6 / 1e18; // 1 usd+ = usdPrice usdc
+    //     return usdPrice;
+    // }
 
-    function _k(uint256 x, uint256 y) internal view returns (uint256) {
+    // function _k(uint256 x, uint256 y) internal view returns (uint256) {
         
-            uint256 _x = (x * 1e18) / 1e6;
-            uint256 _y = (y * 1e18) / 1e6;
-            uint256 _a = (_x * _y) / 1e18;
-            uint256 _b = ((_x * _x) / 1e18 + (_y * _y) / 1e18);
-            return (_a * _b) / 1e18; // x3y+y3x >= k
+    //         uint256 _x = (x * 1e18) / 1e6;
+    //         uint256 _y = (y * 1e18) / 1e6;
+    //         uint256 _a = (_x * _y) / 1e18;
+    //         uint256 _b = ((_x * _x) / 1e18 + (_y * _y) / 1e18);
+    //         return (_a * _b) / 1e18; // x3y+y3x >= k
         
-    }
+    // }
 
-    function _get_y(uint256 x0, uint256 xy, uint256 y) internal view returns (uint256) {
-        for (uint256 i = 0; i < 255; i++) {
-            uint256 k = _f(x0, y);
-            if (k < xy) {
-                // there are two cases where dy == 0
-                // case 1: The y is converged and we find the correct answer
-                // case 2: _d(x0, y) is too large compare to (xy - k) and the rounding error
-                //         screwed us.
-                //         In this case, we need to increase y by 1
-                uint256 dy = ((xy - k) * 1e18) / _d(x0, y);
-                if (dy == 0) {
-                    if (k == xy) {
-                        // We found the correct answer. Return y
-                        return y;
-                    }
-                    if (_k(x0, y + 1) > xy) {
-                        // If _k(x0, y + 1) > xy, then we are close to the correct answer.
-                        // There's no closer answer than y + 1
-                        return y + 1;
-                    }
-                    dy = 1;
-                }
-                y = y + dy;
-            } else {
-                uint256 dy = ((k - xy) * 1e18) / _d(x0, y);
-                if (dy == 0) {
-                    if (k == xy || _f(x0, y - 1) < xy) {
-                        // Likewise, if k == xy, we found the correct answer.
-                        // If _f(x0, y - 1) < xy, then we are close to the correct answer.
-                        // There's no closer answer than "y"
-                        // It's worth mentioning that we need to find y where f(x0, y) >= xy
-                        // As a result, we can't return y - 1 even it's closer to the correct answer
-                        return y;
-                    }
-                    dy = 1;
-                }
-                y = y - dy;
-            }
-        }
-        revert("!y");
-    }
+    // function _get_y(uint256 x0, uint256 xy, uint256 y) internal view returns (uint256) {
+    //     for (uint256 i = 0; i < 255; i++) {
+    //         uint256 k = _f(x0, y);
+    //         if (k < xy) {
+    //             // there are two cases where dy == 0
+    //             // case 1: The y is converged and we find the correct answer
+    //             // case 2: _d(x0, y) is too large compare to (xy - k) and the rounding error
+    //             //         screwed us.
+    //             //         In this case, we need to increase y by 1
+    //             uint256 dy = ((xy - k) * 1e18) / _d(x0, y);
+    //             if (dy == 0) {
+    //                 if (k == xy) {
+    //                     // We found the correct answer. Return y
+    //                     return y;
+    //                 }
+    //                 if (_k(x0, y + 1) > xy) {
+    //                     // If _k(x0, y + 1) > xy, then we are close to the correct answer.
+    //                     // There's no closer answer than y + 1
+    //                     return y + 1;
+    //                 }
+    //                 dy = 1;
+    //             }
+    //             y = y + dy;
+    //         } else {
+    //             uint256 dy = ((k - xy) * 1e18) / _d(x0, y);
+    //             if (dy == 0) {
+    //                 if (k == xy || _f(x0, y - 1) < xy) {
+    //                     // Likewise, if k == xy, we found the correct answer.
+    //                     // If _f(x0, y - 1) < xy, then we are close to the correct answer.
+    //                     // There's no closer answer than "y"
+    //                     // It's worth mentioning that we need to find y where f(x0, y) >= xy
+    //                     // As a result, we can't return y - 1 even it's closer to the correct answer
+    //                     return y;
+    //                 }
+    //                 dy = 1;
+    //             }
+    //             y = y - dy;
+    //         }
+    //     }
+    //     revert("!y");
+    // }
 
-    function _f(uint256 x0, uint256 y) internal pure returns (uint256) {
-        uint256 _a = (x0 * y) / 1e18;
-        uint256 _b = ((x0 * x0) / 1e18 + (y * y) / 1e18);
-        return (_a * _b) / 1e18;
-    }
+    // function _f(uint256 x0, uint256 y) internal pure returns (uint256) {
+    //     uint256 _a = (x0 * y) / 1e18;
+    //     uint256 _b = ((x0 * x0) / 1e18 + (y * y) / 1e18);
+    //     return (_a * _b) / 1e18;
+    // }
 
-    function _d(uint256 x0, uint256 y) internal pure returns (uint256) {
-        return (3 * x0 * ((y * y) / 1e18)) / 1e18 + ((((x0 * x0) / 1e18) * x0) / 1e18);
-    }
+    // function _d(uint256 x0, uint256 y) internal pure returns (uint256) {
+    //     return (3 * x0 * ((y * y) / 1e18)) / 1e18 + ((((x0 * x0) / 1e18) * x0) / 1e18);
+    // }
 
 
 }
