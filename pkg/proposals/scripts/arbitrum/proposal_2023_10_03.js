49641f81beaebbbe46fb73cc9313ef7996babd9a
Naran Mucherinov <n.mucherinov@gmail.com>
2023-10-03 18:44:46 +0700

    Grant PORTFOLIO_AGENT_ROLE on all chains

---
+++ pkg/proposals/scripts/arbitrum/proposal_2023_10_03.js
@@ -0,0 +1,36 @@
+const hre = require("hardhat");
+const {getContract} = require("@overnight-contracts/common/utils/script-utils");
+const {createProposal, testProposal} = require("@overnight-contracts/common/utils/governance");
+const {Roles} = require("@overnight-contracts/common/utils/roles");
+
+const path = require('path');
+let filename = path.basename(__filename);
+filename = filename.substring(0, filename.indexOf(".js"));
+
+async function main() {
+
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+    let pm = await getContract('PortfolioManager', 'arbitrum');
+    let pm_dai = await getContract('PortfolioManager', 'arbitrum_dai');
+
+    addresses.push(pm.address);
+    values.push(0);
+    abis.push(pm.interface.encodeFunctionData('grantRole', [Roles.PORTFOLIO_AGENT_ROLE, '0xdf5D41F42f5E4571b35A6A3cdaB994e9B433Fe66']));
+
+    addresses.push(pm_dai.address);
+    values.push(0);
+    abis.push(pm_dai.interface.encodeFunctionData('grantRole', [Roles.PORTFOLIO_AGENT_ROLE, '0xdf5D41F42f5E4571b35A6A3cdaB994e9B433Fe66']));
+
+    await createProposal(filename, addresses, values, abis);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
---
+++ pkg/proposals/scripts/polygon/proposal_2023_10_03.js
@@ -0,0 +1,31 @@
+const hre = require("hardhat");
+const {getContract} = require("@overnight-contracts/common/utils/script-utils");
+const {createProposal, testProposal} = require("@overnight-contracts/common/utils/governance");
+const {Roles} = require("@overnight-contracts/common/utils/roles");
+
+const path = require('path');
+let filename = path.basename(__filename);
+filename = filename.substring(0, filename.indexOf(".js"));
+
+async function main() {
+
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+    let pm = await getContract('PortfolioManager', 'polygon');
+
+    addresses.push(pm.address);
+    values.push(0);
+    abis.push(pm.interface.encodeFunctionData('grantRole', [Roles.PORTFOLIO_AGENT_ROLE, '0xdf5D41F42f5E4571b35A6A3cdaB994e9B433Fe66']));
+
+    await createProposal(filename, addresses, values, abis);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
