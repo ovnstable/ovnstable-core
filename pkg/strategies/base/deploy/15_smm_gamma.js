48982d059abf847d185b0774f9c7e6c400c9efc9
Naran Mucherinov <n.mucherinov@gmail.com>
2023-10-04 18:59:27 +0700

    Deploy StrategySmmGamma

---
+++ pkg/strategies/base/deploy/15_smm_gamma.js
@@ -0,0 +1,29 @@
+const { deployProxyMulti } = require("@overnight-contracts/common/utils/deployProxy");
+const { deploySection, settingSection } = require("@overnight-contracts/common/utils/script-utils");
+const { BASE } = require("@overnight-contracts/common/utils/assets");
+
+let rebaseToken = '0xAC3Ed9F5ea7F6FBD119aaF8C62E25Fe29eC61Bf1';
+let hedgeExchanger = '0x9c452d988BED48b34468999a10ff5163FDC29A3E';
+
+module.exports = async ({ deployments }) => {
+    const { save } = deployments;
+
+    await deploySection(async (name) => {
+        await deployProxyMulti(name, 'StrategyEts', deployments, save, null);
+    });
+
+    await settingSection(async (strategy) => {
+        await (await strategy.setParams(await getParams())).wait();
+    });
+};
+
+async function getParams() {
+    return {
+        asset: BASE.usdbc,
+        rebaseToken: rebaseToken,
+        hedgeExchanger: hedgeExchanger,
+    };
+}
+
+module.exports.tags = ['StrategySmmGamma'];
+module.exports.strategySmmGammaParams = getParams;
