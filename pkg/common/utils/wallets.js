a2cd0bad224201622e8962b36705d69a3e2d3bf2
y.pavlov <lenvonder12vorh@gmail.com>
2023-10-06 14:15:36 +0300

    Add wallets.js

---
+++ pkg/common/utils/wallets.js
@@ -0,0 +1,11 @@
+class Wallets {
+
+    static get DEV() { return '0x5CB01385d3097b6a189d1ac8BA3364D900666445'; }
+    static get DEV_JUNIOR() { return '0x66BC0120b3287f08408BCC76ee791f0bad17Eeef'; }
+    static get TEST_FRONT() { return '0x4473D652fb0b40b36d549545e5fF6A363c9cd686'; }
+}
+
+
+module.exports.Wallets = Wallets;
+
+
