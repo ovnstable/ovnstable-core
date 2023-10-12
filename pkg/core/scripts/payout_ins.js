cc686425801c795e7004ef1a9d0193df99280e1e
y.pavlov <lenvonder12vorh@gmail.com>
2023-10-12 11:15:56 +0300

    Add payout_ins.js

---
+++ pkg/core/scripts/payout_ins.js
@@ -0,0 +1,21 @@
+const {
+    getContract,
+} = require("@overnight-contracts/common/utils/script-utils");
+
+
+async function main() {
+
+    let exchange = await getContract('InsuranceExchange');
+    await (await exchange.setPayoutTimes(1637193600, 24 * 60 * 60, 15 * 60)).wait();
+    await (await exchange.payout()).wait();
+
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
