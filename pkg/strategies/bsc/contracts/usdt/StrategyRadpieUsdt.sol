e245ebf914ea90fefbff068c89dd128e9cc01f1e
Naran Mucherinov <95479946+4yuka@users.noreply.github.com>
2023-10-16 19:42:28 +0700

    Add claiming in Radpie BSC strategies (#261)

--- pkg/strategies/bsc/contracts/usdt/StrategyRadpieUsdt.sol
+++ pkg/strategies/bsc/contracts/usdt/StrategyRadpieUsdt.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@overnight-contracts/core/contracts/Strategy.sol";
 import "@overnight-contracts/connectors/contracts/stuff/Radpie.sol";
+import "@overnight-contracts/connectors/contracts/stuff/PancakeV3.sol";
 
 contract StrategyRadpieUsdt is Strategy {
 
@@ -10,15 +11,25 @@ contract StrategyRadpieUsdt is Strategy {
 
     struct StrategyParams {
         address usdt;
+        address esRdnt;
+        address rdnt;
+        address wBnb;
         address radpiePoolHelper;
         address radiantStaking;
+        address radiantRewardManager;
+        address pancakeSwapV3Router;
     }
 
     // --- params
 
     IERC20 public usdt;
+    IERC20 public esRdnt;
+    IERC20 public rdnt;
+    IERC20 public wBnb;
     IRadpiePoolHelper public radpiePoolHelper;
     IRadiantStaking public radiantStaking;
+    IRDNTRewardManager public radiantRewardManager;
+    address public pancakeSwapV3Router;
 
     // --- events
 
@@ -36,9 +47,23 @@ contract StrategyRadpieUsdt is Strategy {
     // --- Setters
 
     function setParams(StrategyParams calldata params) external onlyAdmin {
+        require(params.usdt != address(0), 'usdt is empty');
+        require(params.esRdnt != address(0), 'esRdnt is empty');
+        require(params.rdnt != address(0), 'rdnt is empty');
+        require(params.wBnb != address(0), 'wBnb is empty');
+        require(params.radpiePoolHelper != address(0), 'radpiePoolHelper is empty');
+        require(params.radiantStaking != address(0), 'radiantStaking is empty');
+        require(params.radiantRewardManager != address(0), 'radiantRewardManager is empty');
+        require(params.pancakeSwapV3Router != address(0), 'pancakeSwapV3Router is empty');
+
         usdt = IERC20(params.usdt);
+        esRdnt = IERC20(params.esRdnt);
+        rdnt = IERC20(params.rdnt);
+        wBnb = IERC20(params.wBnb);
         radpiePoolHelper = IRadpiePoolHelper(params.radpiePoolHelper);
         radiantStaking = IRadiantStaking(params.radiantStaking);
+        radiantRewardManager = IRDNTRewardManager(params.radiantRewardManager);
+        pancakeSwapV3Router = params.pancakeSwapV3Router;
 
         emit StrategyUpdatedParams();
     }
@@ -105,6 +130,32 @@ contract StrategyRadpieUsdt is Strategy {
     }
 
     function _claimRewards(address _to) internal override returns (uint256) {
-        return 0;
+
+        // claim rewards
+        uint256 entitledRDNT = radiantRewardManager.entitledRDNT(address(this));
+        if (entitledRDNT > 0) {
+            radiantRewardManager.redeemEntitledRDNT();
+        }
+
+        // sell rewards
+        uint256 totalUsdt;
+
+        uint256 esRdntBalance = esRdnt.balanceOf(address(this));
+        if (esRdntBalance > 0) {
+            totalUsdt += PancakeSwapV3Library.pathSwap(
+                pancakeSwapV3Router,
+                address(esRdnt),
+                abi.encodePacked(address(esRdnt), uint24(2500), address(rdnt), uint24(2500), address(wBnb), uint24(100), address(usdt)),
+                address(this),
+                esRdntBalance,
+                0
+            );
+        }
+
+        if (totalUsdt > 0) {
+            usdt.transfer(_to, totalUsdt);
+        }
+
+        return totalUsdt;
     }
 }
--- pkg/strategies/bsc/deploy/usdt/12_strategy_radpie_usdt.js
+++ pkg/strategies/bsc/deploy/usdt/12_strategy_radpie_usdt.js
@@ -17,8 +17,13 @@ module.exports = async ({deployments}) => {
 async function getParams() {
     return {
         usdt: BSC.usdt,
+        esRdnt: BSC.esRdnt,
+        rdnt: BSC.rdnt,
+        wBnb: BSC.wBnb,
         radpiePoolHelper: '0xEdAb351C299eAe8E06E4f97a3f262Bb4E364612C',
         radiantStaking: '0xe05157aA8D14b8ED1d816D505b3D5DEEB83ca131',
+        radiantRewardManager: '0x91DD506e1f27f50dd40d0E8634013b1F2393DCa0',
+        pancakeSwapV3Router: BSC.pancakeSwapV3Router,
     }
 }
 
