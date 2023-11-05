2703ac4397624986470c1387520328ef85bf7aed
y.pavlov <lenvonder12vorh@gmail.com>
2023-11-05 17:00:46 +0300

    add proposals

---
+++ pkg/proposals/scripts/arbitrum/02_set_cash_silo_usdc.js
@@ -0,0 +1,37 @@
+const hre = require("hardhat");
+const {getContract, showM2M, execTimelock} = require("@overnight-contracts/common/utils/script-utils");
+const {createProposal, testProposal, testUsdPlus} = require("@overnight-contracts/common/utils/governance");
+const {Roles} = require("@overnight-contracts/common/utils/roles");
+
+const path = require('path');
+const {prepareEnvironment} = require("@overnight-contracts/common/utils/tests");
+let filename = path.basename(__filename);
+filename = filename.substring(0, filename.indexOf(".js"));
+
+async function main() {
+
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+    let pm = await getContract('PortfolioManager', 'arbitrum');
+    let strategy = await getContract('StrategySiloUsdc', 'arbitrum');
+
+    addresses.push(pm.address);
+    values.push(0);
+    abis.push(pm.interface.encodeFunctionData('addStrategy', [strategy.address]));
+
+    addresses.push(pm.address);
+    values.push(0);
+    abis.push(pm.interface.encodeFunctionData('setCashStrategy', [strategy.address]));
+
+    await createProposal(filename, addresses, values, abis);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
