1076f709dbbb145faf542ed6453db1b21763e9cd
Valpaq <viktor.paramonov2000@gmail.com>
2023-11-21 16:11:56 +0300

    moonwell usdc

--- pkg/common/utils/assets.js
+++ pkg/common/utils/assets.js
@@ -31,6 +31,7 @@ let BASE = {
     // moonwell
     moonwellUnitroller: '0xfBb21d0380beE3312B33c4353c8936a0F13EF26C',
     moonwellUsdbc: '0x703843C3379b52F9FF486c9f5892218d2a065cC8',
+    moonwellUsdc: '0xEdc817A28E8B93B03976FBd4a3dDBc9f7D176c22',
     moonwellWeth: '0x628ff693426583D9a7FB391E54366292F509D457',
     moonwellDai: '0x73b06D8d18De422E269645eaCe15400DE7462417',
     // compound
