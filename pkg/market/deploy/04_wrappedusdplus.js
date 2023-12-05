6b54f10ec4fdb8cfda9c2bf9f2190d2c0458ff95
y.pavlov <lenvonder12vorh@gmail.com>
2023-12-05 13:11:20 +0300

    [Arbitrum] Upgrade WrappedUsdPlusToken to support pause

--- pkg/market/deploy/04_wrappedusdplus.js
+++ pkg/market/deploy/04_wrappedusdplus.js
@@ -2,6 +2,7 @@ const {deployProxy} = require("@overnight-contracts/common/utils/deployProxy");
 const {getContract} = require("@overnight-contracts/common/utils/script-utils");
 const hre = require("hardhat");
 const {ethers} = require("hardhat");
+const {ZERO_ADDRESS} = require("@openzeppelin/test-helpers/src/constants");
 
 module.exports = async ({deployments}) => {
     const {save} = deployments;
@@ -29,10 +30,17 @@ module.exports = async ({deployments}) => {
 
     let wrappedUsdPlusToken = await ethers.getContract('WrappedUsdPlusToken');
 
+    if (await wrappedUsdPlusToken.roleManager() === ZERO_ADDRESS){
+
+        let roleManager = await getContract('RoleManager');
+        await (await wrappedUsdPlusToken.setRoleManager(roleManager.address)).wait();
+    }
+
     console.log('WrappedUsdPlusToken deploy done()');
-    console.log('Symbol:   ' + await wrappedUsdPlusToken.symbol());
-    console.log('Name:     ' + await wrappedUsdPlusToken.name());
-    console.log('Decimals: ' + await wrappedUsdPlusToken.decimals());
+    console.log('Symbol:      ' + await wrappedUsdPlusToken.symbol());
+    console.log('Name:        ' + await wrappedUsdPlusToken.name());
+    console.log('Decimals:    ' + await wrappedUsdPlusToken.decimals());
+    console.log('RoleManager: ' + await wrappedUsdPlusToken.roleManager());
 };
 
 module.exports.tags = ['base', 'WrappedUsdPlusToken'];
