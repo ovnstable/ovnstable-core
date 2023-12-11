1c0c26b92c0e61412e82841d35cb6dc81e3f8f4f
matveyb <matvey.b2022@gmail.com>
2023-12-11 08:58:51 +0100

    fix aero usdbc

--- pkg/common/utils/script-utils.js
+++ pkg/common/utils/script-utils.js
@@ -827,7 +827,7 @@ async function transferAsset(assetAddress, to, amount) {
         case "BASE":
             switch (assetAddress) {
                 case BASE.usdbc:
-                    from = '0xd77bb090ef30844b086ae14bcedbab340d435159';
+                    from = '0xc68a33de9CEAC7BdaED242aE1DC40D673eD4f643';
                     break;
                 case BASE.usdc:
                     from = '0x20fe51a9229eef2cf8ad9e89d91cab9312cf3b7a';
