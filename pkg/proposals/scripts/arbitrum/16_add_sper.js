16a9eb28236100ea3a741f7526e2ae42ece3fcc6
y.pavlov <lenvonder12vorh@gmail.com>
2023-12-20 14:41:59 +0300

    [CORE] Add proposal: Sper AlphaArb to USD+ on Arbitrum

---
+++ pkg/proposals/scripts/arbitrum/16_add_sper.js
@@ -0,0 +1,32 @@
+const hre = require("hardhat");
+const {getContract, showM2M, execTimelock} = require("@overnight-contracts/common/utils/script-utils");
+const {createProposal, testProposal, testUsdPlus} = require("@overnight-contracts/common/utils/governance");
+const {Roles} = require("@overnight-contracts/common/utils/roles");
+
+const path = require('path');
+const {prepareEnvironment} = require("@overnight-contracts/common/utils/tests");
+let filename = path.basename(__filename);
+filename = filename.substring(0, filename.indexOf(".js"));
+
+async function main() {
+
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+    let pm = await getContract('PortfolioManager', 'arbitrum');
+
+    addresses.push(pm.address);
+    values.push(0);
+    abis.push(pm.interface.encodeFunctionData('addStrategy', ['0x0Ce0262Dc2DF64991E3d5AF163175065c1000b86']));
+
+    await createProposal(filename, addresses, values, abis);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
