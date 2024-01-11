68117759f89d5df357760ffa95cc5aa085c12ec7
y.pavlov <lenvonder12vorh@gmail.com>
2024-01-11 17:11:32 +0300

    [Base] Add 11_add_beta_ets.json

---
+++ pkg/proposals/scripts/base/11_add_beta_ets.js
@@ -0,0 +1,45 @@
+const hre = require("hardhat");
+const {getContract, showM2M, execTimelock} = require("@overnight-contracts/common/utils/script-utils");
+const {
+    createProposal,
+    testProposal,
+    testUsdPlus,
+    testStrategy
+} = require("@overnight-contracts/common/utils/governance");
+const {Roles} = require("@overnight-contracts/common/utils/roles");
+
+const path = require('path');
+const {prepareEnvironment} = require("@overnight-contracts/common/utils/tests");
+const {strategySiloUsdc} = require("@overnight-contracts/strategies-arbitrum/deploy/38_strategy_silo_usdc");
+const {ethers} = require("hardhat");
+const {strategyEtsEtaParams} = require("@overnight-contracts/strategies-base/deploy/11_ets_eta");
+let filename = path.basename(__filename);
+filename = filename.substring(0, filename.indexOf(".js"));
+
+async function main() {
+
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+    let strategy = await getContract('StrategyEtsBeta', 'base_dai');
+    let pm = await getContract('PortfolioManager', 'base_dai');
+
+    addresses.push(pm.address);
+    values.push(0);
+    abis.push(pm.interface.encodeFunctionData('addStrategy', [strategy.address]));
+
+    // await testProposal(addresses, values, abis);
+    // await testUsdPlus(filename, 'base');
+
+    await createProposal(filename, addresses, values, abis);
+
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
