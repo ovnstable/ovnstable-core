8c9e225e01fc3f74902cfb80928a6bdd24d0a96a
y.pavlov <lenvonder12vorh@gmail.com>
2024-01-17 11:57:05 +0300

    Deploy StrategyEtsBeta to USDC+

--- pkg/strategies/base/deploy/usdc/02_ets_beta.js
+++ pkg/strategies/base/deploy/usdc/02_ets_beta.js
@@ -7,7 +7,7 @@ module.exports = async ({ deployments }) => {
     const { save } = deployments;
 
     await deploySection(async (name) => {
-        await deployProxyMulti(name, 'StrategyEtsInch', deployments, save, null);
+        await deployProxyMulti(name, 'StrategyEts', deployments, save, null);
     });
 
     await settingSection('BetaBase', async (strategy) => {
@@ -19,11 +19,7 @@ async function getParams() {
     return {
         rebaseToken: '0xea8448d927D8d98165Eb839843D1818c4b333981',
         hedgeExchanger: '0xcB2e0fEdf94f94b49d50904DB2f6DC034BC7ffa9',
-        asset: BASE.dai,
-        underlyingAsset: BASE.usdc,
-        oracleAsset: BASE.chainlinkDai,
-        oracleUnderlyingAsset: BASE.chainlinkUsdc,
-        inchSwapper: BASE.inchSwapper,
+        asset: BASE.usdc,
     };
 }
 
