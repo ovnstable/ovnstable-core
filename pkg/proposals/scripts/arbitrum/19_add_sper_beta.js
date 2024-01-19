4fd515c1ff13734cc3bc7335ea508c0e2ab82f00
y.pavlov <lenvonder12vorh@gmail.com>
2024-01-19 19:23:36 +0300

    Add proposal: 19_add_sper_beta.js

---
+++ pkg/proposals/scripts/arbitrum/19_add_sper_beta.js
@@ -0,0 +1,32 @@
+const hre = require("hardhat");
+const {getContract, showM2M, execTimelock} = require("@overnight-contracts/common/utils/script-utils");
+const {createProposal, testProposal, testUsdPlus, testStrategy} = require("@overnight-contracts/common/utils/governance");
+const {Roles} = require("@overnight-contracts/common/utils/roles");
+
+const path = require('path');
+const {prepareEnvironment} = require("@overnight-contracts/common/utils/tests");
+let filename = path.basename(__filename);
+filename = filename.substring(0, filename.indexOf(".js"));
+
+async function main() {
+
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+    let pm = await getContract('PortfolioManager', 'arbitrum_usdt');
+
+    addresses.push(pm.address);
+    values.push(0);
+    abis.push(pm.interface.encodeFunctionData('addStrategy', ['0x053A0d5268409b5fA2FB02340c9d6e276152DC24']));
+
+    await createProposal(filename, addresses, values, abis);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
