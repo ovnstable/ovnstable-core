a330ad5daa486596a4908c631f6c402fe0634afd
y.pavlov <lenvonder12vorh@gmail.com>
2024-01-22 17:34:13 +0300

    Add proposal: 21_add_insurance_usdt.json

---
+++ pkg/proposals/scripts/arbitrum/21_add_insurance_usdt.js
@@ -0,0 +1,34 @@
+const hre = require("hardhat");
+const {getContract, showM2M, execTimelock} = require("@overnight-contracts/common/utils/script-utils");
+const {createProposal, testProposal, testUsdPlus, testStrategy} = require("@overnight-contracts/common/utils/governance");
+const {Roles} = require("@overnight-contracts/common/utils/roles");
+
+const path = require('path');
+const {prepareEnvironment} = require("@overnight-contracts/common/utils/tests");
+let filename = path.basename(__filename);
+filename = filename.substring(0, filename.indexOf(".js"));
+
+async function main() {
+
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+    let exchange = await getContract('Exchange', 'arbitrum_usdt');
+    let insurance = await getContract('InsuranceExchange', 'arbitrum');
+
+    addresses.push(exchange.address);
+    values.push(0);
+    abis.push(exchange.interface.encodeFunctionData('setInsurance', [insurance.address]));
+
+    // await testProposal(addresses, values, abis);
+    await createProposal(filename, addresses, values, abis);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
