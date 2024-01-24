f9e14fb6d6f35e151c9f2f5e5c735c476c3dcd7d
y.pavlov <lenvonder12vorh@gmail.com>
2024-01-24 12:08:14 +0300

    Merge confilcts in InsuranceExchange

--- pkg/core/contracts/InsuranceExchangeOld.sol
+++ pkg/core/contracts/InsuranceExchangeOld.sol
@@ -286,29 +286,20 @@ contract InsuranceExchangeOld is IInsuranceExchange, Initializable, AccessContro
      * This method should convert this asset to OVN.
      * @param swapData consist of odos data to make swap
      */
-    function premium(SwapData memory swapData) external onlyInsured {
+    function premium(SwapData memory swapData, uint256 premiumAmount) external onlyInsured {
         _swap(swapData);
     }
 
     /**
      * @dev This function is calling when USD+ (or other plus) make payout and there are some loss value.
-     * This method should convert some OVN to outputToken and transfer it to the PortfolioManager contract.
+     * This method should convert some OVN to asset and transfer it to the Exchanger contract.
      * @param swapData consist of odos data to make swap
-     * @param outputAmount needed amount of outputToken (USDC|USDT|DAI) to cover the loss
+     * @param assetAmount needed amount of asset to cover the loss
      * @param to recipient of assets
      */
-    function compensate(SwapData memory swapData, uint256 outputAmount, address to) external onlyInsured {
-        require(swapData.inputTokenAddress == address(asset), 'asset != swapData.inputToken');
-
-        IERC20 outputToken = IERC20(swapData.outputTokenAddress);
-        uint256 expectedAmountOut = assetOracle.convert(address(asset), address(swapData.outputTokenAddress), swapData.amountIn);
-
+    function compensate(SwapData memory swapData, uint256 assetAmount, address to) external onlyInsured {
         _swap(swapData);
-
-        uint256 amountOut = outputToken.balanceOf(address(this));
-        require(amountOut < expectedAmountOut * (10000 + swapSlippage) / 10000, 'swapped too much');
-
-        outputToken.transfer(to, outputAmount);
+        IERC20(swapData.outputTokenAddress).transfer(to, assetAmount);
     }
 
     function _swap(SwapData memory swapData) internal {
