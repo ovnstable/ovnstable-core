8fb13f278aaffba7373d820eda02efa42d7e7314
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2024-02-19 12:41:16 +0200

    linea proposals

--- pkg/proposals/scripts/linea/07_set_exchangerA.js
+++ pkg/proposals/scripts/linea/07_set_exchangerA.js
@@ -7,7 +7,7 @@ const path = require('path');
 const {prepareEnvironment} = require("@overnight-contracts/common/utils/tests");
 const {strategySiloUsdc} = require("@overnight-contracts/strategies-arbitrum/deploy/38_strategy_silo_usdc");
 const {ethers} = require("hardhat");
-const {BASE} = require("@overnight-contracts/common/utils/assets");
+const {LINEA} = require("@overnight-contracts/common/utils/assets");
 let filename = path.basename(__filename);
 filename = filename.substring(0, filename.indexOf(".js"));
 
@@ -17,27 +17,28 @@ async function main() {
     let values = [];
     let abis = [];
 
-    let epsilon = await getContract('StrategyEtsEpsilon', 'base');
+    let strategy = await getContract('strategylphaLinea', 'linea');
 
-    let epsilonParams =  {
-        asset: BASE.usdbc,
-        rebaseToken: '0xa42DFcE3518B730624F055AF8A95222F02CF5AFA',
-        hedgeExchanger: '0x33c1327834BcedD2206335ED62E09ce65617d346',
+    let strategyParams =  {
+        asset: LINEA.usdc,
+        rebaseToken: '0xC98C43CADfC611eABC08940a86B910C6433FA12A',
+        hedgeExchanger: '0x631e1a02B52e48311c7FC91F55FfB15c26b50503',
     }
 
-    addresses.push(epsilon.address);
+    addresses.push(strategy.address);
     values.push(0);
-    abis.push(epsilon.interface.encodeFunctionData('setParams', [epsilonParams]));
+    abis.push(strategy.interface.encodeFunctionData('setParams', [strategyParams]));
+
+    
   
     //
     // await showM2M();
     // await testProposal(addresses, values, abis);
     // await showM2M();
     //
-    // await testStrategy(filename, epsilon, 'base');
-    // await testUsdPlus(filename, 'base');
-    //
-    await createProposal(filename, addresses, values, abis);
+    await testStrategy(filename, strategy, 'linea'); 
+    // await testUsdPlus(filename, 'linea');
+    // await createProposal(filename, addresses, values, abis); 
 }
 
 main()
--- pkg/proposals/scripts/linea/08_set_exchangerB.js
+++ pkg/proposals/scripts/linea/08_set_exchangerB.js
@@ -7,7 +7,7 @@ const path = require('path');
 const {prepareEnvironment} = require("@overnight-contracts/common/utils/tests");
 const {strategySiloUsdc} = require("@overnight-contracts/strategies-arbitrum/deploy/38_strategy_silo_usdc");
 const {ethers} = require("hardhat");
-const {BASE} = require("@overnight-contracts/common/utils/assets");
+const {LINEA} = require("@overnight-contracts/common/utils/assets");
 let filename = path.basename(__filename);
 filename = filename.substring(0, filename.indexOf(".js"));
 
@@ -16,28 +16,27 @@ async function main() {
     let addresses = [];
     let values = [];
     let abis = [];
+ 
+    let strategy = await getContract('strategyBetaLinea', 'linea');
 
-    let epsilon = await getContract('StrategyEtsEpsilon', 'base');
-
-    let epsilonParams =  {
-        asset: BASE.usdbc,
-        rebaseToken: '0xa42DFcE3518B730624F055AF8A95222F02CF5AFA',
-        hedgeExchanger: '0x33c1327834BcedD2206335ED62E09ce65617d346',
+    let strategyParams =  {
+        asset: LINEA.usdt,
+        rebaseToken: '0x2253BdD62eA63F7CBbf92785EEdcCAc7521FB6A1',
+        hedgeExchanger: '0x40ae104C59af1B9d23Dcd9c5715780E2132631f1',
     }
 
-    addresses.push(epsilon.address);
+    addresses.push(strategy.address);
     values.push(0);
-    abis.push(epsilon.interface.encodeFunctionData('setParams', [epsilonParams]));
+    abis.push(strategy.interface.encodeFunctionData('setParams', [strategyParams]));
   
     //
     // await showM2M();
     // await testProposal(addresses, values, abis);
     // await showM2M();
-    //
-    // await testStrategy(filename, epsilon, 'base');
-    // await testUsdPlus(filename, 'base');
-    //
-    await createProposal(filename, addresses, values, abis);
+    // 
+    await testStrategy(filename, strategy, 'linea');
+    // await testUsdPlus(filename, 'linea');
+    // await createProposal(filename, addresses, values, abis); 
 }
 
 main()
