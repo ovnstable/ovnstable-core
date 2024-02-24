432b2a0083bf92c66540d96ad6f69a021c68529a
venikpapa <venikpapa@gmail.com>
2024-02-24 08:57:02 +0300

    minor

--- pkg/proposals/scripts/linea/07_set_exchanger.js
+++ pkg/proposals/scripts/linea/07_set_exchanger.js
@@ -1,18 +1,7 @@
 const hre = require("hardhat");
-const {
-    getContract,
-    showM2M,
-    execTimelock,
-    initWallet,
-} = require("@overnight-contracts/common/utils/script-utils");
-const {
-    createProposal,
-    testProposal,
-    testUsdPlus,
-    testStrategy,
-} = require("@overnight-contracts/common/utils/governance");
+const { getContract, showM2M, execTimelock, initWallet } = require("@overnight-contracts/common/utils/script-utils");
+const { createProposal, testProposal, testUsdPlus, testStrategy } = require("@overnight-contracts/common/utils/governance");
 const { Roles } = require("@overnight-contracts/common/utils/roles");
-
 const path = require("path");
 const { ethers } = require("hardhat");
 const { LINEA } = require("@overnight-contracts/common/utils/assets");
@@ -24,33 +13,28 @@ async function main() {
     let values = [];
     let abis = [];
 
-    let strategyA = await getContract("StrategyEtsAlpha", "linea");
+    let strategyAlpha = await getContract("StrategyEtsAlpha", "linea");
+    let strategyBeta = await getContract("StrategyEtsBetaUsdt", "linea_usdt");
 
-    let strategyAParams = {
+    let strategyAlphaParams = {
         asset: LINEA.usdc,
         rebaseToken: "0xC98C43CADfC611eABC08940a86B910C6433FA12A",
-        hedgeExchanger: "0xa2dbE1D92d9C66DbB3e4C9358d91988907bC9Ad4", /// !!! новый проверить
+        hedgeExchanger: "0xa2dbE1D92d9C66DbB3e4C9358d91988907bC9Ad4"
     };
 
-    addresses.push(strategyA.address);
-    values.push(0);
-    abis.push(
-        strategyA.interface.encodeFunctionData("setParams", [strategyAParams])
-    );
-
-    let strategyB = await getContract("StrategyEtsBetaUsdt", "linea_usdt");
-
-    let strategyBParams = {
+    let strategyBetaParams = {
         asset: LINEA.usdt,
         rebaseToken: "0x2253BdD62eA63F7CBbf92785EEdcCAc7521FB6A1",
-        hedgeExchanger: "0xea50c697278920B143223334ffa3856d0644F7C6",  /// !!! новый проверить
+        hedgeExchanger: "0xea50c697278920B143223334ffa3856d0644F7C6"
     };
 
-    addresses.push(strategyB.address);
+    addresses.push(strategyAlpha.address);
     values.push(0);
-    abis.push(
-        strategyB.interface.encodeFunctionData("setParams", [strategyBParams])
-    );
+    abis.push(strategyAlpha.interface.encodeFunctionData("setParams", [strategyAlphaParams]));
+
+    addresses.push(strategyBeta.address);
+    values.push(0);
+    abis.push(strategyBeta.interface.encodeFunctionData("setParams", [strategyBetaParams]));
 
     //
     // await showM2M();
