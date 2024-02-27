3c8be35ea81f87616db20b07a464de1799f53d7b
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2024-02-27 16:16:27 +0200

    add exchanger

---
+++ pkg/proposals/scripts/zksync/04_set_exchanger.js
@@ -0,0 +1,45 @@
+const hre = require("hardhat");
+const { getContract, showM2M, execTimelock, initWallet } = require("@overnight-contracts/common/utils/script-utils");
+const { createProposal, testProposal, testUsdPlus, testStrategy } = require("@overnight-contracts/common/utils/governance");
+const { Roles } = require("@overnight-contracts/common/utils/roles");
+const path = require("path");
+const { ethers } = require("hardhat");
+const { ZKSYNC } = require("@overnight-contracts/common/utils/assets");
+let filename = path.basename(__filename);
+filename = filename.substring(0, filename.indexOf(".js"));
+
+async function main() {
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+    let strategy = await getContract("StrategyAlphaZk", "zksync"); 
+
+    let strategyParams = {
+        asset: ZKSYNC.usdc,
+        rebaseToken: "0x8E86e46278518EFc1C5CEd245cBA2C7e3ef11557",
+        hedgeExchanger: "0x44A0C5dD9BbC32714b1B24E39BDd02739361e643"
+    };
+ 
+
+    addresses.push(strategy.address);
+    values.push(0);
+    abis.push(strategy.interface.encodeFunctionData("setParams", [strategyParams]));
+ 
+
+    //
+    // await showM2M();
+    // await testProposal(addresses, values, abis);
+    // await showM2M();
+    //
+    await testStrategy(filename, strategyA, "zksync"); 
+    await testUsdPlus(filename, "zksync"); 
+    // await createProposal(filename, addresses, values, abis);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
