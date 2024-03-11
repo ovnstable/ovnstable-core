305b1d463e660129b5705fca009425be111ac782
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2024-03-11 22:25:21 +0200

    lint fixes

--- pkg/common/utils/initLog.js
+++ pkg/common/utils/initLog.js
@@ -1,19 +1,22 @@
 const dotenv = require("dotenv");
 const Sentry = require("@sentry/node");
+const { captureConsoleIntegration } = require("@sentry/integrations");
+
 if (!process || !process.env || !process.env.PK) {
-  dotenv.config({ path: __dirname + "/../../../.env" });
+    dotenv.config({ path: `${__dirname}/../../../.env` });
 }
-if (process.env.DSN)
-  Sentry.init({
-    dsn: process.env.DSN,
-    attachStacktrace: true,
-    integrations: [
-      new Sentry.Integrations.CaptureConsole({
-        levels: ["error",  'debug', 'assert'],
-      }),
-    ],
-    release: "1.0.0",
-    environment: "prod",
-    maxBreadcrumbs: 50,
-  });
+if (process.env.DSN) {
+    Sentry.init({
+        dsn: process.env.DSN,
+        attachStacktrace: true,
+        integrations: [
+            captureConsoleIntegration({
+                levels: ["error", "debug", "assert"],
+            }),
+        ],
+        release: "1.0.0",
+        environment: "prod",
+        maxBreadcrumbs: 50,
+    });
+}
 module.exports = Sentry;
