bdc2a192a014f7a1a126781e5807a0c63dfcc194
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2024-03-13 08:20:23 +0200

    fix

--- pkg/core/deploy/chains/base/01_basePayoutManager.js
+++ pkg/core/deploy/chains/base/01_basePayoutManager.js
@@ -1,13 +1,21 @@
 const {deployProxy} = require("@overnight-contracts/common/utils/deployProxy");
 const {ethers} = require("hardhat");
 const {Roles} = require("@overnight-contracts/common/utils/roles");
-const {getContract} = require("@overnight-contracts/common/utils/script-utils");
+const {getContract, getWalletAddress} = require("@overnight-contracts/common/utils/script-utils");
 const {COMMON} = require("@overnight-contracts/common/utils/assets");
 const hre = require("hardhat");
 
 module.exports = async ({deployments}) => {
     const {save} = deployments;
+    const newAdmin =  "0x05129E3CE8C566dE564203B0fd85111bBD84C424";
     await deployProxy('BasePayoutManager', deployments, save);
+    let payoutManager = await ethers.getContract('BasePayoutManager');
+    const currentWallet =  await getWalletAddress();
+    if (currentWallet !==newAdmin) {
+        await (await payoutManager.grantRole(Roles.DEFAULT_ADMIN_ROLE, newAdmin)).wait()
+        await (await payoutManager.revokeRole(Roles.DEFAULT_ADMIN_ROLE, currentWallet)).wait()
+    }
+
 
     if (hre.ovn && hre.ovn.setting){
 
@@ -24,6 +32,8 @@ module.exports = async ({deployments}) => {
         await (await payoutManager.grantRole(Roles.EXCHANGER, exchangeUsdPlus.address)).wait();
         await (await payoutManager.grantRole(Roles.EXCHANGER, exchangeDaiPlus.address)).wait();
         await (await payoutManager.grantRole(Roles.EXCHANGER, exchangeUsdcPlus.address)).wait();
+        await (await payoutManager.grantRole(Roles.DEFAULT_ADMIN_ROLE, "0x05129E3CE8C566dE564203B0fd85111bBD84C424")).wait()
+        await (await payoutManager.revokeRole(Roles.DEFAULT_ADMIN_ROLE, payoutManager.address)).wait()
         console.log('EXCHANGER role done()');
     } 
 
