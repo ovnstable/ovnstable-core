8dc73951e8ed9cb62dc268095b17ae85d82ea9da
valpaq <71923310+valpaq@users.noreply.github.com>
2024-03-18 23:29:47 +0300

    Usdt fix pm sper (#306)
    * fix
    
    * deployment
    
    * fix
    
    ---------
    
    Co-authored-by: Valpaq <viktor.paramonov2000@gmail.com>

---
+++ pkg/proposals/scripts/arbitrum/31_update_usdt_pm_and_drop_sper.js
@@ -0,0 +1,43 @@
+const hre = require("hardhat");
+const { getContract, showM2M, execTimelock } = require("@overnight-contracts/common/utils/script-utils");
+const { createProposal, testProposal, testUsdPlus, testStrategy } = require("@overnight-contracts/common/utils/governance");
+const { Roles } = require("@overnight-contracts/common/utils/roles");
+
+const path = require('path');
+const { prepareEnvironment } = require("@overnight-contracts/common/utils/tests");
+let filename = path.basename(__filename);
+filename = filename.substring(0, filename.indexOf(".js"));
+
+async function main() {
+
+    let addresses = [];
+    let values = [];
+    let abis = [];
+
+    let pm = await getContract('PortfolioManager', 'arbitrum_usdt');
+
+
+    addresses.push(pm.address);
+    values.push(0);
+    abis.push(pm.interface.encodeFunctionData('upgradeTo', ['0x3bb538455820a077424FcfeF794AB8ee8B15be6d']));
+
+    addresses.push(pm.address);
+    values.push(0);
+    abis.push(pm.interface.encodeFunctionData('removeStrategy', ['0xEAF3Bc644bDa5aEc842fC1d3937A533EF67887B6']));
+
+    addresses.push(pm.address);
+    values.push(0);
+    abis.push(pm.interface.encodeFunctionData('upgradeTo', ['0xa78BE2fD2A7357961D950A235fF52Ce0b8f31201']));
+
+    // await testProposal(addresses, values, abis);
+    // await testUsdPlus(filename, 'arbitrum_usdt');
+    await createProposal(filename, addresses, values, abis);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
+
