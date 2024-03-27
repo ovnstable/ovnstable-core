d4b46600a659cf2eb3c61252823d21418e2e89e5
venikpapa <venikpapa@gmail.com>
2024-03-27 06:05:30 +0300

    fix skims

--- pkg/core/deploy/chains/blast/02_setting_blastPayoutManager.js
+++ pkg/core/deploy/chains/blast/02_setting_blastPayoutManager.js
@@ -1,57 +1,32 @@
 const { ethers } = require("hardhat");
-const {
-    getContract,
-    getPrice,
-} = require("@overnight-contracts/common/utils/script-utils");
-const {
-    createSkim,
-    createSkimTo,
-    createSkimToWithFee,
-    createBribe,
-    createBribeWithFee,
-    createSync,
-    createCustomBribe,
-    createCustom,
-} = require("@overnight-contracts/common/utils/payoutListener");
+const { getContract, getPrice } = require("@overnight-contracts/common/utils/script-utils");
+const { createSkim, createSkimTo, createSkimToWithFee, createBribe, createBribeWithFee, createSync, createCustomBribe, createCustom } = require("@overnight-contracts/common/utils/payoutListener");
 const { Roles } = require("@overnight-contracts/common/utils/roles");
 const { COMMON } = require("@overnight-contracts/common/utils/assets");
 
 module.exports = async () => {
     const payoutManager = await getContract("BlastPayoutManager", "blast");
     const usdPlus = await getContract("UsdPlusToken", "blast");
-    const treasury = "0xe497285e466227f4e8648209e34b465daa1f90a0";
+    const usdcPlus = await getContract("UsdPlusToken", "blast_usdc");
 
     let items = [];
-console.log('start to deploy skims')
-    // items.push(...test());
+
     items.push(...swapBlast());
     await (await payoutManager.addItems(items)).wait();
 
     console.log("BlastPayoutManager setting done");
 
     function swapBlast() {
+
         let dex = "SwapBlast";
+        let to = "0xfD5844867387Cecc3A0393b3e0BE32479Ea9e61a";
         let fee = 20;
-        return [
-            createCustom(
-                "0x49B6992DbACf7CAa9cbf4Dbc37234a0167b8edCD",
-                usdPlus.address,
-                "USD+-USDB",
-                dex,
-                "0xd090575313764dE730761733BeD23C1440e7B48C",
-                fee,
-                treasury,
-            ),
-            createCustom(
-                "0xB70Ab4C4BE5872fDD22f43C5907428198CDCB2d5",
-                usdPlus.address,
-                "USD+-USDB",
-                dex,
-                "0xC64A7142263a0157493FB65F5C6d6D62688717AA",
-                fee,
-                treasury,
-            ),
-        ];
+        
+        items.push(createSkimToWithFee('0x49B6992DbACf7CAa9cbf4Dbc37234a0167b8edCD', usdPlus.address, 'USD+/USDB', dex, to, fee, COMMON.rewardWallet));
+        items.push(createSkimToWithFee('0xB70Ab4C4BE5872fDD22f43C5907428198CDCB2d5', usdPlus.address, 'USD+/USDC+', dex, to, fee, COMMON.rewardWallet));
+        items.push(createSkimToWithFee('0xB70Ab4C4BE5872fDD22f43C5907428198CDCB2d5', usdcPlus.address, 'USD+/USDC+', dex, to, fee, COMMON.rewardWallet));
+        return items;
+                
     }
 };
 
