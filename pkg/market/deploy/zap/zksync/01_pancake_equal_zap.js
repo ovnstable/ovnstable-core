1614568ba9d32dbb81633f0fac2d0fe144842f03
Leonid <zotovleonid@icloud.com>
2024-03-28 20:10:04 +0300

    rm settings

---
+++ pkg/market/deploy/zap/zksync/01_pancake_equal_zap.js
@@ -0,0 +1,25 @@
+const {
+    deployProxy,
+} = require("@overnight-contracts/common/utils/deployProxy");
+const { ZKSYNC } = require("@overnight-contracts/common/utils/assets");
+const { ethers } = require("hardhat");
+const { Roles } = require("@overnight-contracts/common/utils/roles");
+
+module.exports = async ({ deployments }) => {
+    const { save } = deployments;
+
+    await deployProxy("PancakeEqualZap", deployments, save);
+    console.log("PancakeEqualZap deploy done()");
+
+    let params = {
+        odosRouter: ZKSYNC.odosRouterV2,
+        npm: ZKSYNC.pancakeNpm,
+    };
+
+    let zap = await ethers.getContract("PancakeEqualZap");
+
+    await (await zap.setParams(params)).wait();
+    console.log("PancakeEqualZap setParams done()");
+};
+
+module.exports.tags = ["PancakeEqualZap"];
