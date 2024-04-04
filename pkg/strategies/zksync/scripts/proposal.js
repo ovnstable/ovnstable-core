1f305c03037508bc3b577ca23c41db479ee65760
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2024-04-04 19:09:51 +0300

    Fix

--- pkg/strategies/zksync/scripts/proposal.js
+++ pkg/strategies/zksync/scripts/proposal.js
@@ -8,7 +8,7 @@ filename = filename.substring(0, filename.indexOf(".js"));
 async function main() {
 
     let pm = await getContract('PortfolioManager', 'zksync');
-    let strategy = await getContract('StrategyZerolend', 'localhost');
+    let strategy = await getContract('StrategyZerolend', 'zksync');
     let addresses = [];
     let values = [];
     let abis = [];
@@ -16,9 +16,8 @@ async function main() {
     addProposal(pm.address, 0, pm.interface.encodeFunctionData('addStrategy', [strategy.address]));
 
     addProposal(pm.address, 0, pm.interface.encodeFunctionData('setCashStrategy', [strategy.address]));
-
     await testProposal(addresses, values, abis);
-   
+    await testUsdPlus(filename, "zksync");
     // await createProposal(filename, addresses, values, abis);
 
     function addProposal(address, value, abi) {
