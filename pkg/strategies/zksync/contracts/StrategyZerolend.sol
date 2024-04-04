4dd2069413a656b287334167e2838c46c1648e21
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2024-04-04 14:49:55 +0300

    fix strategy

--- pkg/strategies/zksync/contracts/StrategyZerolend.sol
+++ pkg/strategies/zksync/contracts/StrategyZerolend.sol
@@ -17,8 +17,8 @@ contract StrategyZerolend is Strategy {
     IERC20 public usdc;
     IERC20 public z0USDC;
     IPool public pool;
-    address public earlyZERO; 
-
+    IRewardsController public rewardsController;
+    IERC20 public earlyZERO;
 
     // --- events
     event StrategyUpdatedParams();
@@ -29,11 +29,11 @@ contract StrategyZerolend is Strategy {
         address usdc;
         address z0USDC;
         address pool;
+        address rewardsController;
         address earlyZERO;
     }
 
-    function initialize() initializer public {
-    }
+    function initialize() public initializer {}
 
     // --- Setters
 
@@ -41,7 +41,8 @@ contract StrategyZerolend is Strategy {
         usdc = IERC20(params.usdc);
         z0USDC = IERC20(params.z0USDC);
         pool = IPool(params.pool);
-        earlyZERO = params.earlyZERO;
+        rewardsController = IRewardsController(params.rewardsController);
+        earlyZERO = IERC20(params.earlyZERO);
 
         emit StrategyUpdatedParams();
     }
@@ -83,35 +84,26 @@ contract StrategyZerolend is Strategy {
     }
 
     function _claimRewards(address _beneficiary) internal override returns (uint256) {
-        address(pool).call(
-            abi.encodeWithSignature(
-                "claimRewards(address[],uint256,address,address)",
-                [
-                    0xb727F8e11bc417c90D4DcaF82EdA06cf590533B5,
-                    0x3E1F1812c2a4f356d1b4FB5Ff7cca5B2ac653b94,
-                    0x15b362768465F966F1E5983b7AE87f4C5Bf75C55,
-                    0x0325F21eB0A16802E2bACD931964434929985548,
-                    0xd97Ac0ce99329EE19b97d03E099eB42D7Aa19ddB,
-                    0x41c618CCE58Fb27cAF4EEb1dd25de1d03A0DAAc6,
-                    0x016341e6Da8da66b33Fd32189328c102f32Da7CC,
-                    0xE60E1953aF56Db378184997cab20731d17c65004,
-                    0x9ca4806fa54984Bf5dA4E280b7AA8bB821D21505,
-                    0xa333c6FF89525939271E796FbDe2a2D9A970F831,
-                    0x2B1BBe3ba39B943eEEf675d6d42607c958F8d20f,
-                    0xc3b6D357e0BeADb18A23a53E1dc4839C2D15bdC2,
-                    0x54330D2333AdBF715eB449AAd38153378601cf67,
-                    0xDB87A5493e308Ee0DEb24C822a559bee52460AFC,
-                    0x1f2dA4FF84d46B12f8931766D6D728a806B410d6,
-                    0x7c65E6eC6fECeb333092e6FE69672a3475C591fB,
-                    0xaBd3C4E4AC6e0d81FCfa5C41a76e9583a8f81909,
-                    0x9002ecb8a06060e3b56669c6B8F18E1c3b119914,
-                    0x56f58d9BE10929CdA709c4134eF7343D73B080Cf
-                ],
-                type(uint256).max,
-                _beneficiary,
-                earlyZERO
-            )
+        address ZERO_ADDRESS = 0x0000000000000000000000000000000000000000;
+        if (address(pool) == ZERO_ADDRESS) {
+            return 0;
+        }
+
+        // claim rewards
+        uint256 z0USDBBalance = z0USDC.balanceOf(address(this));
+        if (z0USDBBalance == 0) {
+            return 0;
+        }
+
+        address[] memory assets = new address[](1);
+        assets[0] = address(z0USDC);
+        uint256 claimedBalance = rewardsController.claimRewards(
+            assets,
+            type(uint256).max,
+            address(this),
+            address(earlyZERO)
         );
+        if (claimedBalance > 0) earlyZERO.transfer(_beneficiary, claimedBalance);
         return 0;
     }
 }
