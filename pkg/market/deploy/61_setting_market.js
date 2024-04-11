32305983e43b93b764cd5994042130444a43ab44
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2024-04-11 17:51:02 +0300

    fix

--- pkg/market/deploy/61_setting_market.js
+++ pkg/market/deploy/61_setting_market.js
@@ -1,12 +1,12 @@
 const {getContract} = require("@overnight-contracts/common/utils/script-utils");
-const hre, { ethers } = require("hardhat");
+const { ethers } = require("hardhat");
 
 module.exports = async ({getNamedAccounts, deployments}) => {
 
-    const market = await getContract("Market", hre.network.name);
+    const market = await getContract("Market", 'localhost');
     const exchange = await getContract("Exchange");
-    const usdPlusToken = await getContract("UsdPlusToken", hre.network.name);
-    const wrappedUsdPlusToken = await getContract("WrappedUsdPlusToken", hre.network.name); 
+    const usdPlusToken = await getContract("UsdPlusToken", 'localhost');
+    const wrappedUsdPlusToken = await getContract("WrappedUsdPlusToken", 'localhost'); 
 
      let asset = await exchange.usdc();
     await (await market.setTokens(asset, usdPlusToken.address, wrappedUsdPlusToken.address)).wait();
