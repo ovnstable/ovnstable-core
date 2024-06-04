214ce8d8f626a89c722b35f2e70ca0279a6d65cd
kostyambl <matveich1903@gmail.com>
2024-06-04 23:36:42 +0300

    add PancakeCLZap.sol

---
+++ pkg/market/contracts/zap/PancakeCLZap.sol
@@ -0,0 +1,208 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./OdosZap.sol";
+import "@overnight-contracts/connectors/contracts/stuff/PancakeV3.sol";
+
+contract PancakeCLZap is OdosZap {
+    INonfungiblePositionManager public npm;
+
+    event TokenId(uint256 tokenId); // name..?
+
+    struct ZapParams {
+        address odosRouter;
+        address npm;
+    }
+
+    struct PancakeCLZapInParams {
+        address pair;
+        uint256[] priceRange;
+        uint256[] amountsOut;
+        int24 tickDelta;
+    }
+
+    struct ResultOfLiquidity {
+        uint amountAsset0Before;
+        uint amountAsset1Before;
+
+        uint amountAsset0After;
+        uint amountAsset1After;
+
+        uint[] amountsPut;
+        uint[] amountsReturned;
+    }
+
+    function setParams(ZapParams memory params) external onlyAdmin {
+        require(params.odosRouter != address(0), "Zero address not allowed");
+        require(params.npm != address(0), "Zero address not allowed");
+        odosRouter = params.odosRouter;
+        npm = INonfungiblePositionManager(params.npm);
+    }
+
+    function zapIn(SwapData memory swapData, PancakeCLZapInParams memory pancakeData) external {
+        _prepareSwap(swapData);
+        _swap(swapData);
+
+        IPancakeV3Pool pool = IPancakeV3Pool(pancakeData.pair);
+
+        address[] memory tokensOut = new address[](2);
+        tokensOut[0] = pool.token0();
+        tokensOut[1] = pool.token1();
+        // uint256[] memory amountsOut = new uint256[](2);
+
+        for (uint256 i = 0; i < tokensOut.length; i++) {
+            IERC20 asset = IERC20(tokensOut[i]);
+
+            if (pancakeData.amountsOut[i] > 0) {
+                asset.transferFrom(msg.sender, address(this), pancakeData.amountsOut[i]);
+            }
+
+            pancakeData.amountsOut[i] = asset.balanceOf(address(this));
+        }
+
+        _addLiquidity(pancakeData);
+    }
+
+    function getProportion(
+        PancakeCLZapInParams memory pancakeData
+    ) public view returns (uint256 token0Amount, uint256 token1Amount, uint256 denominator) {
+
+        IPancakeV3Pool pool = IPancakeV3Pool(pancakeData.pair);
+        uint256 dec0 = 10 ** IERC20Metadata(pool.token0()).decimals();
+        uint256 dec1 = 10 ** IERC20Metadata(pool.token1()).decimals();
+        (uint160 sqrtRatioX96, int24 tick,,,,,) = pool.slot0();
+        int24 tickSpacing = pool.tickSpacing();
+        int24 lowerTick;
+        int24 upperTick;
+
+        if(pancakeData.tickDelta == 0) {
+            (lowerTick, upperTick) = _priceToTicks(pancakeData.priceRange, dec0, pool.tickSpacing());
+        } else {
+            lowerTick = tick / tickSpacing * tickSpacing - (tickSpacing * (pancakeData.tickDelta / 2));
+            upperTick = tick + tickSpacing * ((pancakeData.tickDelta + 1) / 2);
+        }
+
+        uint160 sqrtRatio0 = TickMath.getSqrtRatioAtTick(lowerTick);
+        uint160 sqrtRatio1 = TickMath.getSqrtRatioAtTick(upperTick);
+
+        uint128 liquidity = LiquidityAmounts.getLiquidityForAmounts(sqrtRatioX96, sqrtRatio0, sqrtRatio1, dec0 * 1000, dec1 * 1000);
+        (token0Amount, token1Amount) = LiquidityAmounts.getAmountsForLiquidity(sqrtRatioX96, sqrtRatio0, sqrtRatio1, liquidity);
+
+        denominator = dec0 > dec1 ? dec0 : dec1;
+
+        token0Amount = token0Amount * (denominator / dec0);
+        token1Amount = token1Amount * (denominator / dec1);
+    }
+
+    function _addLiquidity(PancakeCLZapInParams memory pancakeData) internal {
+
+        IPancakeV3Pool pool = IPancakeV3Pool(pancakeData.pair);
+
+        address[] memory tokensOut = new address[](2);
+        tokensOut[0] = pool.token0();
+        tokensOut[1] = pool.token1();
+
+        ResultOfLiquidity memory result;
+
+        IERC20 asset0 = IERC20(tokensOut[0]);
+        IERC20 asset1 = IERC20(tokensOut[1]);
+        asset0.approve(address(npm), pancakeData.amountsOut[0]);
+        asset1.approve(address(npm), pancakeData.amountsOut[1]);
+
+        result.amountAsset0Before = asset0.balanceOf(address(this));
+        result.amountAsset1Before = asset1.balanceOf(address(this));
+
+        int24 lowerTick;
+        int24 upperTick;
+        int24 tickSpacing = pool.tickSpacing();
+        (, int24 tick,,,,,) = pool.slot0();
+
+        if (pancakeData.tickDelta == 0) {
+            (lowerTick, upperTick) = _priceToTicks(pancakeData.priceRange, 10 ** IERC20Metadata(tokensOut[0]).decimals(), tickSpacing);
+        } else {
+            lowerTick = tick / tickSpacing * tickSpacing - (tickSpacing * (pancakeData.tickDelta / 2));
+            upperTick = tick + tickSpacing * ((pancakeData.tickDelta + 1) / 2);
+        }
+
+        (uint256 tokenId,,,) = npm.mint(MintParams(tokensOut[0], tokensOut[1], pool.fee(),
+            lowerTick, upperTick, pancakeData.amountsOut[0], pancakeData.amountsOut[1], 0, 0, msg.sender, block.timestamp));
+
+        emit TokenId(tokenId);
+
+        result.amountAsset0After = asset0.balanceOf(address(this));
+        result.amountAsset1After = asset1.balanceOf(address(this));
+
+        if (result.amountAsset0After > 0) {
+            asset0.transfer(msg.sender, result.amountAsset0After);
+        }
+
+        if (result.amountAsset1After > 0) {
+            asset1.transfer(msg.sender, result.amountAsset1After);
+        }
+
+        result.amountsPut = new uint256[](2);
+        result.amountsPut[0] = result.amountAsset0Before - result.amountAsset0After;
+        result.amountsPut[1] = result.amountAsset1Before - result.amountAsset1After;
+
+        result.amountsReturned = new uint256[](2);
+        result.amountsReturned[0] = result.amountAsset0After;
+        result.amountsReturned[1] = result.amountAsset1After;
+
+        emit PutIntoPool(result.amountsPut, tokensOut);
+        emit ReturnedToUser(result.amountsReturned, tokensOut);
+    }
+
+    function _getSqrtRatioByPrice(uint256 price, uint256 decimals) internal pure returns (uint160) {
+        return SafeCast.toUint160(_sqrt(FullMath.mulDiv(price, 2 ** 192, decimals)));
+    }
+
+    function _getPriceBySqrtRatio(uint160 sqrtRatio, uint256 decimals) internal pure returns (uint256) {
+        return FullMath.mulDiv(uint256(sqrtRatio), uint256(sqrtRatio) * decimals, 2 ** 192);
+    }
+
+    function _priceToTicks(uint256[] memory priceRange, uint256 dec0, int24 tickSpacing) internal pure returns (int24 lowerTick, int24 upperTick) {
+
+        lowerTick = TickMath.getTickAtSqrtRatio(_getSqrtRatioByPrice(priceRange[0], dec0));
+        upperTick = TickMath.getTickAtSqrtRatio(_getSqrtRatioByPrice(priceRange[1], dec0));
+
+        if (lowerTick % tickSpacing != 0) {
+            lowerTick = lowerTick > 0 ? lowerTick - lowerTick % tickSpacing : lowerTick - tickSpacing - (lowerTick % tickSpacing);
+        }
+        if (upperTick % tickSpacing != 0) {
+            upperTick = upperTick > 0 ? upperTick + tickSpacing - (upperTick % tickSpacing) : upperTick - (upperTick % tickSpacing);
+        }
+    }
+
+    function _sqrt(uint y) internal pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+
+    function getPriceFromTick(PancakeCLZapInParams memory pancakeData) public view returns (uint256 left, uint256 right) {
+        IPancakeV3Pool pool = IPancakeV3Pool(pancakeData.pair);
+        uint256 dec0 = 10 ** IERC20Metadata(pool.token0()).decimals();
+        int24 tickSpacing = pool.tickSpacing();
+        (, int24 tick,,,,,) = pool.slot0();
+
+        int24 lowerTick = tick / tickSpacing * tickSpacing - (tickSpacing * (pancakeData.tickDelta / 2));
+        int24 upperTick = tick + tickSpacing * ((pancakeData.tickDelta + 1) / 2);
+
+        left = _getPriceBySqrtRatio(TickMath.getSqrtRatioAtTick(lowerTick), dec0);
+        right = _getPriceBySqrtRatio(TickMath.getSqrtRatioAtTick(upperTick), dec0);
+    }
+
+    function getCurrentPrice(address pair) public view returns (uint256) {
+        IPancakeV3Pool pool = IPancakeV3Pool(pair);
+        uint256 dec0 = IERC20Metadata(pool.token0()).decimals();
+        (uint160 sqrtRatioX96,,,,,,) = pool.slot0();
+        return FullMath.mulDiv(uint256(sqrtRatioX96) * 10 ** dec0, uint256(sqrtRatioX96), 2 ** (96 + 96));
+    }
+}
