766d1e0fe0d4378af74e923607cc50d07c2efce3
Nikita Slezkin <34321043+venikpapa@users.noreply.github.com>
2024-07-01 14:05:55 +0300

    Morpho deposit (#353)
    * morpho strategy
    
    * tests
    
    * fix
    
    * tests
    
    * tests2
    
    * morpho proposal
    
    * morpho proposal 2
    
    * fix review
    
    * removing garbage
    
    * init
    
    * fixed bugs
    
    * new setting
    
    * clean up
    
    * test morpho done
    
    * clean up
    
    * proposal
    
    ---------
    
    Co-authored-by: zxmonty <zxmonty@hotmail.com>

---
+++ pkg/strategies/base/contracts/StrategyMorphoDirect.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+
+import "@overnight-contracts/connectors/contracts/stuff/Morpho.sol";
+
+
+
+contract StrategyMorphoDirect is Strategy {
+    
+    // --- params
+
+    IERC20 public usdcToken;
+    IMorpho public morpho;
+    Id public marketId;
+    MarketParams public marketParams;
+    
+    // --- events
+
+    event StrategyUpdatedParams();
+
+    
+    // --- structs
+
+    struct StrategyParams {
+        address usdc;
+        address morpho;
+        Id marketId;
+        MarketParams marketParams;
+    }
+
+
+    // ---  constructor
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    function initialize() initializer public {
+        __Strategy_init();
+    }
+
+
+    // --- Setters
+
+    function setParams(StrategyParams calldata params) external onlyAdmin {
+        usdcToken = IERC20(params.usdc);
+        morpho = IMorpho(params.morpho);
+        marketId = params.marketId;
+        marketParams = params.marketParams;
+        
+        emit StrategyUpdatedParams();
+    }
+
+
+    // --- logic
+
+    function _stake(
+        address _asset,
+        uint256 _amount
+    ) internal override {
+
+        require(_asset == address(usdcToken), "Some token not compatible");
+        
+        usdcToken.approve(address(morpho), _amount);
+
+        morpho.supply(marketParams, _amount, 0, address(this), "");
+        
+    }
+
+    function _unstake(
+        address _asset,
+        uint256 _amount,
+        address _beneficiary
+    ) internal override returns (uint256) {
+
+        require(_asset == address(usdcToken), "Some token not compatible");
+
+        morpho.withdraw(marketParams, _amount, 0, address(this), address(this));
+        
+        return usdcToken.balanceOf(address(this));
+    }
+
+    function _unstakeFull(
+        address _asset,
+        address _beneficiary
+    ) internal override returns (uint256) {
+
+        require(_asset == address(usdcToken), "Some token not compatible");
+
+        morpho.withdraw(marketParams, 0, morpho.position(marketId, address(this)).supplyShares, address(this), address(this));
+
+        return usdcToken.balanceOf(address(this));
+    }
+
+    function netAssetValue() external view override returns (uint256) {
+        return usdcToken.balanceOf(address(this)) + SharesMathLib.toAssetsDown(morpho.position(marketId, address(this)).supplyShares, morpho.market(marketId).totalSupplyAssets, morpho.market(marketId).totalSupplyShares);
+    }
+
+    function liquidationValue() external view override returns (uint256) {
+        return usdcToken.balanceOf(address(this)) + SharesMathLib.toAssetsDown(morpho.position(marketId, address(this)).supplyShares, morpho.market(marketId).totalSupplyAssets, morpho.market(marketId).totalSupplyShares);
+    }
+
+    function _claimRewards(address _beneficiary) internal override returns (uint256) {
+        return 0;
+    }
+}
