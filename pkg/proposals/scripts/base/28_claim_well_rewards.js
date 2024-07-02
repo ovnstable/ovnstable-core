cb8a8f38e983e212d236c5f51e6a39a87b5aff7e
kostyambl <matveich1903@gmail.com>
2024-07-02 19:19:55 +0300

    add rewards transfer

--- pkg/proposals/scripts/base/28_claim_well_rewards.js
+++ pkg/proposals/scripts/base/28_claim_well_rewards.js
@@ -27,7 +27,8 @@ async function main() {
     let strategyUsdc = await getContract('StrategyMoonwellUsdc', 'base_usdc');
     let well = await getERC20ByAddress("0xA88594D404727625A9437C3f886C7643872296AE");
 
-    let newImpl = "0x2684B920519C639E6fcF568cAc11bD9780954863";
+    let newImplUsdc = "0x0f50F1D8DB0Eb0d96f377924183AE1BaE371A955";
+    let newImplDai = "0x57E0551890f96f9D1Ab4B873e6Be173476097966";
     addProposalItem(strategyDai, 'upgradeTo', [newImpl]);
     addProposalItem(strategyUsdc, 'upgradeTo', [newImpl]);
     addProposalItem(strategyDai, 'transferRewards', []);
