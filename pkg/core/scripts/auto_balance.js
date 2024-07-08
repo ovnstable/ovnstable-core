d817941e53d2bd375c77c22f469dc9c90fa6b0ea
venikpapa <venikpapa@gmail.com>
2024-07-08 20:38:29 +0300

    auto minor

--- pkg/core/scripts/auto_balance.js
+++ pkg/core/scripts/auto_balance.js
@@ -10,13 +10,13 @@ async function main() {
     let pm = await getContract('PortfolioManager', 'base_dai');
     let cdai = "0xb864BA2aab1f53BC3af7AE49a318202dD3fd54C2";
     let dai = await getERC20('dai');
-    let onek = ethers.BigNumber.from("830000000000000000000");
+    let onek = ethers.BigNumber.from("170000000000000000000");
 
     let kek = [
         {
           strategy: '0xb91107e0B01b0Cd2167abf3a0C1ed14E9C58cCa1',
           minWeight: 0,
-          targetWeight: 96700,
+          targetWeight: 99100,
           maxWeight: 100000,
           riskFactor: 0,
           enabled: true,
@@ -25,7 +25,7 @@ async function main() {
         {
           strategy: '0x3E018a972ed429B01d11BdA4d19E6902680104c8',
           minWeight: 0,
-          targetWeight: 3300,
+          targetWeight: 900,
           maxWeight: 100000,
           riskFactor: 0,
           enabled: false,
@@ -46,11 +46,21 @@ async function main() {
         let balance = await dai.balanceOf(cdai);
         console.log("balance", balance.toString());
         if (balance.gte(onek)) {
-            kek[1].enabled = true;
-            kek[0].targetWeight += 100;
-            kek[1].targetWeight -= 100;
-            await (await pm.setStrategyWeights(kek)).wait();
-            await (await pm.balance()).wait();
+            while(true) {
+                kek[1].enabled = true;
+                kek[0].targetWeight += 20;
+                kek[1].targetWeight -= 20;
+                await (await pm.setStrategyWeights(kek)).wait();
+                await (await pm.balance()).wait();
+
+                let balance = await dai.balanceOf(cdai);
+                console.log("balance2", balance.toString());
+                if (balance.gte(onek)) {
+                    let a = 0;
+                } else {
+                    break;
+                }
+            }
         }
         await delay(60000);
     }
