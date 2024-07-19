a349a24e78a6de092bdd234a760b45320fc114f2
kostyambl <matveich1903@gmail.com>
2024-07-19 11:54:56 +0300

    duplicate strategies

---
+++ pkg/strategies/base/deploy/25_ets_upsilon.js
@@ -0,0 +1,29 @@
+const { deployProxyMulti } = require("@overnight-contracts/common/utils/deployProxy");
+const { deploySection, settingSection } = require("@overnight-contracts/common/utils/script-utils");
+const { BASE } = require("@overnight-contracts/common/utils/assets");
+
+let rebaseToken = '0xcDAF2d4F4EDE613cCC188A58EE174bdE532e35b0';
+let hedgeExchanger = '0x4D15F20a06D1aB832995824f3ca81e5F77c4D9e1';
+
+module.exports = async ({ deployments }) => {
+    const { save } = deployments;
+
+    await deploySection(async (name) => {
+        await deployProxyMulti(name, 'StrategyEts', deployments, save, null);
+    });
+
+    await settingSection(async (strategy) => {
+        await (await strategy.setParams(await getParams())).wait();
+    });
+};
+
+async function getParams() {
+    return {
+        asset: BASE.usdc,
+        rebaseToken: rebaseToken,
+        hedgeExchanger: hedgeExchanger,
+    };
+}
+
+module.exports.tags = ['StrategyEtsUpsilon'];
+module.exports.strategyEtsUpsilonParams = getParams;
