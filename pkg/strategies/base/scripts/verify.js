99858d6a911f36e406266c7bdca035692baf7fc7
kostyambl <matveich1903@gmail.com>
2024-07-24 17:43:09 +0300

    duplicate strategies

--- pkg/strategies/base/scripts/verify.js
+++ pkg/strategies/base/scripts/verify.js
@@ -1,7 +1,7 @@
 const {verify } = require("@overnight-contracts/common/utils/verify-utils");
 
 async function main() {
-    let items = ["StrategyEtsUpsilon"];
+    let items = ["StrategyEtsTau"];
     await verify(items);
 }
 
