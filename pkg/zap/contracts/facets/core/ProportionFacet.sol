41dae1bb1095a112aa5aecd74d3cb0bb16e34b69
kostyambl <matveich1903@gmail.com>
2024-07-30 21:20:32 +0300

    move proportions

--- pkg/zap/contracts/facets/core/ProportionFacet.sol
+++ pkg/zap/contracts/facets/core/ProportionFacet.sol
@@ -5,26 +5,6 @@ import "../../interfaces/IMasterFacet.sol";
 import "../../interfaces/Constants.sol";
 
 contract ProportionFacet is IProportionFacet {
-    function getProportion(
-        address pair,
-        int24[] memory tickRange
-    ) internal view returns (uint256 token0Amount, uint256 token1Amount) {
-        IMasterFacet master = IMasterFacet(address(this));
-        (uint256 decimals0, uint256 decimals1) = master.getPoolDecimals(pair);
-        uint256 dec0 = 10 ** decimals0;
-        uint256 dec1 = 10 ** decimals1;
-        uint160 sqrtRatioX96 = master.getPoolSqrtRatioX96(pair);
-
-        uint160 sqrtRatio0 = master.getSqrtRatioAtTick(tickRange[0]);
-        uint160 sqrtRatio1 = master.getSqrtRatioAtTick(tickRange[1]);
-        uint128 liquidity = master.getLiquidityForAmounts(sqrtRatioX96, sqrtRatio0, sqrtRatio1, dec0 * 1000, dec1 * 1000);
-        (token0Amount, token1Amount) = master.getAmountsForLiquidity(sqrtRatioX96, sqrtRatio0, sqrtRatio1, liquidity);
-        uint256 denominator = dec0 > dec1 ? dec0 : dec1;
-
-        token0Amount = token0Amount * (denominator / dec0);
-        token1Amount = token1Amount * (denominator / dec1);
-    }
-
     function getProportionForZap(
         address pair,
         int24[] memory tickRange,
@@ -156,5 +136,25 @@ contract ProportionFacet is IProportionFacet {
             }
         }
         return result;
+    }
+
+    function getProportion(
+        address pair,
+        int24[] memory tickRange
+    ) internal view returns (uint256 token0Amount, uint256 token1Amount) {
+        IMasterFacet master = IMasterFacet(address(this));
+        (uint256 decimals0, uint256 decimals1) = master.getPoolDecimals(pair);
+        uint256 dec0 = 10 ** decimals0;
+        uint256 dec1 = 10 ** decimals1;
+        uint160 sqrtRatioX96 = master.getPoolSqrtRatioX96(pair);
+
+        uint160 sqrtRatio0 = master.getSqrtRatioAtTick(tickRange[0]);
+        uint160 sqrtRatio1 = master.getSqrtRatioAtTick(tickRange[1]);
+        uint128 liquidity = master.getLiquidityForAmounts(sqrtRatioX96, sqrtRatio0, sqrtRatio1, dec0 * 1000, dec1 * 1000);
+        (token0Amount, token1Amount) = master.getAmountsForLiquidity(sqrtRatioX96, sqrtRatio0, sqrtRatio1, liquidity);
+        uint256 denominator = dec0 > dec1 ? dec0 : dec1;
+
+        token0Amount = token0Amount * (denominator / dec0);
+        token1Amount = token1Amount * (denominator / dec1);
     }
 }
