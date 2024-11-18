6378010775e7890c49c308e187f690190367a270
Emil Khodzhakhov <hodzhakhov@gmail.com>
2024-11-18 23:10:25 +0300

    fix

--- pkg/core/scripts/aero-pool.js
+++ pkg/core/scripts/aero-pool.js
@@ -12,7 +12,7 @@ const hre = require("hardhat");
 
 async function main() {
     let amount = 10000;
-    let iterations = 10;
+    let iterations = 1;
 
     let poolAddress = "0x8dd9751961621Fcfc394d90969E5ae0c5BAbE147";
     let devAddress = "0x086dFe298907DFf27BD593BD85208D57e0155c94";
