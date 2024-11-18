a0a2d00d0ef0260d83c2827b170ef63e5123bb3f
Monty Python <zxmonty@hotmail.com>
2024-11-18 12:53:05 +0300

    clean

--- pkg/fund/deploy/aero_swap.js
+++
@@ -1,8 +0,0 @@
-const {deployProxy} = require("@overnight-contracts/common/utils/deployProxy");
-
-module.exports = async ({deployments}) => {
-    const {save} = deployments;
-    await deployProxy('AeroSwap', deployments, save);
-};
-
-module.exports.tags = ['AeroSwap'];
