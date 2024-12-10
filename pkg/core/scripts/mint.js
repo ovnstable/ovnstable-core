c8b34c04abccaaa493e0f97254876a28ed56685b
TechGeorgii <master.wsd064@gmail.com>
2024-12-10 19:24:36 +0700

    Library function to send tx via Thirdweb Engine

--- pkg/core/scripts/mint.js
+++ pkg/core/scripts/mint.js
@@ -1,7 +1,25 @@
 const {toAsset} = require("@overnight-contracts/common/utils/decimals");
 
-const {getContract, showM2M, getCoreAsset, transferETH, initWallet, getWalletAddress} = require("@overnight-contracts/common/utils/script-utils");
+const {getContract, showM2M, getCoreAsset, transferETH, initWallet, getWalletAddress } = require("@overnight-contracts/common/utils/script-utils");
+const { engineSendTxSigned } = require("@overnight-contracts/common/utils/engine");
 
+
+// engine version
+/*
+async function main() {
+    let wallet = "..."; // backed wallet
+    let exchange = await getContract('Exchange');
+    let asset = await getCoreAsset();
+
+    let tx = await asset.populateTransaction.approve(exchange.address, toAsset(1));
+    await engineSendTxSigned(tx, wallet);
+    console.log('Asset approve done');
+
+    tx = await exchange.populateTransaction.buy(asset.address, toAsset(1));
+    await engineSendTxSigned(tx, wallet);
+    console.log('Exchange.buy done');
+}
+*/
 
 async function main() {
 
