7f871015573962873d4346b1c737306452e4ae05
gregorygromov <gregorygromow@icloud.com>
2024-12-25 17:10:24 +0300

    claim Fenix merkl

--- pkg/strategies/blast/scripts/claimFenixRewards.js
+++ pkg/strategies/blast/scripts/claimFenixRewards.js
@@ -14,25 +14,25 @@ async function main() {
         BLAST.distributor,
         ["0xa09A8e94FBaAC9261af8f34d810D96b923B559D2"], // users
         ["0x52f847356b38720B55ee18Cb3e094ca11C85A192"], // tokens
-        ["122078259306390000000000"],                   // amounts
+        ["152456877230060000000000"],                   // amounts
         [                                               // proofs
             [
-                "0xf38d7b0878d90acb6530ff7cfded82d16cd0a52d0be51f898d3978d49f720d95",
-                "0x08f959888057ea920ff82427b1c663d05db8987f274468431b292c817c7ad880",
-                "0xa6382dbf23d9d3daa0b618e2798587db174bb272fbb6029385813ff8b6833e9a",
-                "0xdb3cbe21f9db28ad2fa750c23b45c6c28a69e5f9e97c8b84da8e6f9c7512f386",
-                "0x95100f186316459bbc1b643dec478706b62ed9859a02e5b2d7faec40935283b5",
-                "0xe20c3c66fdb662cd04bbb5388fe41f06b0f8ac0cdc5edcf52191176825edf3be",
-                "0xa7a8d0aa04019ba4f45cdd4ca8279958f65017ba417137bc12f8392339e220a9",
-                "0x47a4a249a9b6212c22579b6077cfdf021e032056305fd25f1958bc3d5b32d0d8",
-                "0x9865299c237c8dca031632161d7091fc6833a0934e0c458be810f73ab3acff18",
-                "0x8cbce7ec7fe42c07c3773d46fc9f084e2d911155aaa541da50d28e122b01523b",
-                "0x901d2d88bc2f2a8855f9141619d7b5e20b6c1d8c910eddfe02061d0b61c9b869",
-                "0x52a1c8c2d71f11a4fa3aabbc04963abee59613965f930dd64a1ace0ba662d2ed",
-                "0x0258757f4fbf2694a68ccf32ff33819685999899b9231b09a215c6ecf037a593",
-                "0x477f5a1cee146117a1c0893a3b796780d4b1b0a951529a243aaa886cd99c1490",
-                "0x2a635201dfc43a6d7d820fbd7d2c5957365d4f6822d74ab18779259b397bda61",
-                "0x8448df1a14f7b08a2abd1986646575e3618e90d8ffab7bd467ba09418f0f06c7"
+                "0x66d2c3685d11487a88232c1efe299c853915beb6f5665e3d89b9c40d8399f94c",
+                "0x115b6fdccf91ce52e25fede8203f746c951de122b5b140094b5e56d1138911d8",
+                "0x69cb2b0b5184563565119d5e0948ad89ad504f0e4964b4ca415c128cf2f1ce02",
+                "0x0a2a19b5919f49bde87da97b34c96ff2d472aab9180a4cc83e517bd2a3a3c1b9",
+                "0xec17d8d979502cc4c17d9a6c819258b9df718d0d6c8dce3c44ddd7b5a4fbcfd5",
+                "0xd9505bce68acfbb69e2108fa6ed8eb193460e2b3b6c1800102714241088aa913",
+                "0x1fccb1ee7caf5607faeb31d24e55125b59ec5acea55835e78c0da48ee78b9e2c",
+                "0xebf5ac7b988e995d253f951bad50c164b87ff5dc04c8b99ec736fd9e9fb5fa2b",
+                "0x10645a80af32ea7fa73c9558114ccc1d8796c123d586e964a5c6931b265c2d40",
+                "0xe9538d71b259818f654f202992a27adec1833b020f9393e3c3847fb2b9d683d4",
+                "0x83f21b6c8501bdd7b50b4d2171c742c50400deedfceb33f95ad156706a113d40",
+                "0xb29e59e62fdfbf380bba751d41ecd53fc344222c0563cb929dc8775768477139",
+                "0x26354bb4b2e2321e935130b3a5c81d86a889582c5eff953374142d96350834aa",
+                "0x0035811ff0994b8c39fc98b126dd39d18574f6a36ce4b9c452203a3c07b3f1d7",
+                "0xb7c591d08ca34a35d5f0631f5e442fcb5287c4c280afd4dbfdabb69a646fd41f",
+                "0x95cf461a7e61137994bf50bc4ab6166da9dc06e1a9705daad9b9f0e007ed44d0"
             ]
         ]
     );
