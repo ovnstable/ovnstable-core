29a1c5d758423b017341bb9ba375bec9fefc99bf
MkJck <mkjck@MacJack.local>
2026-01-26 11:16:21 +0100

    proposals to fix balanceOf in linea, bsc & op

---
+++ pkg/proposals/scripts/optimism/37_fix_balanceOf.js
@@ -0,0 +1,40 @@
+const { getContract } = require("@overnight-contracts/common/utils/script-utils");
+const { testProposal } = require("@overnight-contracts/common/utils/governance");
+const path = require('path');
+let filename = path.basename(__filename);
+filename = filename.substring(0, filename.indexOf(".js"));
+
+
+async function main() {
+
+  let addresses = [];
+  let values = [];
+  let abis = [];
+
+  const UsdPlusToken = await getContract('UsdPlusToken', 'optimism');
+  const DaiPlusToken = await getContract('DaiPlusToken', 'optimism_dai');
+
+
+  const newImplUsdPlus = "0x6c70719c9ebc9F1Dedfd9Ac1197dBfF96De03fCA";  // change it after deploy to the correct one
+
+  addProposalItem(UsdPlusToken, 'upgradeTo', [newImplUsdPlus]);
+  addProposalItem(DaiPlusToken, 'upgradeTo', [newImplUsdPlus]);
+
+  await testProposal(addresses, values, abis);
+  // await createProposal(filename, addresses, values, abis);
+
+  // ========================================================================
+
+  function addProposalItem(contract, methodName, params) {
+    addresses.push(contract.address);
+    values.push(0);
+    abis.push(contract.interface.encodeFunctionData(methodName, params));
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
