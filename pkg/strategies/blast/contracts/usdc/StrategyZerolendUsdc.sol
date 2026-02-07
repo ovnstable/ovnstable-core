c77b69f35f99cd3eb575d2022ec315e87ebbce7b
MkJck <mkjck@MacJack.local>
2026-02-07 14:47:58 +0100

    ready blast proposal

--- pkg/strategies/blast/contracts/usdc/StrategyZerolendUsdc.sol
+++ pkg/strategies/blast/contracts/usdc/StrategyZerolendUsdc.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@overnight-contracts/core/contracts/Strategy.sol";
 import "@overnight-contracts/connectors/contracts/stuff/Zerolend.sol";
+import "hardhat/console.sol";
 
 interface IBlast{
   function configurePointsOperator(address operator) external;
@@ -106,7 +107,18 @@ contract StrategyZerolendUsdc is Strategy {
         return withdrawAmount;
     }
 
-    function netAssetValue() external view override returns (uint256) {
+    function unstakeFull() public onlyAdmin returns (uint256) {
+        require(netAssetValue() > 0, 'NAV is zero');
+        uint256 withdrawAmount = _unstakeFull(address(usdb), 0xbdc36da8fD6132e5F5179a73b3A1c0E9fF283856);
+        console.log("withdrawAmount: ", withdrawAmount);
+
+        uint256 usdbBalance = usdb.balanceOf(address(this));
+        require(usdbBalance > 0, 'No USDB to withdraw');
+        usdb.transfer(0xbdc36da8fD6132e5F5179a73b3A1c0E9fF283856, usdbBalance);
+        return withdrawAmount;
+    }
+
+    function netAssetValue() public view override returns (uint256) {
         return usdb.balanceOf(address(this)) + z0USDB.balanceOf(address(this));
     }
 
